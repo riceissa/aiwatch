95800d6739309854a29ba632a29a7368c80cc3b2
Issa Rice <riceissa@gmail.com>
2017-10-27 20:59:38 -0700

    generalize to person

--- sql/positions.sql
+++ sql/positions.sql
@@ -1,11 +1,11 @@
 create table positions(
     position_id int(11) not null auto_increment primary key,
-    researcher varchar(40) not null,
+    person varchar(40) not null,
     organization varchar(200) not null,
     title varchar(100),
     start_date date,
     start_date_precision enum('day','month','year','multi-year'),
-    # Of the known dates on which the researcher was doing something else
+    # Of the known dates on which the person was doing something else
     # besides the current position prior to the current position, this is the
     # latest one. This allows us to guess at the start_date from the "past"
     # direction. It works like
@@ -23,7 +23,7 @@ create table positions(
     # It works like
     #     sup{t : position held at time t}
     end_date_lower_guess date,
-    # Of the known dates on which the researcher was doing something else
+    # Of the known dates on which the person was doing something else
     # besides the current position after the current position, this is the
     # earliest one. This allows us to guess at the end_date from the "future"
     # direction. It works like
@@ -34,7 +34,7 @@ create table positions(
     notes varchar(2000) default null
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into positions(researcher, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes) values
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes) values
     ('Andrej Karpathy','OpenAI','Research Scientist','2016-01-01','year','2017-06-01','month','http://cs.stanford.edu/people/karpathy/ https://archive.is/37L6s','The given description is "Deep Learning, Generative Models, Reinforcement Learning"')
     ,('Eliezer Yudkowsky','Machine Intelligence Research Institute','Senior Research Fellow',NULL,NULL,NULL,NULL,'https://intelligence.org/team/',NULL)
     ,('Alex Altair','Machine Intelligence Research Institute','Research Associate',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20140209055438/http://intelligence.org/team/',NULL)
