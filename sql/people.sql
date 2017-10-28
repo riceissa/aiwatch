e72e00b9b225f57b919903b09408950ee5a5314f
Issa Rice <riceissa@gmail.com>
2017-10-27 21:01:03 -0700

    generalize to person

--- sql/people.sql
+++ sql/people.sql
@@ -1,6 +1,6 @@
-create table researchers(
-    researcher_id int(11) not null auto_increment primary key,
-    researcher varchar(40) not null,
+create table people(
+    person_id int(11) not null auto_increment primary key,
+    person varchar(40) not null,
     country varchar(20),
     facebook_username varchar(100),
     github_username varchar(100),
@@ -8,10 +8,10 @@ create table researchers(
     website varchar(40),
     urls varchar(2000),
     notes varchar(2000) default null,
-    unique key researcher(`researcher`)
+    unique key person(`person`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into researchers(researcher, country, facebook_username, github_username, linkedin_username, website, urls, notes) values
+insert into people(person, country, facebook_username, github_username, linkedin_username, website, urls, notes) values
     ('Andrej Karpathy',NULL,NULL,'karpathy',NULL,'http://cs.stanford.edu/people/karpathy/','http://cs.stanford.edu/people/karpathy/ https://karpathy.github.io/',NULL)
     ,('Eliezer Yudkowsky','United States','yudkowsky',NULL,NULL,'http://yudkowsky.net/','http://yudkowsky.net/ https://intelligence.org/team/',NULL)
     ,('Paul Christiano','United States',NULL,'paulfchristiano',NULL,'https://paulfchristiano.com','https://paulfchristiano.com/ai/',NULL)
