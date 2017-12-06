ff4f2ce550c80fb1d11d5c3b0f9338bc6de69677
Issa Rice <riceissa@gmail.com>
2017-12-05 16:56:38 -0800

    add

--- sql/positions.sql
+++ sql/positions.sql
@@ -501,4 +501,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Hiroyuki Morikawa','Whole Brain Architecture Initiative','Advisor',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20171206000415/https://wba-initiative.org/en/about/organization/ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3070741',NULL,NULL,NULL,NULL,NULL)
     ,('Hideyuki Nakashima','Whole Brain Architecture Initiative','Advisor',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20171206000415/https://wba-initiative.org/en/about/organization/ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3070741',NULL,NULL,NULL,NULL,NULL)
     ,('Koji Morikawa','Whole Brain Architecture Initiative','Auditor-secretary',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20171206000415/https://wba-initiative.org/en/about/organization/ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3070741',NULL,NULL,NULL,NULL,NULL)
+    ,('Richard Loosemore','Susaro','CEO','2017-05-01','month',NULL,NULL,'https://www.linkedin.com/in/richard-loosemore-47a2164/ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3070741',NULL,NULL,NULL,NULL,NULL)
 ;
