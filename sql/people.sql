a777f3ce7f7adbbd2ea64d14e7d9ff9f602068f5
Issa Rice <riceissa@gmail.com>
2017-12-12 17:17:19 -0800

    add

--- sql/people.sql
+++ sql/people.sql
@@ -77,6 +77,7 @@ insert into people(person, country, facebook_username, github_username, linkedin
     ,('Angela P. Schoellig',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'http://wrai.org/slides/Safe%20Reinforcement%20Learning%20in%20Robotics%20with%20Bayesian%20Models%20-%20Felix%20Berkenkamp.pdf',NULL)
     ,('Andreas Krause',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'http://wrai.org/slides/Safe%20Reinforcement%20Learning%20in%20Robotics%20with%20Bayesian%20Models%20-%20Felix%20Berkenkamp.pdf',NULL)
     ,('Tobias Baumann',NULL,NULL,NULL,NULL,NULL,NULL,'http://tobiasbaumann.me/','http://s-risks.org/focus-areas-of-worst-case-ai-safety/ http://s-risks.org/training-neural-networks-to-detect-suffering/',NULL)
+    ,('Jim O’Neill',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'https://foresight.org/publications/AGI-Timeframes&PolicyWhitePaper.pdf',NULL)
 ;
 
 # Insert the rest of the people by taking names from positions
