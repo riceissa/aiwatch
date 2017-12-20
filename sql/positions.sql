873ed099a06893bc8d3279ffb8e0d684eb74f3ee
Issa Rice <riceissa@gmail.com>
2017-12-19 17:41:55 -0800

    add more relation/type values

--- sql/positions.sql
+++ sql/positions.sql
@@ -41,9 +41,9 @@ create table positions(
     # cannot be sure), and 'unrelated' means the
     # position doesn't have to do with safety (but we include it anyway
     # because we want to track movement in and out of safety).
-    ai_safety_relation enum('position','organization','unrelated'),
+    ai_safety_relation enum('position','agi_organization','gcr_organization','unrelated'),
 
-    position_type enum('technical research','general'),
+    position_type enum('technical research','general','policy','research advisor','general advisor','research associate','board member'),
     notes varchar(2000) default null
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
