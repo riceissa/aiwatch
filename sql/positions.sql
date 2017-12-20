77d0bfd8899417ccf5f37803cad30c07e33ad85f
Issa Rice <riceissa@gmail.com>
2017-12-19 17:51:17 -0800

    comment + more types

--- sql/positions.sql
+++ sql/positions.sql
@@ -2,7 +2,13 @@ create table positions(
     position_id int(11) not null auto_increment primary key,
     person varchar(40) not null,
     organization varchar(200) not null,
+
+    # The title is whatever the organization decides to call it, and
+    # is not normalized.  For a more normalized take on the kind of
+    # position, see the ai_safety_relation and position_type columns
+    # of this same table.
     title varchar(100),
+
     start_date date,
     start_date_precision enum('day','month','year','multi-year'),
 
@@ -43,7 +49,7 @@ create table positions(
     # because we want to track movement in and out of safety).
     ai_safety_relation enum('position','agi_organization','gcr_organization','unrelated'),
 
-    position_type enum('technical research','general','policy','research advisor','general advisor','research associate','board member'),
+    position_type enum('technical research','general','policy','research advisor','general advisor','research associate','board member','undergraduate','graduate','postdoc','intern'),
     notes varchar(2000) default null
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
