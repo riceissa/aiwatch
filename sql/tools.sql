2506c2f70ce345f2ac2d5978b73f9a601aeb619d
Issa Rice <riceissa@gmail.com>
2018-01-13 14:17:24 -0800

    add more

--- sql/tools.sql
+++ sql/tools.sql
@@ -4,10 +4,15 @@ create table tools(
     creator varchar(40),
     creator_type enum('person','organization'),
     creation_date date,
+    creation_date_precision enum('day','month','year','multi-year'),
     url varchar(200),
     description varchar(2000)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into tools(tool_name, creator, creator_type, creation_date, url, description) values
-    ('AI Watch','Issa Rice','person','2017-10-23','https://aiwatch.issarice.com/','A website to track people and organizations working on AI safety.')
+insert into tools(tool_name, creator, creator_type, creation_date, creation_date_precision, url, description) values
+    ('AI Watch','Issa Rice','person','2017-10-23','day','https://aiwatch.issarice.com/','A website to track people and organizations working on AI safety.')
+    ,('The Uncertain Future','Machine Intelligence Research Institute','organization',NULL,NULL,'http://theuncertainfuture.com/','A tool to model future technology and its effect on civilization.')
+    ,('AGI Failures Modes and Levels map','Alexey Turchin','person','2015-01-01','year','http://immortality-roadmap.com/AIfails.pdf','A flowchart about failure modes of artificial general intelligence, grouped by the stage of development. There is an accompanying post on LessWrong at http://lesswrong.com/lw/mgf/a_map_agi_failures_modes_and_levels/')
+    ,('Road to AI Safety Excellence','Toon Alfrink','person',NULL,NULL,'https://wiki.lesswrong.com/wiki/Road_to_AI_Safety_Excellence','A proposed course that is designed to produce AI safety researchers.')
+    ,('Map of the AI Safety Community','Søren Elverlin','person','2017-09-26','day','https://aisafety.com/2017/09/26/map-ai-safety-community/','A pictorial map that lists organizations and individuals in the AI safety community.')
 ;
