f4f48168e2cdde50ec171d6c25b20fc6019b05b1
Issa Rice <riceissa@gmail.com>
2018-06-13 20:15:49 -0700

    add new page

---
+++ access-portal/upcoming-people/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+  <meta name="dcterms.date" content="2018-06-13">
+  <meta property="og:title" content="Upcoming AI safety people" />
+  <meta property="og:site_name" content="AI Watch" />
+  <meta property="og:locale" content="en_US" />
+  <title>Upcoming AI safety people - AI Watch</title>
+  <link rel="stylesheet" href="/tablesorter.css">
+  <script src="/jquery.min.js"></script>
+  <script src="/jquery.tablesorter.js"></script>
+  <script src="/anchor.min.js"></script>
+<?php include_once("../style.inc"); ?>
+<?php include_once("../analytics.inc"); ?>
+</head>
+<body>
+<nav>
+  <a href="/">Home</a>,
+  <a href="/about/">About</a>,
+  <a href="/compare.php">Compare</a>
+</nav>
+
+<h1>Upcoming AI safety people</h1>
+
+<p>This page tracks people who are currently training to work on AI
+  safety, but have not held positions at organizations or published
+  work. The idea is to stay "one step ahead" by knowing which people
+  will likely enter the field.</p>
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Type of position</th>
+      <th>Source</th>
+      <th>Notes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>David Kristoffersson</td>
+      <td>Technical research?</td>
+      <td><a href="https://www.greaterwrong.com/posts/FvA2qL6ChCbyi5Axk/book-review-naive-set-theory-miri-research-guide">Source</a></td>
+      <td>He might already hold a position, seeing that his post is from 2015.</td>
+    </tr>
+    <tr>
+      <td>elriggs</td>
+      <td>Technical research?</td>
+      <td><a href="https://www.greaterwrong.com/posts/iHuMFnHHhtehTBYEY/saving-the-world-in-80-days-prologue">Source</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>Andrew Quinn</td>
+      <td>Technical research?</td>
+      <td><a href="https://www.greaterwrong.com/posts/8vBSgToXryKGncMyk/math-towards-proof-writing-as-a-skill-in-itself">Source</a></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><a href="https://www.greaterwrong.com/posts/eAsN5vNjvmxzACuuX/swimming-upstream-a-case-study-in-instrumental-rationality">Alexander Matt Turner</a> (TurnTrout)</td>
+      <td>Technical research</td>
+      <td><a href="https://www.greaterwrong.com/posts/EvKWNRkJgLosgRDSa/lightness-and-unease">Source</a></td>
+      <td><a href="https://github.com/alexander-turner">GitHub account</a>
+      </td>
+  </tbody>
+</table>
+
+<script>
+    $(function(){$("table").tablesorter();});
+    anchors.add();
+</script>
+</body>
+</html>
