3afed0dc97d559c1ea2941cf2a1ead8dcc783e66
Issa Rice <riceissa@gmail.com>
2018-06-20 13:32:49 -0700

    clean up

--- scripts/open_phil_team.py
+++ scripts/open_phil_team.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import pdb
-
 import csv
 import requests
 import sys
@@ -103,7 +101,7 @@ def main():
         # Sometimes the Internet Archive moves forward or backward to a different
         # snapshot than the one requested, so we need to check r.url to see which
         # one we got.
-        snap_id = r.url.split('/')[4][:8]
+        snap_id = r.url.split('/')[4][:8]  # This is the YYYYMMDD part
         snap_date = snap_id[:4] + "-" + snap_id[4:6] + "-" + snap_id[6:]
 
         for role_tag in soup.find_all("div", {"class": "company-role"}):
