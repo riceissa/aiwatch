1e702a671c0a23d5844280c507045e14de9a839c
Issa Rice <riceissa@gmail.com>
2018-06-19 18:17:08 -0700

    add comment

--- scripts/open_phil_team.py
+++ scripts/open_phil_team.py
@@ -7,6 +7,11 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
+# After running this script I made some small manual changes to the resulting
+# CSV:
+#   - There were some mostly blank lines that I deleted.
+#   - One of the role names was in all-caps so I title-cased it.
+
 URLS = [
         "https://web.archive.org/web/20160111014138/http://www.openphilanthropy.org:80/about/team",
         "https://web.archive.org/web/20160112052008/http://www.openphilanthropy.org:80/about/team",
