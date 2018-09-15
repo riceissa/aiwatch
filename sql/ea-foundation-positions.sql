0ce1986620fb92162dd55a4d638661393399bed3
Issa Rice <riceissa@gmail.com>
2018-09-15 15:58:07 -0700

    start on EAF

---
+++ sql/ea-foundation-positions.sql
@@ -0,0 +1,18 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ('Adriano Mannino','Effective Altruism Foundation','President',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Lucius Caviola','Effective Altruism Foundation','Leader',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Jonas Vollmer','Effective Altruism Foundation','Head of operations',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Jonathan Erhardt','Effective Altruism Foundation','Scientific assistant',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Thomas Metzinger','Effective Altruism Foundation','Scientific advisor',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Thomas Metzinger','Effective Altruism Foundation','Board member',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'board member','Effective altruism')
+    ,('Tobias Pulver','Effective Altruism Foundation','Head of outreach',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Ewelina Tur','Effective Altruism Foundation','Scientific Associate',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Julienne Wenger','Effective Altruism Foundation','Illustration and catering',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Patrick Germann','Effective Altruism Foundation','Communications',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Daniel Kestenholz','Effective Altruism Foundation','Business operations',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Natalie Cargill','Effective Altruism Foundation','Scientific Associate',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'volunteer','Effective altruism')
+    ,('Charlotte Blattner','Effective Altruism Foundation','Scientific Associate',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'volunteer','Effective altruism')
+    ,('Corina Fuhrer','Effective Altruism Foundation','Legal advisor',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'volunteer','Effective altruism')
+    ,('Kaspar Etter','Effective Altruism Foundation','Consultant',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'volunteer','Effective altruism')
+    ,('Massimo Mannino','Effective Altruism Foundation','Consultant',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,'volunteer','Effective altruism')
+;
