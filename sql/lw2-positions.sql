fa00d3f25bba4a622aa518fdfee700b80dd27c3c
Issa Rice <riceissa@gmail.com>
2018-10-14 13:08:32 -0700

    add more

--- sql/lw2-positions.sql
+++ sql/lw2-positions.sql
@@ -1,6 +1,8 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-    ('Oliver Habryka','LessWrong 2.0','Developer','2017-06-01','month',NULL,NULL,'https://github.com/riceissa/aiwatch/issues/8#issue-369865886 https://www.lesswrong.com/about',NULL,'position','software engineering','full-time','AI safety')
-    ,('Matthew Graves','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about',NULL,'position',NULL,NULL,'AI safety')
-    ,('Ben Albert Pace','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about',NULL,'position',NULL,NULL,'AI safety')
-    ,('Raymond Arnold','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about',NULL,'position',NULL,NULL,'AI safety')
+    ('Oliver Habryka','LessWrong 2.0','Developer','2017-06-01','month',NULL,NULL,'https://github.com/riceissa/aiwatch/issues/8#issue-369865886 https://www.lesswrong.com/about https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20',NULL,'position','software engineering','full-time','AI safety')
+    ,('Matthew Graves','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20 https://www.lesswrong.com/posts/rEHLk9nC5TtrNoAKT/lw-2-0-strategic-overview',NULL,'position',NULL,NULL,'AI safety')
+    ,('Ben Albert Pace','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20 https://www.lesswrong.com/posts/rEHLk9nC5TtrNoAKT/lw-2-0-strategic-overview',NULL,'position',NULL,NULL,'AI safety')
+    ,('Raymond Arnold','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/about https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20 https://www.lesswrong.com/posts/rEHLk9nC5TtrNoAKT/lw-2-0-strategic-overview',NULL,'position',NULL,NULL,'AI safety')
+    ,('Harmanas Chopra','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20 https://www.lesswrong.com/posts/rEHLk9nC5TtrNoAKT/lw-2-0-strategic-overview',NULL,'position',NULL,NULL,'AI safety')
+    ,('Eric Rogstad','LessWrong 2.0',NULL,NULL,NULL,NULL,NULL,'https://www.lesswrong.com/posts/bxctncS8qcgeCEMGN/lw-2-0-open-beta-starts-9-20 https://www.lesswrong.com/posts/rEHLk9nC5TtrNoAKT/lw-2-0-strategic-overview',NULL,'position',NULL,NULL,'AI safety')
 ;
