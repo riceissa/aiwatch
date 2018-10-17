0d0789767b6f65205187f81ecfbc5006768a4ee4
Issa Rice <riceissa@gmail.com>
2018-10-17 16:37:01 -0700

    add

---
+++ sql/cfar-positions.sql
@@ -0,0 +1,2 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+;
