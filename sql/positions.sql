7cb816714c4d96bfc2ae8cd581ded0bf3175da4f
Issa Rice <riceissa@gmail.com>
2018-10-21 18:06:38 -0700

    add

--- sql/positions.sql
+++ sql/positions.sql
@@ -815,4 +815,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('David Simmons','Machine Intelligence Research Institute','Intern','2018-06-01','year','2018-09-01','year','https://intelligence.org/workshops/#july-2018',NULL,'position','technical research','intern','AI safety')
     ,('Alex Zhu','Machine Intelligence Research Institute','Intern','2018-06-01','year','2018-09-01','year','https://intelligence.org/workshops/#july-2018',NULL,'position','technical research','intern','AI safety')
     ,('Sören Mindermann','Future of Humanity Institute',NULL,NULL,NULL,NULL,NULL,'https://arxiv.org/pdf/1712.05812.pdf',NULL,'position','technical research',NULL,'AI safety')
+    ,('Angela P.', NULL, 'Executive Assistant', '2018-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/angela-p-3949325a/', NULL, 'position', NULL, NULL, 'AI safety')
 ;
