3c9280cf25b80877285e94f32d0db7ae1f21d122
Vipul Naik <vipulnaik1@gmail.com>
2019-03-24 19:26:26 +0000

    Add three org docs for CEA

---
+++ sql/cea-org-docs.sql
@@ -0,0 +1,41 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+    (
+        'https://www.centreforeffectivealtruism.org/blog/announcing-a-change-of-leadership-at-cea/', /* url */
+        'Announcing a change of leadership at CEA', /* title */
+        '2019-02-08', /* publication_date */
+        '2019-02-27', /* modified_date */
+        NULL, /* author */
+        'Centre for Effective Altruism', /* publisher */
+        'Centre for Effective Altruism', /* affected_organizations */
+        'Larissa Hesketh-Rowe', /* affected_people */
+        'Employee departure', /* document_scope */
+        'Effective altruism/movement growth', /* cause_area */
+        'The post announces that Larissa Hesketh-Rowe is stepping down as CEO of CEA, and says that she was asked to resign because she was unable to meet the performance expectations for the role' /* notes */
+    )
+    ,(
+        'https://www.facebook.com/groups/effective.altruists/permalink/2160126377377041/', /* url */
+        'Does anyone know what happened at CEA regarding the leadership change?', /* title */
+        '2019-02-26', /* publication_date */
+        NULL, /* modified_date */
+        'Milan Griffes', /* author */
+        'Effective Altruism Facebook group', /* publisher */
+        'Centre for Effective Altruism', /* affected_organizations */
+        'Larissa Hesketh-Rowe', /* affected_people */
+        'Employee departure', /* document_scope */
+        'Effective altruism/movement growth', /* cause_area */
+        'Milan Griffes seeks more background for the public announcement of the departure of CEA CEO Larissa Hesketh-Rowe https://www.centreforeffectivealtruism.org/blog/announcing-a-change-of-leadership-at-cea/ In the comments, Sam Deere of CEA says "We don''t provide specific details when staff depart the organisation." Also, others in the comments say that the announcement by CEA was unprofessional in many ways. Relatedly, Milan notes in the comments that Kerry Vaughan also appears to have left CEA' /* notes */
+    )
+    ,(
+        'https://www.jefftk.com/p/simultaneous-shortage-and-oversupply#fb-979079255202_979254219572', /* url */
+        'I took a significant pay cut to work as a software engineer for CEA', /* title */
+        '2019-01-27', /* publication_date */
+        NULL, /* modified_date */
+        'Sarah Spikes', /* author */
+        'Jeff Kaufman', /* publisher */
+        'Centre for Effective Altruism', /* affected_organizations */
+        'Sarah Spikes|Sam Deere', /* affected_people */
+        'Job experience', /* document_scope */
+        'Effective altruism/movement growth', /* cause_area */
+        'In this comment on a post by Jeff Kaufman, Sarah Spikes describes her experience working for CEA, which she finds disappointing in various ways, due to the lack of recognition for her experience and the cancellation of the projects she was working on, without explanation. A later comment also notes her salary of $84,000. Original thread on Facebook: https://www.facebook.com/jefftk/posts/979079255202?comment_id=979254219572&comment_tracking=%7B%22tn%22%3A%22R%22%7D' /* notes */
+    )
+;
