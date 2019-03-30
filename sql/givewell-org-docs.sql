c5df28e16bcbe1f6f02a5318184d072b618a691c
Vipul Naik <vipulnaik1@gmail.com>
2019-03-30 11:50:17 -0700

    Add five docs about what it is like to work at GiveWell

---
+++ sql/givewell-org-docs.sql
@@ -0,0 +1,67 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+   (
+        'https://80000hours.org/2013/11/thoughts-on-my-experience-working-at-givewell/', /* url */
+        'Thoughts on my experience working at GiveWell', /* title */
+        '2013-11-04', /* publication_date */
+        NULL, /* modified_date */
+        'Nick Beckstead', /* author */
+        '80,000 Hours', /* publisher */
+        'GiveWell', /* affected_organizations */
+        'Nick Beckstead', /* affected_people */
+        'Job experience', /* document_scope */
+        'Charity evaluator', /* cause_area */
+        'Nick Beckstead describes his experience working at GiveWell for two months for a trial work period. He describes his work as involving literature searches, literature evaluation, and cost-effectiveness models. He describes GiveWell as having a direct feedback culture and less autonomy than academia, though he says that the latter was not a problem for him because he and his manager were in synch. He also notes that GiveWell is strong in terms of personal development and the impact of the work, and he personally also found the job satisfying. Ultimately, Beckstead chose to work at the Future of Humanity Institute instead, but recommends to people interested to try a trial work period at GiveWell' /* notes */
+    )
+    ,(
+        'https://www.benkuhn.net/gw', /* url */
+        'What I learned from working at GiveWell', /* title */
+        '2014-02-01', /* publication_date */
+        NULL, /* modified_date */
+        'Ben Kuhn', /* author */
+        NULL, /* publisher */
+        'GiveWell', /* affected_organizations */
+        'Ben Kuhn', /* affected_people */
+        'Job experience', /* document_scope */
+        'Charity evaluator', /* cause_area */
+        'Ben Kuhn describes his experience working at GiveWell for four weeks. He describes his main takeaways as: be more skeptical, public health is really hard, GiveWell takes a broader view than he previously thought, things depends ridiculously on the details, and research is harder than he thought' /* notes */
+    )
+    ,(
+        'https://80000hours.org/2016/08/reflections-from-a-givewell-employee/', /* url */
+        'Should you work at GiveWell? Reflections from a recent employee.', /* title */
+        '2016-08-15', /* publication_date */
+        NULL, /* modified_date */
+        'Milan Griffes', /* author */
+        '80,000 Hours', /* publisher */
+        'GiveWell', /* affected_organizations */
+        'Milan Griffes', /* affected_people */
+        'Job experience', /* document_scope */
+        'Charity evaluator', /* cause_area */
+        'Milan Griffes, who worked at GiveWell from August 2014 to May 2016, describes his experience working for the organization. He lists the following positives: worldview building, professional skills, networking within the effective altruism community, and humanitarian impact. He lists the following as things GiveWell does well as an employer: flexible working schedule, access to co-founders and senior staff, receptiveness to staff input, clear communication about performance and trajectory, and hard-working culture. He lists the following areas to improve upon: top-down decision making, time tracking, performance evaluation, team building, and lack of explicit emphasis on self-care. The original post is on the author''s personal blog https://flightfromperfection.com/givewell-reflection.html' /* notes */
+    )
+    ,(
+        'https://80000hours.org/podcast/episodes/james-snowden-givewell-research/', /* url */
+        'Finding the best charity requires estimating the unknowable. Here’s how GiveWell tries to do that, according to researcher James Snowden.', /* title */
+        '2018-07-16', /* publication_date */
+        NULL, /* modified_date */
+        'James Snowden', /* author */
+        '80,000 Hours', /* publisher */
+        'GiveWell', /* affected_organizations */
+        'James Snowden', /* affected_people */
+        'Job experience', /* document_scope */
+        'Charity evaluator', /* cause_area */
+        'James Snowden, a research consultant at GiveWell, is interviewed for the 80,000 Hours podcast by Robert Wiblin. A summary and full transcript are available at the link. The podcast is primarily focused on the way GiveWell does research and makes decisions. However, the part starting from "If someone was listening and they were considering working at GiveWell, what are the most satisfying things about working at GiveWell and what are some things that you wish you could change if you could?" discusses the experience of working at GiveWell. Snowden mentions a focused and quiet office culture but a vibrant Slack culture, much less formal intra-company communication than public-facing communication, but a lot of seriousness in the work environment. Snowden encourages people with skillsets matching any of GiveWell''s job postings to apply to GiveWell' /* notes */
+    )
+    ,(
+        'https://blog.givewell.org/2019/03/07/what-is-it-like-to-work-at-givewell/', /* url */
+        'What is it like to work at GiveWell?', /* title */
+        '2019-03-07', /* publication_date */
+        NULL, /* modified_date */
+        'James Snowden', /* author */
+        'GiveWell', /* publisher */
+        'GiveWell', /* affected_organizations */
+        'James Snowden', /* affected_people */
+        'Job experience', /* document_scope */
+        'Charity evaluator', /* cause_area */
+        'James Snowden, a research consultant at GiveWell, explains why he started working with GiveWell, describes why he thinks it is a great place to work at, and adds notes on GiveWell''s job application process. His reasons for GiveWell being a great place to work at are: the work helps people a lot, the work is intellectually stimulating, the work was something he was suited for (though this may not apply to others), the co-workers were excellent (both competent and thoughtful), and he was able to manage working remotely. The post was solicited by GiveWell in an effort to help GiveWell with its team expansion efforts' /* notes */
+    )
+ ;
