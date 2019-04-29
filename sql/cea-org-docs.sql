ddedd58168e6f34d63ff1b3b76de8a6ae72a6bee
Issa Rice <riceissa@gmail.com>
2019-04-29 16:25:27 +0100

    add oliver habryka comment

--- sql/cea-org-docs.sql
+++ sql/cea-org-docs.sql
@@ -38,4 +38,17 @@ insert into organization_documents(url, title, publication_date, modified_date, 
         'Effective altruism/movement growth', /* cause_area */
         'In this comment on a post by Jeff Kaufman, Sarah Spikes describes her experience working for CEA, which she finds disappointing in various ways, due to the lack of recognition for her experience and the cancellation of the projects she was working on, without explanation. A later comment also notes her salary of $84,000. Original thread on Facebook: https://www.facebook.com/jefftk/posts/979079255202?comment_id=979254219572&comment_tracking=%7B%22tn%22%3A%22R%22%7D' /* notes */
     )
+    ,(
+        'https://www.facebook.com/robbensinger/posts/10157945186060447?comment_id=10157951341920447&reply_comment_id=10157956601780447&comment_tracking=%7B%22tn%22%3A%22R%22%7D', /* url */
+        'Since I am mentioned in person in this comment …', /* title */
+        '2017-01-15', /* publication_date */
+        NULL, /* modified_date */
+        'Oliver Habryka', /* author */
+        'Facebook', /* publisher */
+        'Centre for Effective Altruism', /* affected_organizations */
+        'Oliver Habryka', /* affected_people */
+        NULL, /* document_scope */
+        'Movement building', /* cause_area */
+        'In response to an anonymous comment saying in part “I feel like a lot of skilled people are now wasting their time on EA (e.g., Oliver Habryka), many of whom would otherwise be working on issues more directly related to AGI”, Oliver Habryka responds that he is no longer working in effective altruist movement building and that he almost certainly should not have spent two years working in the area.' /* notes */
+    )
 ;
