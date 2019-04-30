229adb239ac82b528a077a0f938ac58f8de2c481
Issa Rice <riceissa@gmail.com>
2019-04-30 11:20:55 +0100

    add intersect view

---
+++ access-portal/intersect.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once("backend/globalVariables/passwordFile.inc");
+$head_date = trim(file_get_contents("head_date.txt"));
+$title = "";
+
+if (($_REQUEST['person1'] ?? '') and ($_REQUEST['person1'] ?? '')) {
+  $person1 = $_REQUEST['person1'];
+  $person2 = $_REQUEST['person2'];
+} else {
+  $person1 = "";
+  $person2 = "";
+}
+
+if (($_REQUEST['organization1'] ?? '') and ($_REQUEST['organization2'] ?? '')) {
+  $organization1 = $_REQUEST['organization1'];
+  $organization2 = $_REQUEST['organization2'];
+} else {
+  $organization1 = "";
+  $organization2 = "";
+}
+
+$site_name = $subdomain === "aiwatch" ? "AI Watch" : "Org Watch";
+?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+  <meta name="dcterms.date" content="<?= $head_date ?>">
+  <meta property="og:title" content="<?= $title ? $title : $site_name ?>" />
+  <meta property="og:site_name" content="<?= $site_name ?>" />
+  <meta property="og:locale" content="en_US" />
+  <title><?= $title ? $title . " - " . $site_name : $site_name ?></title>
+  <link rel="stylesheet" href="/tablesorter.css">
+  <script src="/jquery.min.js"></script>
+  <script src="/jquery.tablesorter.js"></script>
+  <script src="/anchor.min.js"></script>
+<?php include_once("style.inc"); ?>
+<?php include_once("analytics.inc"); ?>
+</head>
+<body>
+<?php include_once("navbar.inc"); ?>
+<?php
+include_once("backend/util.inc");
+
+if ($person1 && $person2) {
+  include_once("backend/intersect_people.inc");
+} else if ($organization1 && $organization2) {
+  include_once("backend/intersect_organizations.inc");
+}
+?>
