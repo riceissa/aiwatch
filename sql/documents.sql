709fec57334f941f34740481b096421bc5e74aaa
Issa Rice <riceissa@gmail.com>
2019-05-17 12:24:08 +0200

    reorder to match organization_documents schema

--- sql/documents.sql
+++ sql/documents.sql
@@ -1,15 +1,16 @@
 create table documents(
     document_id int(11) not null auto_increment primary key,
-    title varchar(100) not null unique,
     url varchar(400) not null unique,
-    document_type enum('blog post','wiki page','paper')
-    affected_organizations varchar(1000),
-    affected_people varchar(2000),
-    affected_agendas varchar(1000),
+    title varchar(100) not null unique,
     publication_date date,
     publication_date_precision enum('day','month','year','multi-year'),
     modified_date date,
     modified_date_precision enum('day','month','year','multi-year'),
+    author varchar(200),
+    document_type enum('blog post','wiki page','paper')
+    affected_organizations varchar(1000),
+    affected_people varchar(2000),
+    affected_agendas varchar(1000),
     publisher varchar(40),
     notes varchar(2000)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
