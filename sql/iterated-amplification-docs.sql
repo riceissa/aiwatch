0712b9d7f166680571a077a97f4ba6953fa98e99
Issa Rice <riceissa@gmail.com>
2019-05-17 12:23:50 +0200

    add

---
+++ sql/iterated-amplification-docs.sql
@@ -0,0 +1,17 @@
+insert into documents(url, title, publication_date, publication_date_precision, modified_date, modified_date_precision, author, document_type, affected_organizations, affected_people, affected_agendas, publisher, notes) values
+  (
+    'https://intelligence.org/2018/05/19/challenges-to-christianos-capability-amplification-proposal/', /* url */
+    'Challenges to Christiano’s capability amplification proposal', /* title */
+    '2018-05-19', /* publication_date */
+    'day', /* publication_date_precision */
+    NULL, /* modified_date */
+    NULL, /* modified_date_precision */
+    'Eliezer Yudkowsky', /* author */
+    'blog post', /* document_type */
+    NULL, /* affected_organizations */
+    'Paul Christiano', /* affected_people */
+    'Iterated amplification', /* affected_agendas */
+    'Machine Intelligence Research Institute', /* publisher */
+    'This post was summarized in Alignment Newsletter #7 <https://www.alignmentforum.org/posts/4ap6WQx52txzsJx6r/the-alignment-newsletter-7-05-21-18>.' /* notes */
+  )
+;
