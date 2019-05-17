bd39717cfba76b3a681dda7273e6047da9c825f7
Issa Rice <riceissa@gmail.com>
2019-05-17 10:55:43 +0200

    add more fields

--- sql/documents.sql
+++ sql/documents.sql
@@ -3,6 +3,9 @@ create table documents(
     title varchar(100) not null unique,
     url varchar(400) not null unique,
     document_type enum('blog post','wiki page','paper')
+    affected_organizations varchar(1000),
+    affected_people varchar(2000),
+    affected_agendas varchar(1000),
     publication_date date,
     publication_date_precision enum('day','month','year','multi-year'),
     modified_date date,
