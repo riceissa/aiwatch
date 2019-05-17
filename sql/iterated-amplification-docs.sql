ebbe6109d7017af7dfc017b0da5044b81b45ff7b
Issa Rice <riceissa@gmail.com>
2019-05-17 13:02:05 +0200

    add more docs

--- sql/iterated-amplification-docs.sql
+++ sql/iterated-amplification-docs.sql
@@ -14,4 +14,34 @@ insert into documents(url, title, publication_date, publication_date_precision, 
     'Machine Intelligence Research Institute', /* publisher */
     'This post was summarized in Alignment Newsletter #7 <https://www.alignmentforum.org/posts/4ap6WQx52txzsJx6r/the-alignment-newsletter-7-05-21-18>.' /* notes */
   )
+  ,(
+    'https://futureoflife.org/2019/04/11/an-overview-of-technical-ai-alignment-with-rohin-shah-part-1/', /* url */
+    'AI Alignment Podcast: An Overview of Technical AI Alignment with Rohin Shah (Part 1)', /* title */
+    '2019-04-11', /* publication_date */
+    'day', /* publication_date_precision */
+    NULL, /* modified_date */
+    NULL, /* modified_date_precision */
+    'Lucas Perry', /* author */
+    'interview', /* document_type */
+    NULL, /* affected_organizations */
+    'Rohin Shah', /* affected_people */
+    '', /* affected_agendas */
+    'Future of Life Institute', /* publisher */
+    'Part one of an interview with Rohin Shah that goes covers some technical agendas for AI alignment.' /* notes */
+  )
+  ,(
+    'https://futureoflife.org/2019/04/25/an-overview-of-technical-ai-alignment-with-rohin-shah-part-2/', /* url */
+    'AI Alignment Podcast: An Overview of Technical AI Alignment with Rohin Shah (Part 2)', /* title */
+    '2019-04-25', /* publication_date */
+    'day', /* publication_date_precision */
+    NULL, /* modified_date */
+    NULL, /* modified_date_precision */
+    'Lucas Perry', /* author */
+    'interview', /* document_type */
+    NULL, /* affected_organizations */
+    'Rohin Shah|Dylan Hadfield-Menell|Gillian Hadfield', /* affected_people */
+    'Embedded agency|Cooperative inverse reinforcement learning|inverse reinforcement learning|deep reinforcement learning from human preferences|recursive reward modeling', /* affected_agendas */
+    'Future of Life Institute', /* publisher */
+    'Part two of a podcast episode that goes into details about some technical approaches to AI alignment.' /* notes */
+  )
 ;
