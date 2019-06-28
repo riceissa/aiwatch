14094f1d99fe72eb6ce856f54672d12b34e8f4bb
Issa Rice <riceissa@gmail.com>
2019-06-28 15:18:08 -0700

    add paisri

---
+++ sql/positions/paisri-positions.sql
@@ -0,0 +1,3 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+  ('G Gordon Worley III','Phenomenological AI Safety Research Institute','Director of Research',NULL,NULL,NULL,NULL,'http://web.archive.org/web/20190428041905/https://paisri.org/people.html',NULL,'position',NULL,NULL,'AI safety')
+;
