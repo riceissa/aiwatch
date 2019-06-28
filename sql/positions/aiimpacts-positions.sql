0082506bd93ec6f9197157860bedc5e1a3cf750b
Issa Rice <riceissa@gmail.com>
2019-06-28 15:27:29 -0700

    syntax

--- sql/positions/aiimpacts-positions.sql
+++ sql/positions/aiimpacts-positions.sql
@@ -1,5 +1,5 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-    ,('Katja Grace','Machine Intelligence Research Institute','Researcher, AI Impacts','2016-03-01','month',NULL,NULL,'https://web.archive.org/web/20160301205029/http://intelligence.org:80/team/',NULL,'position',NULL,NULL,'AI safety')
+    ('Katja Grace','Machine Intelligence Research Institute','Researcher, AI Impacts','2016-03-01','month',NULL,NULL,'https://web.archive.org/web/20160301205029/http://intelligence.org:80/team/',NULL,'position',NULL,NULL,'AI safety')
     ,('Katja Grace','AI Impacts',NULL,NULL,NULL,NULL,NULL,'https://aiimpacts.org/about/ https://futureoflife.org/first-ai-grant-recipients/',NULL,'position','background',NULL,'AI safety')
     ,('Michael Wulfsohn','AI Impacts',NULL,NULL,NULL,NULL,NULL,'https://aiimpacts.org/about/',NULL,'position','background',NULL,'AI safety')
     ,('Finan Adamson','AI Impacts',NULL,NULL,NULL,NULL,NULL,'https://aiimpacts.org/about/',NULL,'position','background',NULL,'AI safety')
