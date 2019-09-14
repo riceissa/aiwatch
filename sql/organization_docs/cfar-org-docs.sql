9803262089f0ac5aaa331f66982fd71862912843
Vipul Naik <vipulnaik1@gmail.com>
2019-09-14 11:26:34 -0700

    Add two Duncan posts clarifying his exit from Center for Applied Rationality and implications for the organization

--- sql/organization_docs/cfar-org-docs.sql
+++ sql/organization_docs/cfar-org-docs.sql
@@ -37,6 +37,32 @@ insert into organization_documents(url, title, publication_date, modified_date, 
         'Third-party commentary on organization', /* document_scope */
         'Rationality improvement', /* cause_area */
         'In a writeup explaining the $150,000 grant made from the Effective Altruism Funds'' Long Term Future Fund to the Center for Applied Rationality, Oliver Habryka comments on the organization''s personnel and financial issues. He notes that key people Julia Galef, Andrew Critch, Kenzi Ashkie, and Duncan Sabien have left and/or reduced their involvement, and Anna Salamon seems less involved in some respects. He also talks about how CFAR decided not to run a fundraiser in 2018 because they felt it would be in bad taste after the Brent Dill controversy, and how this leaves them more cash-strapped now.' /* notes */
-    )    
+    )
+    ,(
+        'https://www.facebook.com/duncan.sabien/posts/2483881418313227', /* url */
+        'Hey, everyone, just a quick status update: my part-time professional involvement with CFAR has now gone to zero.', /* title */
+        '2019-04-21', /* publication_date */
+        NULL, /* modified_date */
+        'Duncan Sabien', /* author */
+        NULL, /* publisher */
+        'Center for Applied Rationality', /* affected_organizations */
+        'Duncan Sabien', /* affected_people */
+        'Employee departure', /* document_scope */
+        'Rationality improvement', /* cause_area */
+        'Duncan Sabien played a key role at the Center for Applied Rationality in conducting workshops and shaping the curriculum till his departure in late 2018. After departure, Sabien continued to help a little bit with CFAR workshops. In this April 2019 Facebook post, he confirms that he will no longer be professionally involved with CFAR.' /* notes */
+    )
+    ,(
+        'https://www.facebook.com/duncan.sabien/posts/2519421394759229', /* url */
+        'I predict that the Center for Applied Rationality of 2019 and 2020 and beyond will be not-at-all "Duncan shaped."', /* title */
+        '2019-05-12', /* publication_date */
+        NULL, /* modified_date */
+        'Duncan Sabien', /* author */
+        NULL, /* publisher */
+        'Center for Applied Rationality', /* affected_organizations */
+        'Duncan Sabien|Timothy Telleen-Lawton|Anna Salamon|Kenzi Ashkie', /* affected_people */
+        'Employee departure', /* document_scope */
+        'Rationality improvement', /* cause_area */
+        'Duncan Sabien played a key role at the Center for Applied Rationality in conducting workshops and shaping the curriculum till his departure in late 2018 and the complete ending of his work with CFAR in April 2019 https://www.facebook.com/duncan.sabien/posts/2483881418313227 He explains that he believes that the new CFAR, under Tim Telleen-Lawton, will be quite different fom the CFAR where Duncan played a key role, partly due to a deliberate attempt by Tim to explore new directions. He also mentions the limited amount of knowledge transfer time (10 hours) from him to the new team, as well as the departure of Val and Anna Salamon switching to part-time.' /* notes */
+    )
 ;
 
