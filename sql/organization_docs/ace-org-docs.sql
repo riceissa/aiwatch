fa7cf71e133a7b9da196c24d4f2a1d1dfffd3aa6
Vipul Naik <vipulnaik1@gmail.com>
2019-11-10 10:31:27 -0800

    Add some org docs from Animal Charity Evaluators; also update Makefile and org docs schema

---
+++ sql/organization_docs/ace-org-docs.sql
@@ -0,0 +1,41 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+    (
+        'https://animalcharityevaluators.org/blog/roundtable-what-can-animal-charities-do-to-promote-healthy-work-cultures/', /* url */
+        'Roundtable: what can animal charities do to promote healthy work cultures?', /* title */
+        '2018-04-17', /* publication_date */
+        NULL, /* modified_date */
+        'Toni Adleberg', /* author */
+        'Animal Charity Evaluators', /* publisher */
+        'Encompass|Animal Charity Evaluators|The Good Food Institute|Compassion in World Farming USA', /* affected_organizations */
+        'Aryenish Birdie|Sofia Davis-Fogel|Cameron Meyer Shorb|David Soleil', /* affected_people */
+        'General discussion of organizational practices', /* document_scope */
+        'Animal welfare', /* cause_area */
+        'The blog post is a roundtable between people in animal welfare organizations. It is mainly focused on what the organizations can do to promote healthy work cultures. Common themes emphasized by multiple participants include: encouraging and listening to feedback, work-life balance, clarity of policies, and the importance of investing in culture.' /* notes */
+    )
+    ,(
+        'https://animalcharityevaluators.org/blog/roundtable-how-can-remote-organizations-effectively-hire-and-onboard-new-employees/', /* url */
+        'Roundtable: how can remote organizations effectively hire and onboard new employees?', /* title */
+        '2019-09-28', /* publication_date */
+        NULL, /* modified_date */
+        'Cash Callaghan', /* author */
+        'Animal Charity Evaluators', /* publisher */
+        'The Humane League|Centre for Effective Altruism|Animal Equality|Rethink Charity|Animal Charity Evaluators|ProVeg International', /* affected_organizations */
+        'Stephanie Frankle|Julia Wise|Antonia Vitale|Baxter Bullock|Leah Edgerton', /* affected_people */
+        'General discussion of organizational practices', /* document_scope */
+        'AI safety|Animal welfare', /* cause_area */
+        'The blog post is a roundtable between people working in executive, operational, and community health roles at organizations in the animal welfare and effective altruism spaces. It is mainly focused on how organizations with remote employees can effectively hire and onboard new employees. Some of the organizations are completely remote (they have no central office) whereas others have both on-site and remote employees. Common themes emphasized by multiple participants include: using "culture add" and not just culture fit (to reduce risk of homogeneity), frequent check-ins, in-person meetings where feasible, and culture orientation for new employees (either an explicit orientation session, or through Slack and check-in meetings).' /* notes */
+    )
+    ,(
+        'https://animalcharityevaluators.org/blog/roundtable-how-to-maintain-work-life-balance-and-how-to-identify-signs-of-a-healthy-remote-team/', /* url */
+        'Roundtable: how can remote employees maintain a healthy work-life balance?', /* title */
+        '2019-10-26', /* publication_date */
+        NULL, /* modified_date */
+        'Cash Callaghan', /* author */
+        'Animal Charity Evaluators', /* publisher */
+        'The Humane League|Centre for Effective Altruism|Animal Equality|Rethink Charity|Animal Charity Evaluators|ProVeg International', /* affected_organizations */
+        'Stephanie Frankle|Julia Wise|Antonia Vitale|Baxter Bullock|Leah Edgerton', /* affected_people */
+        'General discussion of organizational practices', /* document_scope */
+        'AI safety|Animal welfare', /* cause_area */
+        'The blog post is a roundtable between people working in executive, operational, and community health roles at organizations in the animal welfare and effective altruism spaces. It is mainly focused on how organizations with remote employees can maintain a healthy work-life balance. Some of the organizations are completely remote (they have no central office) whereas others have both on-site and remote employees. Common themes emphasized by multiple participants include: work-life separation through time (keeping clear working hours boundaries) and space (e.g., having a designated space for working in one''s home), open and proactive communication, encouraging time off with leadership setting an example, and keeping a healthy level of social interaction through tools such as Slack. Also cross-posted to the Effective Altruism Forum at https://forum.effectivealtruism.org/posts/nzwzrnEWvqgnxinkM/roundtable-how-can-remote-employees-maintain-a-healthy-work (no comments there as of November 10, 2019)' /* notes */
+    )
+;
