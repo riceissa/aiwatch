759d05840e948cd8b7309b423ca962dbb97b6dc7
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-07-05 20:50:42 -0300

    Fix error in date format

--- sql/positions/80000hours-positions.sql
+++ sql/positions/80000hours-positions.sql
@@ -2,5 +2,6 @@
 # https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE
 
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ('Peter McIntyre', '80,000 Hours', 'Head of Coaching', '2016-02', NULL, '2018-06', NULL, 'https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE', 'profile says he worked as Head of Coaching between Feb 2016 and Jul 2018', NULL, 'effective altruism') 
+    ('Peter McIntyre', '80,000 Hours', 'Head of Coaching', '2016-02-01', NULL, '2018-06-01', NULL, 'https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE', 'profile says he worked as Head of Coaching between Feb 2016 and Jul 2018', NULL, 'effective altruism') 
 ;
+
