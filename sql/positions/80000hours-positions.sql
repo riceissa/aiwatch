af7eb4c90083d4cc62ed7e1ba9f7df4c3cd1f538
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-07-05 22:33:49 -0300

    Add 80000 Hours team for 2012

--- sql/positions/80000hours-positions.sql
+++ sql/positions/80000hours-positions.sql
@@ -1,7 +1,37 @@
 # The archive snapshots used, in order:
 # https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE
+# http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team
 
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
     ('Peter McIntyre', '80,000 Hours', 'Head of Coaching', '2016-02-01', NULL, '2018-06-01', NULL, 'https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE', 'profile says he worked as Head of Coaching between Feb 2016 and Jul 2018', NULL, 'effective altruism') 
+    ,('William MacAskill', '80,000 Hours', 'President', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', 'William MacAskill was originally named William Crouch', NULL, 'effective altruism')
+    ,('Benjamin Todd', '80,000 Hours', 'Managing Director', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Vicky Clayton', '80,000 Hours', 'Researcher', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Nick Beckstead', '80,000 Hours', 'Career impact assessor', '2011-11-01', NULL, NULL, NULL, 'https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE', NULL, NULL, 'effective altruism') 
+    ,('Ashley Dodge', '80,000 Hours', 'Events Team Member', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Max Harris', '80,000 Hours', 'Events Manager', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Richard Batty', '80,000 Hours', 'Director of Community', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Lisanne Pueschel', '80,000 Hours', 'Website launch helper', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('David Leon', '80,000 Hours', 'Web content writer and editor', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Sebastian Farquhar', '80,000 Hours', 'Content Manager', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Karwai Ng', '80,000 Hours', 'Marketing', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Niel Bowerman', '80,000 Hours', 'Fundraiser', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Matt Gibb', '80,000 Hours', 'Cofounder and technical advisor', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Eric Gastfriend', '80,000 Hours', '80,000 Hours online social network developer', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
+    ,('Robbie Shade', '80,000 Hours', '80,000 Hours website lead developer', '2011-11-01', NULL, NULL, NULL, 'http://web.archive.org/web/20120115044830/http://80000hours.org/meet-the-team', NULL, NULL, 'effective altruism')
 ;
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
 
