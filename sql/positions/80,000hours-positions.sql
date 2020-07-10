76103f2da0a5a131fe8247a9c2842c4fe6d35550
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-07-10 17:01:54 -0300

    Fix Peter Hartree joining date

--- sql/positions/80,000hours-positions.sql
+++
@@ -1,6 +0,0 @@
-# The archive snapshots used, in order:
-# https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE
-
-insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ('Peter McIntyre', '80,000 Hours', 'Head of Coaching', '2016-02', NULL, '2018-06', NULL, 'https://www.linkedin.com/in/peteramcintyre/?fbclid=IwAR2gbTSKWW1uN-AVtO_kx7_Aw_08OisKikAd-88FjjjLD1IllqzFLC2PQVE', 'profile says he worked as Head of Coaching between Feb 2016 and Jul 2018', NULL, 'effective altruism') 
-;
