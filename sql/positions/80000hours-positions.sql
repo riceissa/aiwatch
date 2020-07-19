2181e53860604c95132be20301eda37a01365238
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-07-19 00:27:09 -0300

    Add George McGowan to positions

--- sql/positions/80000hours-positions.sql
+++ sql/positions/80000hours-positions.sql
@@ -45,6 +45,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Abbie Taylor', '80,000 Hours', 'Head of the Oxford Chapter', '2012-09-24', NULL , NULL, NULL, 'https://web.archive.org/web/20120824064711/http://80000hours.org/meet-the-team https://web.archive.org/web/20120924094442/http://80000hours.org/meet-the-team' , NULL, NULL, 'effective altruism')
     ,('Ilan Fischer', '80,000 Hours', 'Head of the Yale Chapter', '2012-09-24', NULL , NULL, NULL, 'https://web.archive.org/web/20120824064711/http://80000hours.org/meet-the-team https://web.archive.org/web/20120924094442/http://80000hours.org/meet-the-team' , NULL, NULL, 'effective altruism')
     ,('Tom Ash', '80,000 Hours', 'Director of Operations', '2012-09-24', NULL , NULL, NULL, 'https://web.archive.org/web/20120824064711/http://80000hours.org/meet-the-team https://web.archive.org/web/20120924094442/http://80000hours.org/meet-the-team' , NULL, NULL, 'effective altruism')
+    ,('George McGowan', '80,000 Hours', 'Human Resources', '2012-09-24', NULL , NULL, NULL, 'https://web.archive.org/web/20120824064711/http://80000hours.org/meet-the-team https://web.archive.org/web/20120924094442/http://80000hours.org/meet-the-team' , NULL, NULL, 'effective altruism')
 ;
 
 
