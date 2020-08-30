2758b1c1c0164d86346f91b1d4d083c9f56b4757
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-08-30 12:30:02 -0300

    Add Julia Galef to positions

--- sql/positions/cfar-positions.sql
+++ sql/positions/cfar-positions.sql
@@ -33,4 +33,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Luke Raskopf','Center for Applied Rationality','Instructor & Operations','2018-08-12','month',NULL,NULL,'https://web.archive.org/web/20180812171539/http://rationality.org/about/team',NULL,'position',NULL,NULL,'AI safety')
     ,('Brienne Strohl','Center for Applied Rationality','Research and Instructor','2019-04-05','month',NULL,NULL,NULL,NULL,'position',NULL,NULL,'AI safety')
     ,('Xavier Prospero','Center for Applied Rationality','Admin Support','2019-04-29','month',NULL,NULL,NULL,NULL,'position',NULL,NULL,'AI safety')
+    ,('Julia Galef','Center for Applied Rationality','President and founder','2012-01-01','year','2016-12-01','year', 'https://www.facebook.com/julia.galef/posts/10103010525130302', NULL,'position',NULL,NULL,'AI safety')
 ;
