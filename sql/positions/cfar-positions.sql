bb1e98d585bc98e9ec90f55e462c376d65404322
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-08-30 16:13:08 -0300

    Add information on Davis Kingsley position

--- sql/positions/cfar-positions.sql
+++ sql/positions/cfar-positions.sql
@@ -35,5 +35,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Xavier Prospero','Center for Applied Rationality','Admin Support','2019-04-29','month',NULL,NULL,NULL,NULL,'position',NULL,NULL,'AI safety')
     ,('Julia Galef','Center for Applied Rationality','President and cofounder','2012-01-01','year','2016-12-01','year', 'https://www.facebook.com/julia.galef/posts/10103010525130302', NULL,NULL,NULL,NULL,'AI safety')
     ,('Kenzi Amodei', 'Center for Applied Rationality', 'Director Of Operations', '2012-05-01', 'month', '2016-11-01', 'month', 'https://www.linkedin.com/in/kenzi-amodei-28b30111/', NULL, NULL, NULL, NULL, 'AI safety')
-    ,('Davis Kingsley', 'Center for Applied Rationality', 'Instructor', '2016-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/davis-kingsley-4a8a6a142/', NULL, NULL, NULL, 'contract', 'AI safety')
+    ,('Davis Kingsley', 'Center for Applied Rationality', 'Instructor', '2016-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/davis-kingsley-4a8a6a142/', NULL, NULL, NULL, NULL, 'AI safety')
 ;
