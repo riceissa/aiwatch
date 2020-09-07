921740ccef4a982b5bbc81f7c3be2feb7b127574
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 17:41:35 -0300

    Add John Patrick Addison to positions

--- sql/positions/gwwc-positions.sql
+++ sql/positions/gwwc-positions.sql
@@ -5,6 +5,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Luke Freeman', 'Giving What We Can', 'Head of organization', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lukekevinfreeman/', NULL, NULL, 'Effective altruism')
     ,('Julia Wise', 'Giving What We Can', 'President', '2017-01-01', 'year', '2020-01-01', 'year', 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
     ,('Julia Wise', 'Giving What We Can', 'Advisor', NULL, NULL, NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, 'advisor', 'Effective altruism')
-    ,('Sam Deere', Giving What We Can', 'Director of Communications', '2015-02-01', 'month', '2016-01-01', 'month', 'https://www.linkedin.com/in/samdeere/?originalSubdomain=uk', NULL, NULL, 'Effective altruism')
-    ,('Sam Deere', Giving What We Can', 'President and Technical Director', '2016-01-01', 'month', '2016-07-01', 'month', 'https://www.linkedin.com/in/samdeere/?originalSubdomain=uk', NULL, NULL, 'Effective altruism')
+    ,('Sam Deere', 'Giving What We Can', 'Director of Communications', '2015-02-01', 'month', '2016-01-01', 'month', 'https://www.linkedin.com/in/samdeere/?originalSubdomain=uk', NULL, NULL, 'Effective altruism')
+    ,('Sam Deere', 'Giving What We Can', 'President and Technical Director', '2016-01-01', 'month', '2016-07-01', 'month', 'https://www.linkedin.com/in/samdeere/?originalSubdomain=uk', NULL, NULL, 'Effective altruism')
+    ,('John Patrick Addison', 'Giving What We Can', 'Developer', NULL, NULL, NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
 ;
