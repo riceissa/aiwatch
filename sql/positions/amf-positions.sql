b3c76649cdd3b949a04e40a41e3940a3241f4fc6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 13:17:02 -0300

    Add Shaun Walsh to positions

---
+++ sql/positions/amf-positions.sql
@@ -0,0 +1,2 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ,('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, NULL)
