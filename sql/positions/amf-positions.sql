7f6619ee07f1604f683811d72468872cb9ba7daa
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 15:39:03 -0300

    Fill in cause_area value in all positions

--- sql/positions/amf-positions.sql
+++ sql/positions/amf-positions.sql
@@ -1,7 +1,7 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ,('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, NULL)
-    ,('Julian Austin', 'Against Malaria Foundation', 'Operations Director', '2017-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/julian-austin-23bb333b/?originalSubdomain=uk', NULL, NULL, NULL)
-    ,('Rob Mather', 'Against Malaria Foundation', 'Founder, CEO', '2004-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20100219084813/http://www.againstmalaria.com:80/AboutUs.aspx', NULL, NULL, NULL)
-    ,('Andrew Garner', 'Against Malaria Foundation', 'Head of Technology and Sole Developer', '2005-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/andrewjgarner/?originalSubdomain=uk', NULL, NULL, NULL)
-    ,('Peter Sherratt', 'Against Malaria Foundation', 'Executive Chair', '2005-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/peter-sherratt-74290812/?originalSubdomain=uk', NULL, NULL, NULL)
-    ,('Jenny Kent', 'Against Malaria Foundation', 'Donation administration', NULL, NULL, NULL, NULL, 'https://www.againstmalaria.com/People.aspx', NULL, 'volunteer', NULL)
+    ,('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
+    ,('Julian Austin', 'Against Malaria Foundation', 'Operations Director', '2017-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/julian-austin-23bb333b/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
+    ,('Rob Mather', 'Against Malaria Foundation', 'Founder, CEO', '2004-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20100219084813/http://www.againstmalaria.com:80/AboutUs.aspx', NULL, NULL, 'Global health and poverty')
+    ,('Andrew Garner', 'Against Malaria Foundation', 'Head of Technology and Sole Developer', '2005-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/andrewjgarner/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
+    ,('Peter Sherratt', 'Against Malaria Foundation', 'Executive Chair', '2005-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/peter-sherratt-74290812/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
+    ,('Jenny Kent', 'Against Malaria Foundation', 'Donation administration', NULL, NULL, NULL, NULL, 'https://www.againstmalaria.com/People.aspx', NULL, 'volunteer', 'Global health and poverty')
