37b97e4616ac5a6c40704d736d964726dbcb9f97
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 14:13:23 -0300

    Add Jualian Austin to positions

--- sql/positions/amf-positions.sql
+++ sql/positions/amf-positions.sql
@@ -1,2 +1,3 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
     ,('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, NULL)
+    ,('Julian Austin', 'Against Malaria Foundation', 'Operations Director', '2017-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/julian-austin-23bb333b/?originalSubdomain=uk', NULL, NULL, NULL)
