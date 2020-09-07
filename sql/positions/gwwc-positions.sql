76b89ebb0197a170aedb911ee3becdc45c2a0b38
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 17:04:46 -0300

    Add Luke K. Freeman to positions

--- sql/positions/gwwc-positions.sql
+++ sql/positions/gwwc-positions.sql
@@ -2,4 +2,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ('Thomas Sittler','Giving What We Can','Intern','2015-08-01','month','2015-10-01','month','https://www.linkedin.com/in/thomasmsittler/',NULL,'intern','Effective altruism')
     ,('Toby Ord', 'Giving What We Can', 'Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
     ,('William MacAskill', 'Giving What We Can', 'Co-Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
+    ,('Luke Freeman', 'Giving What We Can', 'Head of organization', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lukekevinfreeman/', NULL, NULL, 'Effective altruism')
 ;
