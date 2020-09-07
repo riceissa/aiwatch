a20da331c50d410b40d8731e64a9aa2d42920d43
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 16:38:04 -0300

    Add William MacAskill to positions

--- sql/positions/gwwc-positions.sql
+++ sql/positions/gwwc-positions.sql
@@ -1,4 +1,5 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ,('Thomas Sittler','Giving What We Can','Intern','2015-08-01','month','2015-10-01','month','https://www.linkedin.com/in/thomasmsittler/',NULL,'intern','Effective altruism')
+    ('Thomas Sittler','Giving What We Can','Intern','2015-08-01','month','2015-10-01','month','https://www.linkedin.com/in/thomasmsittler/',NULL,'intern','Effective altruism')
     ,('Toby Ord', 'Giving What We Can', 'Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
+    ,('William MacAskill', 'Giving What We Can', 'Co-Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
 ;
