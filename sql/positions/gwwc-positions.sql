50672b485e16bfa5d16db5c05769c1c7eac0895e
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-07 17:17:20 -0300

    Add Julia Wise to positions

--- sql/positions/gwwc-positions.sql
+++ sql/positions/gwwc-positions.sql
@@ -3,4 +3,6 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Toby Ord', 'Giving What We Can', 'Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
     ,('William MacAskill', 'Giving What We Can', 'Co-Founder', '2009-01-01', 'year', NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
     ,('Luke Freeman', 'Giving What We Can', 'Head of organization', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lukekevinfreeman/', NULL, NULL, 'Effective altruism')
+    ,('Julia Wise', 'Giving What We Can', 'President', '2017-01-01', 'year', '2020-01-01', 'year', 'https://www.givingwhatwecan.org/about-us/team/', NULL, 'advisor', 'Effective altruism')
+    ,('Julia Wise', 'Giving What We Can', 'Advisor', 'NULL, NULL, NULL, NULL, 'https://www.givingwhatwecan.org/about-us/team/', NULL, NULL, 'Effective altruism')
 ;
