888b228c21825bd9db63ca4caee1479df966d295
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-08 00:12:01 -0300

    Add Tanay Chanda to positions

--- sql/positions/charity-science-health-positions.sql
+++ sql/positions/charity-science-health-positions.sql
@@ -29,5 +29,6 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Sunil Paripelli','Charity Science Health','Hospital Data Collector','2018-06-01','month',NULL,NULL,'https://web.archive.org/web/20180620215159/https://www.charitysciencehealth.com/about.html',NULL,'full-time','Global health and poverty')
     ,('Krutika Ravishankar', 'Charity Science Health', 'Program Manager at Maharashtra', '2019-11-01', 'month', '2020-06-01', 'month', 'https://www.linkedin.com/in/krutikar/?originalSubdomain=in', NULL, NULL, 'Global health and poverty')
     ,('Nithya Nagarathinam', 'Charity Science Health', 'Program Manager', '2019-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/nithyanagarathinam/?originalSubdomain=in', NULL, NULL, 'Global health and poverty')
-    ,('Juliette Finetti', 'Charity Science Health', 'Program Coordinator', '2019-05-01', 'month', '2019'11'01', 'month', 'https://www.linkedin.com/in/juliette-finetti/?originalSubdomain=in', NULL, NULL, 'Global health and poverty')
+    ,('Juliette Finetti', 'Charity Science Health', 'Program Coordinator', '2019-05-01', 'month', '2019-11-01', 'month', 'https://www.linkedin.com/in/juliette-finetti/?originalSubdomain=in', NULL, NULL, 'Global health and poverty')
+    ,('Tanay Chanda, 'Charity Science Health', 'Program Assistant', '2019-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/tanay-chanda-97262013b/?originalSubdomain=in', NULL, NULL, 'Global health and poverty')
 ;
