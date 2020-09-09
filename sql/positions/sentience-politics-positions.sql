57a04eeae701d077fd84fcd6aef2446fb06b5cd6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-09 00:34:13 -0300

    Add Philipp Ryf to positions

---
+++ sql/positions/sentience-politics-positions.sql
@@ -0,0 +1,3 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ('Philipp Ryf', 'Sentience Politics', 'Project Manager', '2014-09-01', 'month', '2016-02-01', 'month', 'https://www.linkedin.com/in/philipp-ryf-24095563/?originalSubdomain=ch', NULL, NULL, 'Animal welfare')
+ 
