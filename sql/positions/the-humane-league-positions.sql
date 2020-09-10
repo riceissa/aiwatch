10b351a0dfe0ad9d72ffd8833c94df1f90a0ac88
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-10 01:26:09 -0300

    Add Neysa Colizzi to positions

--- sql/positions/the-humane-league-positions.sql
+++ sql/positions/the-humane-league-positions.sql
@@ -1,3 +1,5 @@
 (person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area)
     ,('Mark Middleton', 'The Humane League', 'Chairman', NULL, NULL, NULL, NULL, 'https://thehumaneleague.org/our-leadership', NULL, NULL, 'Animal welfare')
     ,('Lydia Chaudhry', 'The Humane League', 'Secretary', NULL, NULL, NULL, NULL, 'https://thehumaneleague.org/our-leadership', NULL, NULL, 'Animal welfare')
+    ,('Suzanne Niemoth', 'The Humane League', 'Treasurer', NULL, NULL, NULL, NULL, 'https://thehumaneleague.org/our-leadership', NULL, NULL, 'Animal welfare')
+    ,('Neysa Colizzi', 'The Humane League', 'Director', NULL, NULL, NULL, NULL, 'https://thehumaneleague.org/our-leadership', NULL, NULL, 'Animal welfare')
