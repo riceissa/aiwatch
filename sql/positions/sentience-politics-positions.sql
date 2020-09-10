5669763d337bef0fa2dd34c3c11fa8047df6c636
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-09 21:30:52 -0300

    Add Ivo Mändli

--- sql/positions/sentience-politics-positions.sql
+++ sql/positions/sentience-politics-positions.sql
@@ -2,4 +2,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ('Philipp Ryf', 'Sentience Politics', 'Project Manager', '2014-09-01', 'month', '2016-02-01', 'month', 'https://www.linkedin.com/in/philipp-ryf-24095563/?originalSubdomain=ch', NULL, NULL, 'Animal welfare')
     ,('Silvano Lieger', 'Sentience Politics', 'Co-Managing Director', '2019-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lieger/', NULL, NULL, 'Animal welfare')
     ,('Johanna Kuhn', 'Sentience Politics', 'Project Manager', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/johanna-kuhn/', NULL, NULL, 'Animal welfare')
+    ,('Ivo Mändli', 'Sentience Politics', 'Board Member', '2019-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ivo-m%C3%A4ndli-296375127/', NULL, NULL, 'Animal welfare')
  
