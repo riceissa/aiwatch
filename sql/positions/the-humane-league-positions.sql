d3348c9f8fa39659f3c0aca2a79b9bb2a60ba4a5
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-10 00:39:48 -0300

    Add Mark Middleton to positions

---
+++ sql/positions/the-humane-league-positions.sql
@@ -0,0 +1,2 @@
+(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area)
+    ,('Mark Middleton', 'The Humane League', 'Chairman', NULL, NULL, NULL, NULL, 'https://thehumaneleague.org/our-leadership', NULL, NULL, 'Animal welfare')
