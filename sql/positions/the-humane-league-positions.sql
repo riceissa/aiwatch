99075022bb8db2cf6e8f48df57448663a3821ea8
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-09-15 23:00:28 -0300

    Add Kalista Barter to positions

--- sql/positions/the-humane-league-positions.sql
+++ sql/positions/the-humane-league-positions.sql
@@ -87,3 +87,4 @@
     ,('Katie Zarajczyk', 'The Humane League', 'Campus Outreach Coordinator', '2018-07-01', 'month', '2019-12-01', 'month', 'https://www.linkedin.com/in/katie-zarajczyk-ma-70a5a3176/', NULL, NULL, 'Animal welfare')
     ,('Katie Zarajczyk', 'The Humane League', 'Fellowship Lead', '2019-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/katie-zarajczyk-ma-70a5a3176/', NULL, NULL, 'Animal welfare')
     ,('Daniela Escamilla', 'The Humane League', 'Social Media Manager', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/danielaescamilla/', NULL, NULL, 'Animal welfare')
+    ,('Kalista Barter', 'The Humane League', 'Senior Director Of Development', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/kalista-barter/', NULL, NULL, 'Animal welfare')
