f4ea49ecb23b47e183f54198c75fff919e2c9a71
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-04 02:03:18 -0300

    Fix errors

--- sql/positions/amf-positions.sql
+++ sql/positions/amf-positions.sql
@@ -1,5 +1,5 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ,('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
+    ('Shaun Walsh', 'Against Malaria Foundation', 'Programme Director', '2016-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shaun-walsh-13615b1/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
     ,('Julian Austin', 'Against Malaria Foundation', 'Operations Director', '2017-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/julian-austin-23bb333b/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
     ,('Rob Mather', 'Against Malaria Foundation', 'Founder, CEO', '2004-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20100219084813/http://www.againstmalaria.com:80/AboutUs.aspx', NULL, NULL, 'Global health and poverty')
     ,('Andrew Garner', 'Against Malaria Foundation', 'Head of Technology and Sole Developer', '2005-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/andrewjgarner/?originalSubdomain=uk', NULL, NULL, 'Global health and poverty')
