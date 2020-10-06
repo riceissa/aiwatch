4969285a5414f0b68f891bb43ec7fc61ca7af853
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-05 23:44:36 -0300

    Add Kenny Torrella to positions

---
+++ sql/positions/mercy-for-animals.sql
@@ -0,0 +1,6 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ('Milo Runkle', 'Mercy For Animals', 'Founder and President', '1999-01-01', 'year', '1999-10-01', 'month', 'https://www.linkedin.com/in/nathanrunkle/', NULL, NULL, 'Animal welfare')
+    ,('Milo Runkle', 'Mercy For Animals', 'Founder', '1999-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nathanrunkle/', NULL, NULL, 'Animal welfare')
+    ,('Kenny Torrella', 'Mercy For Animals', 'Public Relations Manager', '2017-06-01', 'month', '2018-01-01', 'month', 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, NULL, 'Animal welfare')
+    ,('Kenny Torrella', 'Mercy For Animals', 'Director Of Communications', '2018-01-01', 'month', '2019-03-01', 'month', 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, NULL, 'Animal welfare')
+    ,('Kenny Torrella', 'Mercy For Animals', 'Vice President, Public Engagement', '2019-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, 'Full-time', 'Animal welfare')
