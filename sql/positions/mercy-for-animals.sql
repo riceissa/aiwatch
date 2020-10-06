16a3be153661682f42d3f1af9dfb831320ee0ec0
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-06 00:04:54 -0300

    Add Jamie Berger to positions

--- sql/positions/mercy-for-animals.sql
+++ sql/positions/mercy-for-animals.sql
@@ -4,3 +4,6 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Kenny Torrella', 'Mercy For Animals', 'Public Relations Manager', '2017-06-01', 'month', '2018-01-01', 'month', 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, NULL, 'Animal welfare')
     ,('Kenny Torrella', 'Mercy For Animals', 'Director Of Communications', '2018-01-01', 'month', '2019-03-01', 'month', 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, NULL, 'Animal welfare')
     ,('Kenny Torrella', 'Mercy For Animals', 'Vice President, Public Engagement', '2019-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/kenny-torrella-66a0774a/', NULL, 'Full-time', 'Animal welfare')
+    ,('Jamie Berger', 'Mercy For Animals', 'Media Campaigns Coordinator', '2015-01-01', 'month', '2016-02-01', 'month', 'https://www.linkedin.com/in/jamiecberger/', NULL, NULL, 'Animal welfare')
+    ,('Jamie Berger', 'Mercy For Animals', 'Senior Video Producer/Writer', '2016-03-01', 'month', '2018-09-01', 'month', 'https://www.linkedin.com/in/jamiecberger/', NULL, NULL, 'Animal welfare')
+    ,('Jamie Berger', 'Mercy For Animals', 'Executive Assistant to the President', '2018-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jamiecberger/', NULL, NULL, 'Animal welfare')
