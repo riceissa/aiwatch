31b4b9804dc08261b6a2cf4a886d9f80608fcd5e
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-09 17:40:34 -0300

    Add Akbar Ali to positions

--- sql/positions/mercy-for-animals.sql
+++ sql/positions/mercy-for-animals.sql
@@ -75,3 +75,5 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Thaís Vizzaccaro', 'Mercy for Animals', 'Development specialist', '2020-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/thaisvizzaccaro/', NULL, 'full-time', 'Animal welfare')
     ,('Miranda Hoffman, 'Mercy for Animals', 'Advancement Services Manager', start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area)
     ,('Rachel Mazzagatti', 'Mercy for Animals', 'Development Assistant', '2020-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/rachel-mazzagatti711/', NULL, 'full-time', 'Animal welfare')
+    ,('Akbar Ali', 'Mercy for Animals', 'Legal Assistant', '2016-12-01', 'month', '2018-06-01', 'month', 'https://www.linkedin.com/in/akbar-ali-bab69010a/', NULL, NULL, 'Animal welfare')
+    ,('Akbar Ali', 'Mercy for Animals', 'Operations Manager', '2018-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/akbar-ali-bab69010a/', NULL, NULL, 'Animal welfare')
