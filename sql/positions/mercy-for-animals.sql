01df66b9f9b7ca5b884358de06cac5d4a068f8b6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-17 18:41:34 -0300

    Add Fitzia B to positions

--- sql/positions/mercy-for-animals.sql
+++ sql/positions/mercy-for-animals.sql
@@ -108,3 +108,4 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Ygor Nascimento', 'Mercy for Animals', 'Volunteer coordinator', '2017-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ygor-nascimento-a44a26153/', NULL, NULL, 'Animal welfare')
     ,('Courtney L. Dillard', 'Mercy for Animals', 'Social Change Researcher', '2019-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/cldillard/', NULL, NULL, 'Animal welfare')
     ,('Mariana Victoria Braga Resende', 'Mercy for Animals', 'Legal Affairs Intern', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/mariana-victoria-braga-resende-409805176/?originalSubdomain=br', NULL, NULL, 'Animal welfare')
+    ,('Fitzia B', 'Mercy for Animals' , 'Director Of Communications in Latin America', '2020-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/fitzia/', NULL, 'full-time', 'Animal welfare')
