d055e3c1bda62cabddced7dd3ca0970effbdf9ba
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-17 18:19:06 -0300

    Add Mariana Victoria Braga Resende to positions

--- sql/positions/mercy-for-animals.sql
+++ sql/positions/mercy-for-animals.sql
@@ -107,3 +107,4 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Jorge Moisés Assis', 'Mercy for Animals', 'Director of Operations', '2019-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jorge-mois%C3%A9s-assis-230a7721/', NULL, NULL, 'Animal welfare')
     ,('Ygor Nascimento', 'Mercy for Animals', 'Volunteer coordinator', '2017-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ygor-nascimento-a44a26153/', NULL, NULL, 'Animal welfare')
     ,('Courtney L. Dillard', 'Mercy for Animals', 'Social Change Researcher', '2019-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/cldillard/', NULL, NULL, 'Animal welfare')
+    ,('Mariana Victoria Braga Resende', 'Mercy for Animals', 'Legal Affairs Intern', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/mariana-victoria-braga-resende-409805176/?originalSubdomain=br', NULL, NULL, 'Animal welfare')
