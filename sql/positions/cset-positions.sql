33d1c1ac6ada7f97780754e5b1a09f0afbaac115
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-31 17:37:48 -0300

    Add Alex Friedland to positions

--- sql/positions/cset-positions.sql
+++ sql/positions/cset-positions.sql
@@ -1,3 +1,4 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
     ('Aditi Joshi', 'Center for Security and Emerging Technology', 'Semester Research Analyst, CyberAI Project', '2020-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/aditi-joshi-50b78b17a/', NULL, NULL, 'AI safety')
     ,('Alan Omar Loera Martinez', 'Center for Security and Emerging Technology', 'Semester Research Analyst', '2020-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alan-omar-loera-martinez-6a39751b2/', NULL, NULL, 'AI safety') 
+    ,('Alex Friedland', 'Center for Security and Emerging Technology', 'External Affairs Specialist', NULL, NULL, NULL, NULL, 'https://cset.georgetown.edu/staff/alex-friedland/', NULL, NULL, 'AI safety')
