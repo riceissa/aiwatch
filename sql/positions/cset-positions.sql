255639dac36f959ea415167f6b5c13d9241b80fb
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-31 17:10:00 -0300

    Create Center for Security and Emerging Technology file and add Aditi Joshi to positions

---
+++ sql/positions/cset-positions.sql
@@ -0,0 +1,2 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ('Aditi Joshi', 'Center for Security and Emerging Technology', 'Semester Research Analyst, CyberAI Project', '2020-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/aditi-joshi-50b78b17a/', NULL, NULL, 'AI safety')
