8ba273a7076b61e1f0790e81f14862c9a4a0c7bc
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-31 18:32:12 -0300

    Add Ashwin Acharya to positions

--- sql/positions/cset-positions.sql
+++ sql/positions/cset-positions.sql
@@ -5,3 +5,4 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Andrew Imbrie', 'Center for Security and Emerging Technology', 'Senior Fellow', '2019-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/andrew-imbrie-91637317/', NULL, NULL, 'AI safety')
     ,('Andrew Lohn', 'Center for Security and Emerging Technology', 'Senior Fellow', '2020-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/anlohn/', NULL, NULL, 'AI safety')
     ,('Anna Puglisi', 'Center for Security and Emerging Technology', 'Senior Fellow', '2020-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/anna-puglisi-65645a14b/', NULL, 'full-time', 'AI safety')
+    ,('Ashwin Acharya', 'Center for Security and Emerging Technology', 'Research Analyst', '2020-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ashwin-acharya/', NULL, 'full-time', 'AI safety')
