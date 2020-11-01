3e4e17444c728bee017812ff11691e5c05e10566
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-10-31 22:13:45 -0300

    Add Dahlia Peterson to positions

--- sql/positions/cset-positions.sql
+++ sql/positions/cset-positions.sql
@@ -15,3 +15,4 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Catherine Aiken', 'Center for Security and Emerging Technology', 'Survey Specialist', '2019-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/catherine-kane-aiken/', NULL, NULL, 'AI safety')
     ,('Chris Rohlf', 'Center for Security and Emerging Technology', 'Research Fellow', '2020-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chrisrohlf/', NULL, NULL, 'AI safety')
     ,('Cindy Martinez', 'Center for Security and Emerging Technology', 'Semester Research Analyst', '2020-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/cindy-martinez-3049911b4/', NULL, NULL, 'AI safety')
+    ,('Dahlia Peterson', 'Center for Security and Emerging Technology', 'Research Analyst', '2019-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/dahliapeterson/', NULL, NULL, 'AI safety')
