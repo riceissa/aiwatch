cc923c3b698563d5513208d1c19e7e941da321f1
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-11-20 01:37:16 -0300

    New SQL file

--- reload-db.sh
+++ reload-db.sh
@@ -65,3 +65,4 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/mercy-for-animals.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/people.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/cset-positions.sql"
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/metaculus-positions.sql"
