f61a6d4b9a447bf8482050d86964866607404e12
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-12-15 00:26:09 -0300

    Add six positions to file wild-animal-suffering-research.sql

---
+++ sql/positions/wild-animal-suffering-research.sql
@@ -0,0 +1,9 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+     ('Persis Eskander', 'Wild-Animal Suffering Research', 'Executive Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, NULL, 'Animal welfare')
+    ,('Ozy Brennan', 'Wild-Animal Suffering Research', 'Researcher', '2016-04-01', 'month', NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/ https://www.linkedin.com/in/ozy-brennan-1b86b3b2/', NULL, NULL, 'Animal welfare')
+    ,('Catia Faria', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
+    ,('Oscar Horta', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
+    ,('Peter Hurford', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
+    ,('Brian Tomasik', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
+   
