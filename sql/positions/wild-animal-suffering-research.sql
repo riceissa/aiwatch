f9c567c22db4bef4d0069233fd5d717b5cca119e
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-12-15 01:08:48 -0300

    add stuff

--- sql/positions/wild-animal-suffering-research.sql
+++ sql/positions/wild-animal-suffering-research.sql
@@ -2,8 +2,8 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
      ('Persis Eskander', 'Wild-Animal Suffering Research', 'Executive Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, NULL, 'Animal welfare')
     ,('Ozy Brennan', 'Wild-Animal Suffering Research', 'Researcher', '2016-04-01', 'month', NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/ https://www.linkedin.com/in/ozy-brennan-1b86b3b2/', NULL, NULL, 'Animal welfare')
-    ,('Catia Faria', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
-    ,('Oscar Horta', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
-    ,('Peter Hurford', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
-    ,('Brian Tomasik', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Avisor', 'Animal welfare')
+    ,('Catia Faria', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Advisor', 'Animal welfare')
+    ,('Oscar Horta', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Advisor', 'Animal welfare')
+    ,('Peter Hurford', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Advisor', 'Animal welfare')
+    ,('Brian Tomasik', 'Wild-Animal Suffering Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181124160349/https://was-research.org/team/', NULL, 'Advisor', 'Animal welfare')
    
