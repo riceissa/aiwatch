4108c6e697d670479db6144b96ff0df39ce7f6cc
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2020-12-26 23:11:22 -0300

    new file (animal-advocacy-africa.sql)

--- reload-db.sh
+++ reload-db.sh
@@ -80,6 +80,6 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/wild-animal-initiative.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/give-directly.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/raising-for-effective-giving.sql"
-
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/animal-advocacy-africa.sql"
 
 
