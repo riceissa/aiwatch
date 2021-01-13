516e07c3b941858c58d5b7d956bb6121b2852515
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-01-13 00:59:41 -0300

    aff file center-on-long-term-risk.sql

--- reload-db.sh
+++ reload-db.sh
@@ -82,5 +82,7 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/give-directly.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/raising-for-effective-giving.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/animal-advocacy-africa.sql"
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/center-on-long-term-risk.sql"
+
 
 
