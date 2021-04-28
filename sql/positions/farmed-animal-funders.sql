6054301f7897e03e32b54fe039b31edc23b1d4b6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-04-28 16:47:50 -0300

    Add 9 positions to /farmed-animal-funders.sql

--- sql/positions/farmed-animal-funders.sql
+++ sql/positions/farmed-animal-funders.sql
@@ -3,4 +3,12 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
      ('Mikaela Saccoccio', 'Farmed Animal Funders', 'Executive Director', '2019-10-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/ https://www.linkedin.com/in/mikaelasaccoccio', NULL, NULL, 'Animal welfare')
     ,('Kieran Greig', 'Farmed Animal Funders', 'Analyst consulting', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
- 
+    ,('Lewis Bollard', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('John Sobrato', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Timi Sobrato', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Simone Friedman', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Ariel Nessel', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Suzy Welch', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Chuck Laue', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Jennifer Laue', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
+    ,('Jim Greenbaum', 'Farmed Animal Funders', 'Board member', '2021-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210205201352/https://farmedanimalfunders.org/about/', NULL, NULL, 'Animal welfare')
