6b56960433c599c165b5b343bb98229f5d38f3c0
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-04-28 17:07:40 -0300

    Add two positions to nonlinear-fund.sql

---
+++ sql/positions/nonlinear-fund.sql
@@ -0,0 +1,6 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    
+
+
+     ('Kat Woods', 'Nonlinear Fund', 'President and Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
+    ,('Emerson Spartz', 'Nonlinear Fund', 'Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
