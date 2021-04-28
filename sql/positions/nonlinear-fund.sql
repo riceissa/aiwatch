585fed1d8dfe4150ebbb70acfcffc96eabd64cb3
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-04-28 18:21:15 -0300

    Add 5 positions to nonlinear-fund.sql

--- sql/positions/nonlinear-fund.sql
+++ sql/positions/nonlinear-fund.sql
@@ -4,3 +4,9 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
      ('Kat Woods', 'Nonlinear Fund', 'President and Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
     ,('Emerson Spartz', 'Nonlinear Fund', 'Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
+    ,('Jonas Vollmer', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')  
+    ,('Spencer Greenberg', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
+    ,('Alex Zhu', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
+    ,('Robert Miles', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
+    ,('David Moss', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
+ 
