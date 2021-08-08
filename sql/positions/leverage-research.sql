47bffd4822121edee0e4326d256aa04355ece050
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-08-07 22:16:09 -0300

    Try some change

--- sql/positions/leverage-research.sql
+++ sql/positions/leverage-research.sql
@@ -1,5 +1,6 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
+
      ('Geoff Anders', 'Leverage Research', 'Executive Director', '2011-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210802231852/https://www.leverageresearch.org/team https://www.linkedin.com/in/geoff-anders-0246b532/', NULL, NULL, NULL)
     ,('Kerry Vaughan', 'Leverage Research', 'Program Manager', '2019-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210802231852/https://www.leverageresearch.org/team https://www.linkedin.com/in/kerry-vaughan/', NULL, NULL, NULL)
     ,('Evan Pence', 'Leverage Research', 'Researcher', '2020-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210802231852/https://www.leverageresearch.org/team https://www.linkedin.com/in/davidevanpence/', NULL, NULL, NULL)
