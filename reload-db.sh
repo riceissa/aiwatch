b523890e3cefd24746488c9d9f65d9db9482cc2f
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-08-07 22:10:05 -0300

    fix error

--- reload-db.sh
+++ reload-db.sh
@@ -95,7 +95,7 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/The-Centre-for-Long-Term-Resilience.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/Longview-Philanthropy.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/organization_docs/effective-altruism-foundation-docs.sql"
-winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/organization_docs/leverage-research.sql"
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/leverage-research.sql"
 
 
 
