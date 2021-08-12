ffde861a7be150ddc3dc8a38cddc32a67ffd195a
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-08-12 16:52:02 -0300

    Add first document at 80000-hours-doc.sql

---
+++ sql/organization_docs/80000-hours-doc.sql
@@ -0,0 +1,15 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+   (
+        'https://eaforum.issarice.com/posts/ayzEiBgnnHX7DADwD/working-at-ea-organizations-series-80000-hours', /* url */
+        'Working at EA organizations series: 80000 Hours', /* title */
+        '2015-10-21', /* publication_date */
+        NULL, /* modified_date */
+        'SoerenMind', /* author */
+        'Effective Altruism Forum', /* publisher */
+        '80000 Hours', /* affected_organizations */
+        NULL, /* affected_people */
+        'Hiring-related notice', /* document_scope */
+        'Effective altruism', /* cause_area */
+        'EA Forum user SoerenMind shares interview with 80000 Hours co-founder and executive director Ben Todd, who explains current and future talent needs, how to get involved, how competitive are the positions, and what is the application process like at 80000 Hours as well as why work at this organization.' /* notes */
+    )
+;
