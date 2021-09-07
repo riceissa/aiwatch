439220bde0f6a04992d0399513774aa58ae54e8f
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-09-07 01:50:08 -0300

    Add positions to nonlinear-fund.sql

--- sql/positions/nonlinear-fund.sql
+++ sql/positions/nonlinear-fund.sql
@@ -4,9 +4,15 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
      ('Kat Woods', 'Nonlinear Fund', 'President and Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
     ,('Emerson Spartz', 'Nonlinear Fund', 'Co-founder', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210409010336/http://www.nonlinearfund.org/about.html', NULL, NULL, 'AI safety')
-    ,('Jonas Vollmer', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')  
+    ,('Jonas Vollmer', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety') 
     ,('Spencer Greenberg', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
     ,('Alex Zhu', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
     ,('Robert Miles', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
     ,('David Moss', 'Nonlinear Fund', 'Advisor', '2021-03-01', NULL, NULL, NULL, 'https://forum.effectivealtruism.org/posts/fX8JsabQyRSd7zWiD/introducing-the-nonlinear-fund-ai-safety-research-incubation', NULL, 'Advisor', 'AI safety')
+    ,('Tristan Cook', 'Nonlinear Fund', 'Executive Assistant', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
+    ,('Peter Barnett', 'Nonlinear Fund', 'Research Analyst', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
+    ,('Chris Leong', 'Nonlinear Fund', 'Research Analyst', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
+    ,('Corey Wood', 'Nonlinear Fund', 'Research Analyst', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
+    ,('Daniel del Castillo', 'Nonlinear Fund', 'Software Engineer', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
+    ,('Daniel del Castillo', 'Nonlinear Fund', 'Software Engineer', '2021-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210902003125/https://www.nonlinear.org/about.html', NULL, NULL, 'AI safety')
  
