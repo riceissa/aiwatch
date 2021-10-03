d651f4fc01e0d59e942bde8572960e986b8148e8
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-10-03 12:20:48 -0300

    Fix error at reload-db.sh

--- reload-db.sh
+++ reload-db.sh
@@ -98,8 +98,7 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/Longview-Philanthropy.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/leverage-research.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/sens.sql"
-winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/
-redwood-research.sql"
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/redwood-research.sql"
 
 
 
