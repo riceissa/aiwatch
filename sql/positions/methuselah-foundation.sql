372f8eaa23db8ae58b277ca204d1e0f4223aac70
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-10-19 01:02:26 -0300

    Add positions to methuselah-foundation.sql

---
+++ sql/positions/methuselah-foundation.sql
@@ -0,0 +1,191 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+     ('David Gobel', 'Methuselah Foundation', 'Chief Executive Officer', '2009-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20091209015057/http://www.mfoundation.org/index.php?pagename=mj_about_who', NULL, NULL, 'Longevity')
+    ,('Andrzej Bartke', 'Methuselah Foundation', 'Co-chairman of the Scientific Advisory Board', '2009-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20091209015057/http://www.mfoundation.org/index.php?pagename=mj_about_who', NULL, NULL, 'Longevity')
+    ,('Craig A. Cooney', 'Methuselah Foundation', 'Co-chairman of the Scientific Advisory Board', '2009-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20091209015057/http://www.mfoundation.org/index.php?pagename=mj_about_who', NULL, NULL, 'Longevity')
+    ,('Roger Holzberg', 'Methuselah Foundation', 'Executive Advisory Board', '2009-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20091209015057/http://www.mfoundation.org/index.php?pagename=mj_about_who', NULL, 'Board member', 'Longevity')
+    ,('Keith Murphy', 'Methuselah Foundation', 'Chief Executive Officer and President', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803193827/http://www.mfoundation.org/?pn=mj_about_who', NULL, NULL, 'Longevity')
+    ,('David Jacobs', 'Methuselah Foundation', 'Executive Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803193827/http://www.mfoundation.org/?pn=mj_about_who', NULL, 'Board member', 'Longevity')
+    ,('Robert Cohen', 'Methuselah Foundation', 'Executive Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803193827/http://www.mfoundation.org/?pn=mj_about_who', NULL, 'Board member', 'Longevity')
+    ,('Andrzej Bartke', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Craig A. Cooney', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Huber Warner', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Don Ingram', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Tom Johnson', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Christiaan Leeuwenburgh', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('John Speakman', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Steven Spindler', 'Methuselah Foundation', 'Scientific Advisory Board', '2010-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20100803033201/http://www.mfoundation.org/?pn=mj_sab', NULL, NULL, 'Longevity')
+    ,('Tyler Emerson', 'Methuselah Foundation', 'Executive Produce', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Dane Gobel', 'Methuselah Foundation', 'Operations Director', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Florina Linco', 'Methuselah Foundation', 'Social Media Manager', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Kevin Perrot', 'Methuselah Foundation', 'Executive Director', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Ross Robertson', 'Methuselah Foundation', 'Senior Writer', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Bjorn Cooley', 'Methuselah Foundation', 'Full Stack Developer', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Ting Kelly', 'Methuselah Foundation', 'Designer', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Emilie McWilliams', 'Methuselah Foundation', 'Donor Outreach Coordinator', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, NULL, 'Longevity')
+    ,('Anthony Atala', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Stephen BadyLak', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Christopher Breuer', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Karen Christman', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Sangeeta Bhatia', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Scott Collins', 'Methuselah Foundation', 'Advisor', '2013-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people https://www.linkedin.com/in/scottcollinsphd/', NULL, 'Advisor', 'Longevity')
+    ,('Raymond Chung', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Paolo De Coppi', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Gabor Forgacs', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Wolfram Goessling', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Jonathan Epstein', 'Methuselah Foundation', 'Advisor', ''2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Valerie Gouon Evans', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Armand Keating', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Eric Lagasse', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Paolo Macchiarini', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Chris Mason', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Robert Langer', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Jeffrey Molkentin', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('William Hinman', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Keith Murphy', 'Methuselah Foundation', 'Executive Advisory Board', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Board member', 'Longevity')
+    ,('Alan Russell', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Bernard Siegel', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Cathy Prescott', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('William Wagner', 'Methuselah Foundation', 'Advisor', '2014-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20141218185955/http://mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Robert Nerem', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Harald Ott', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Korkut Uygun', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Joseph P. Vacanti', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Laura Niklason', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Andre Terzic', 'Methuselah Foundation', 'Advisor', '2018-02-01', NULL, NULL, NULL, 'https://web.archive.org/web/20180220230911/https://www.mfoundation.org/about#people', NULL, 'Advisor', 'Longevity')
+    ,('Luis Alvarez', 'Methuselah Foundation', 'Advisor', '2019-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190801040932/https://www.mfoundation.org/who-we-are#our-team', NULL, 'Advisor', 'Longevity')
+    ,('Mauris De Silva', 'Methuselah Foundation', 'Advisor', '2019-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190801040932/https://www.mfoundation.org/who-we-are#our-team', NULL, 'Advisor', 'Longevity')
+    ,('Simon Fleming', 'Methuselah Foundation', 'Medical Advisor', '2021-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/simon-fleming-58324345/', NULL, 'Advisor', 'Longevity')
+    ,('Sergio Ruiz', 'Methuselah Foundation', 'Managing Director and CoFounder', '2016-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/sergio-ruiz/', NULL, NULL, 'Longevity')
+    ,('Danielle Ruiz', 'Methuselah Foundation', 'Fellow of Geriatric Medical Research', '2016-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/danielle-ruiz-msn-aprn-agnp-c-6774485b/', NULL, NULL, 'Longevity')
+    ,('Danielle Ruiz', 'Methuselah Foundation', 'Fellow of Geriatric Medical Research', '2016-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/danielle-ruiz-msn-aprn-agnp-c-6774485b/', NULL, NULL, 'Longevity')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
