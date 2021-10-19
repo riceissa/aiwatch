3dcff174fdd4ab9e0fde3430e857c3bbd4c5b012
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2021-10-18 22:31:37 -0300

    Add new org: training-for-good.sql

--- reload-db.sh
+++ reload-db.sh
@@ -100,7 +100,7 @@ winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/pos
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/sens.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/redwood-research.sql"
 winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/sci-foundation.sql"
-
+winpty "$MYSQL" --defaults-extra-file="$HOME/.my.cnf" aiwatch -e "source sql/positions/training-for-good.sql"
 
 
 
