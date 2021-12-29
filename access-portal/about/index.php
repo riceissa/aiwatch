1b809eef1bccd777705248f7647bd72014271f84
Issa Rice <riceissa@gmail.com>
2021-12-29 14:58:51 +0300

    forgot about page

--- access-portal/about/index.php
+++ access-portal/about/index.php
@@ -16,6 +16,7 @@ $site_name = $subdomain === "aiwatch" ? "AI Watch" : "Org Watch";
   <script src="/jquery.min.js"></script>
   <script src="/jquery.tablesorter.js"></script>
   <script src="/anchor.min.js"></script>
+  <script src="change-theme.js"></script>
 <?php include_once("../style.inc"); ?>
 <?php include_once("../analytics.inc"); ?>
 </head>
@@ -281,6 +282,8 @@ if (false) {
 <script>
     $(function(){$("table").tablesorter();});
     anchors.add();
+    document.getElementById('changeThemeMenu').style.display='inline';
+    set_theme_from_cookies();
 </script>
 </body>
 </html>
