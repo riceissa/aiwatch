48ec8129218f25a8cd3c65fe90233508bbbe3402
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-01-03 23:20:55 -0300

    add stuff

--- sql/positions/astera.sql
+++ sql/positions/astera.sql
@@ -1,11 +1,11 @@
-insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
      ('Jed McCaleb','Astera Institute','Founder', '2020-08-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jed-mccaleb-4052a4/',NULL,'full-time',NULL)
-    ,('Ginger Golub','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL, NULL)
+    ,('Ginger Golub','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
     ,('Adam Marblestone','Astera Institute','Research Director, Longevity', '2021-01-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/adam-marblestone-87202813/',NULL,'part-time',NULL)
-    ,('Jessica Baldwin','Astera Institute','Manager, operations', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jessicaleighbaldwin/',NULL,'full-time',NULL)
-    ,('Nicholas Schaum','Astera Institute','Scientific Director: Rejuvenome, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/nicholas-schaum-5a8a12b6/',NULL,'Longevity',NULL)
+    ,('Jessica Baldwin','Astera Institute','Operations Manager', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jessicaleighbaldwin/',NULL,'full-time',NULL)
+    ,('Nicholas Schaum','Astera Institute','Scientific Director: Rejuvenome, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/nicholas-schaum-5a8a12b6/',NULL,NULL,'Longevity')
     ,('Benjamin Reinhardt','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
-    ,('Anastasia Gamick','Astera Institute','Project Manager, FROs, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/anastasiagamick/',NULL,'Longevity',NULL)
+    ,('Anastasia Gamick','Astera Institute','Project Manager, FROs, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/anastasiagamick/',NULL,NULL,'Longevity')
     ,('Michael Nielsen','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
     ,('Pritha Ghosh','Astera Institute','Research Principal', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20211229000323/https://astera.org/about/',NULL,NULL,NULL)
--- sql/positions/redwood-research.sql
+++ sql/positions/redwood-research.sql
@@ -1,65 +1,22 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-     ('Nate Thomas', 'Redwood Research', 'Leadership', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Nathaniel T.', 'Redwood Research', 'Founder & CEO', '2019-09-01', NULL, NULL, NULL, 'https://www.linkedin.com/in/nathaniel-t-18603079/', NULL, NULL, 'AI safety')
-    ,('Buck Shlegeris', 'Redwood Research', 'Leadership', '2009-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+     ('Nathaniel Thomas', 'Redwood Research', 'Founder & Chief Executive Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/nathaniel-t-18603079/', NULL, NULL, 'AI safety')
     ,('Buck Shlegeris', 'Redwood Research', 'Chief Technology Officer', '2020-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/buck-shlegeris-a2b89386/', NULL, 'full-time', 'AI safety')
-    ,('Bill Zito', 'Redwood Research', 'Leadership', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Bill Zito', 'Redwood Research', 'Chief Operating Officer', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Daniel Ziegler', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Ben Weinstein-Raun', 'Redwood Research', 'Member Of Technical Staff', '2021-07-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/benwr/', NULL, 'full-time', 'AI safety')
     ,('Adam Scherlis', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Seraphina Nix', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Paul Christiano', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
-    ,('Paul Christiano', 'Redwood Research', 'Founder of Alignment Research Center', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Holden Karnofsky', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
-    ,('Nate Thomas', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
-    ,('Ajeya Cotra', 'Redwood Research', 'Spotlighted Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Ajeya Cotra', 'Redwood Research', 'Senior Research Analyst at Open Philanthropy', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Blake Borgeson', 'Redwood Research', 'Spotlighted Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Malo Bourgon', 'Redwood Research', 'Spotlighted Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Owen Cotton-Barratt', 'Redwood Research', 'Spotlighted Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Nate Soares', 'Redwood Research', 'Executive Director at the Machine Intelligence Research Institute', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Ajeya Cotra', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Blake Borgeson', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Malo Bourgon', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Owen Cotton-Barratt', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
     ,('Royston Noronha', 'Redwood Research', 'Business Development Manager', '2020-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/roystonnoronha11/', NULL, 'full-time', 'AI safety')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    ,('Tao Lin', 'Redwood Research', 'Software Engineer', '2021-09-01', 'month', NULL, NULL, ' https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/https://www.linkedin.com/in/tao-lin-4651a5172/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACj2ek0BFLE-RuL4mPCjl1eh9334KM0JBds', NULL, 'full-time', 'AI safety')
+    ,('Noa Nabeshima', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('James Bregan', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Claire Zabel', 'Redwood Research', 'Advisor', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
 
 
