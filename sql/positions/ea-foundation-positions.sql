e77ddd5debbedafbec0958926f87d64bb845aa81
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-02-02 00:02:01 -0300

    Update ea-foundation-positions.sql

--- sql/positions/ea-foundation-positions.sql
+++ sql/positions/ea-foundation-positions.sql
@@ -1,14 +1,5 @@
-# Snapshots used, in chronological order:
-# https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/
-# https://web.archive.org/web/20160105114358/http://ea-stiftung.org:80/team/
-# https://web.archive.org/web/20160229092002/http://ea-stiftung.org:80/team/
-# https://web.archive.org/web/20160330010832/http://ea-stiftung.org:80/team/
-# https://web.archive.org/web/20160424142810/http://ea-stiftung.org:80/team/
-# https://web.archive.org/web/20160528181300/http://ea-stiftung.org/team/
-# https://web.archive.org/web/20160826163645/http://ea-stiftung.org:80/team/
-
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ('Adriano Mannino','Effective Altruism Foundation','President',NULL,NULL,'2016-12-20','month','https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20161220084424/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+     ('Adriano Mannino','Effective Altruism Foundation','President',NULL,NULL,'2016-12-20','month','https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20161220084424/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
     ,('Lucius Caviola','Effective Altruism Foundation','Leader',NULL,NULL,'2016-10-26','month','https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
     ,('Jonas Vollmer','Effective Altruism Foundation','Head of operations',NULL,NULL,'2016-01-05','month','https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20160105114358/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
     ,('Jonathan Erhardt','Effective Altruism Foundation','Scientific assistant',NULL,NULL,'2016-12-20','month','https://web.archive.org/web/20151128134254/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
@@ -44,8 +35,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Tobias Pulver','Effective Altruism Foundation','Co-Executive Director','2016-10-26','month','2018-09-03','year','https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,NULL,'Effective altruism')
     ,('Ruairí Donnelly','Effective Altruism Foundation','Co-Executive Director','2016-10-26','month','2018-09-03','year','https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,NULL,'Effective altruism')
     ,('Lucius Caviola','Effective Altruism Foundation','Director of Outreach','2016-10-26','month','2017-06-29','month','https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20170629142528/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
-    # Removed by request:
-    # ,('Alina Mendt','Effective Altruism Foundation','Operations','2016-10-26','month','2017-06-29','month','https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20170629142528/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
+    ,('Alina Mendt','Effective Altruism Foundation','Operations','2016-10-26','month','2017-06-29','month','https://web.archive.org/web/20161026121638/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20170629142528/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
     ,('Lukas Gloor','Effective Altruism Foundation','Director of Strategy','2016-12-20','month',NULL,NULL,'https://web.archive.org/web/20161220084424/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
     ,('Jona Sicheneder','Effective Altruism Foundation','Operations','2016-12-20','month','2018-09-03','year','https://web.archive.org/web/20161220084424/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,NULL,'Effective altruism')
     ,('Jeff Jordan','Effective Altruism Foundation','Research and Growth','2016-12-20','month','2017-01-29','month','https://web.archive.org/web/20161220084424/http://ea-stiftung.org:80/team/ https://web.archive.org/web/20170129135053/http://ea-stiftung.org:80/team/',NULL,NULL,'Effective altruism')
@@ -62,14 +52,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Pascal Zimmer','Effective Altruism Foundation','Advisor','2018-09-03','year',NULL,NULL,'https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,'advisor','Effective altruism')
     ,('Jasper Götting','Effective Altruism Foundation','Community & Speaker (intern or volunteer)','2018-09-03','year',NULL,NULL,'https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,'volunteer','Effective altruism')
     ,('Max Räuker','Effective Altruism Foundation','Community & Speaker (intern or volunteer)','2018-09-03','year',NULL,NULL,'https://web.archive.org/web/20180903155442/https://ea-foundation.org/team/',NULL,'volunteer','Effective altruism')
-;
-
-
-insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-    ('Max Daniel','Effective Altruism Foundation','Researcher',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20171104132257/http://wrai.org/',NULL,NULL,NULL,NULL,'AI safety')
-;
-
-
+    ,('Max Daniel','Effective Altruism Foundation','Researcher',NULL,NULL,NULL,NULL,'https://web.archive.org/web/20171104132257/http://wrai.org/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Thomas Metzinger','Effective Altruism Foundation','Advisor', '2019-01-01',NULL,NULL,NULL,'https://web.archive.org/web/20180915223036/https://ea-foundation.org/team/',NULL,NULL,NULL,'Advisor','AI safety')
     ,('Adrian Hutter','Effective Altruism Foundation','Advisor', '2019-01-01',NULL,NULL,NULL,'https://web.archive.org/web/20180915223036/https://ea-foundation.org/team/',NULL,NULL,NULL,'Advisor','AI safety')
     ,('Persis Eskander','Effective Altruism Foundation','Executive Director (Wild Animal Suffering Research)', '2019-01-01',NULL,NULL,NULL,'https://web.archive.org/web/20180915223036/https://ea-foundation.org/team/',NULL,NULL,NULL,NULL,'AI safety')
