03db2aef947fbba3a51b18e41daad76cd8223c67
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-03-11 14:40:33 -0300

    Add org (FTX Future Fund)

---
+++ sql/positions/FTX-Future-Fund.sql
@@ -0,0 +1,10 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+  
+     ('Nick Beckstead','FTX Future Fund','CEO','2021-11-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nick-beckstead-7aa54374/',NULL,NULL,NULL,NULL,NULL)
+    ,('Leopold Aschenbrenner','FTX Future Fund','Future Fund','2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/leopold-aschenbrenner/',NULL,NULL,NULL,'full-time',NULL)
+    ,('William MacAskill','FTX Future Fund',NULL,'2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
+    ,('Ketan Ramakrishnan','FTX Future Fund',NULL,'2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
+    ,('Sam Bankman-Fried','FTX Future Fund', 'Chief Executive Officer','2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/sam-bankman-fried-8367a346/',NULL,NULL,NULL,NULL,NULL)
+    ,('Caroline Ellison','FTX Future Fund',NULL,'2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
+    ,('Gary Wang','FTX Future Fund',NULL,'2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
+    ,('Nishad Singh','FTX Future Fund', 'Director Of Engineering','2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nishad-singh-47a7b638/',NULL,NULL,NULL,NULL,NULL)
