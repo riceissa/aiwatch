2729040f1965d6d0c1201f659b4c4db3744d59d7
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-03-20 23:43:38 -0300

    update animal-advocacy-africa.sql

--- sql/positions/animal-advocacy-africa.sql
+++ sql/positions/animal-advocacy-africa.sql
@@ -2,10 +2,20 @@ insert into positions(person, organization, title, start_date, start_date_precis
    
 
 
-     ('Lynn Tan', 'Animal Advocacy Africa', 'Director of Research', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about', NULL, NULL, 'Animal welfare')
+     ('Lynn Tan', 'Animal Advocacy Africa', 'Director of Research', '2020-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about https://www.linkedin.com/in/pei-lynn-tan/', NULL, NULL, 'Animal welfare')
     ,('Cameron King', 'Animal Advocacy Africa', 'Director of Operations', '2020-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about https://www.linkedin.com/in/cameron-k-199646137/', NULL, NULL, 'Animal welfare') 
     ,('Jenna Hiscock', 'Animal Advocacy Africa', 'Director of Partnerships Development', '2020-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about https://www.linkedin.com/in/jenna-hiscock-40496b1b1/', NULL, NULL, 'Animal welfare') 
     ,('Brett Thompson', 'Animal Advocacy Africa', 'Co-founder', '2020-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about', NULL, NULL, 'Animal welfare')
-    ,('Brett Thompson', 'Animal Advocacy Africa', 'Advisor', '2020-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brett-thompson-6893171b/', NULL, 'Advisor', 'Animal welfare')
+    ,('Brett Thompson', 'Animal Advocacy Africa', 'Advisor', '2020-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team https://www.linkedin.com/in/brett-thompson-6893171b/', NULL, 'Advisor', 'Animal welfare')
     ,('Calvin Solomon', 'Animal Advocacy Africa', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about', NULL, 'Advisor', 'Animal welfare')
     ,('Catherine Jerotich Chumo', 'Animal Advocacy Africa', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20201228011953/https://www.animaladvocacyafrica.org/about', NULL, 'Advisor', 'Animal welfare')
+    ,('Kikiope Oluwarore', 'Animal Advocacy Africa', 'Program Manager', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team', NULL, NULL, 'Animal welfare')
+    ,('Joy Muthanje Mwaniki', 'Animal Advocacy Africa', 'Research Intern', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team', NULL, 'Intern', 'Animal welfare')
+    ,('Sarah Wanjiru', 'Animal Advocacy Africa', 'Social Media Associate', '2021-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team https://www.linkedin.com/in/sarah-wanjiru-b68399161/', NULL, NULL, 'Animal welfare')
+    ,('Moritz Stumpe', 'Animal Advocacy Africa', 'Research Associate', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team', NULL, NULL, 'Animal welfare')
+    ,('Catherine Chumo', 'Animal Advocacy Africa', 'Communications Advisor', '2019-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team https://www.linkedin.com/in/catherine-j-chumo-970b8518/', NULL, 'Advisor', 'Animal welfare')
+    ,('Cecil Yongo Abungu', 'Animal Advocacy Africa', 'Legal Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220315234328/https://www.animaladvocacyafrica.org/team', NULL, 'Advisor', 'Animal welfare')
+
+
+
+
