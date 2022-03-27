1a8381b59745755cccf457f34dbae35b07d06aac
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-03-26 21:00:29 -0300

    update one-for-the-world.sql

--- sql/positions/one-for-the-world.sql
+++ sql/positions/one-for-the-world.sql
@@ -2,7 +2,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
 	('Joshua McCann', 'One for the World','Co-Founder', '2014-03-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/joshua-mccann-720b9b21/?originalSubdomain=uk https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, NULL, 'Global health and poverty')
 	,('Kate Epstein', 'One for the World', 'Co-Founder', '2012-01-01', 'year', '2014-12-31', 'year', 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, NULL, 'Global health and poverty')
 	,('Steve Hind', 'One for the World', 'Chairman', '2018-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/shind/ https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, NULL, 'Global health and poverty')
-	,('Jack Lewars', 'One for the World', 'Executive Director', '2019-01-01', 'year', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team-', NULL, NULL, 'Global health and poverty')
+	,('Jack Lewars', 'One for the World', 'Executive Director', '2019-09-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/jack-lewars/', NULL, NULL, 'Global health and poverty')
 	,('Evan McVail', 'One for the World', 'Chief Operating Officer', '2018-07-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/evan-mcvail-626213117/', NULL, NULL, 'Global health and poverty')
 	,('Kennan McClung', 'One for the World', 'Director of Growth and Development', '2019-09-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/kennan-mcclung-64615769/', NULL, NULL, 'Global health and poverty')
 	,('Lauranne Duffaut', 'One for the World', 'International Expansion Lead', '2020-02-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/lauranne-duffaut-bb26b15b/', NULL, NULL, 'Global health and poverty')
@@ -15,6 +15,7 @@ insert into positions(person, organization, title, start_date, start_date_precis
 	,('Ian Yamey', 'One for the World', 'Advisory Board Member', '2019-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ianyamey/', NULL, 'Board Member', 'Global health and poverty')
 	,('Robert Struck', 'One for the World', 'Technology', NULL, NULL, NULL , NULL, 'https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, 'Volunteer', 'Global health and poverty')
 	,('Rossa O’Keffe-O’Donovan', 'One for the World', 'Partner Selection', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, NULL, 'Global health and poverty')
+	,('Rossa O’Keffe-O’Donovan', 'One for the World', 'Trustee', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team', NULL, NULL, 'Global health and poverty')
 	,('Charlie Scott', 'One for the World', 'Operations', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, 'Volunteer', 'Global health and poverty')
 	,('Jennifer Wong', 'One for the World', 'Board Member', '2017-01-01', 'year', '2019-12-31', 'year', 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/wongjennifery/', NULL, 'Board Member', 'Global health and poverty')
 	,('Joshua McCann', 'One for the World', 'Board Member', '2014-03-01', 'month', NULL, NULL, 'http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team- https://www.linkedin.com/in/joshua-mccann-720b9b21/?originalSubdomain=uk', NULL, 'Board Member', 'Global health and poverty')
@@ -31,4 +32,58 @@ insert into positions(person, organization, title, start_date, start_date_precis
 	,('Jennifer Wong', 'One for the World', 'Executive Committee', '2017-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/ https://www.linkedin.com/in/wongjennifery/', NULL, NULL, 'Global health and poverty')
 	,('Nadav Steinmetz', 'One for the World', 'Executive Committee', '2017-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/', NULL, NULL, 'Global health and poverty')
 	,('Steve Hind', 'One for the World', 'Board Member', '2016-01-01', 'year', '2018-07-31', 'month', 'https://www.linkedin.com/in/shind/ https://web.archive.org/web/20181011213658/https://www.1fortheworld.org/advisory-board/ http://archive.today/2020.12.15-211556/https://www.1fortheworld.org/meet-the-team-', NULL, 'Board Member', 'Global health and poverty')
-	
+	,('Chloë Cudaback', 'One for the World', 'Director of Communications', '2021-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team https://www.linkedin.com/in/chloecudaback/', NULL, 'full-time', 'Global health and poverty')
+        ,('Ella Matza', 'One for the World', 'Chapter Manager', '2022-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team', NULL, NULL, 'Global health and poverty')
+        ,('Jen Wong', 'One for the World', 'Chair of Trustees', '2022-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team', NULL, NULL, 'Global health and poverty')
+        ,('Josh McCann', 'One for the World', 'Trustee', '2022-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team', NULL, NULL, 'Global health and poverty')
+        ,('Nadav Steinmetz', 'One for the World', 'Board Member', '2017-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211115140022/https://www.1fortheworld.org/meet-the-team https://www.linkedin.com/in/nadav-steinmetz-38bb9518b/', NULL, 'Board Member', 'Global health and poverty')
+        ,('Emma W', 'One for the World', 'President (Stanford)', '2020-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/emmanuellaa/', NULL, NULL, 'Global health and poverty')
+        ,('Victoria Rau', 'One for the World', 'OFTW Member', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/victoria-rau/', NULL, NULL, 'Global health and poverty')
+        ,('Emily Holway', 'One for the World', 'Advisor', '2021-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/emilyholway/', NULL, 'Advisor', 'Global health and poverty')
+        ,('Stefano G.', 'One for the World', 'Founding President', '2018-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/stefano-makeadifferencenow/', NULL, NULL, 'Global health and poverty')
+        ,('Lorelei Robinson', 'One for the World', 'Intern', '2019-07-01', 'month', '2019-09-01', 'month', 'https://www.linkedin.com/in/lorelei-robinson-6b46aa190/', NULL, 'Intern', 'Global health and poverty')
+        ,('Lorelei Robinson', 'One for the World', 'Pledged', '2018-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lorelei-robinson-6b46aa190/', NULL, NULL, 'Global health and poverty')
+        ,('Charles Kay', 'One for the World', 'Co-Founder and Co-President', '2018-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/charles-kay-5b470567/', NULL, NULL, 'Global health and poverty')
+        ,('Courtney Fu', 'One for the World', 'Co-Founder', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/courtney-fu-94b442156/', NULL, NULL, 'Global health and poverty')
+        ,('Emma Ayache', 'One for the World', 'Founding Member & Head of Growth and Strategy', '2017-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/emmaayache/', NULL, NULL, 'Global health and poverty')
+        ,('Hanna Pálya', 'One for the World', 'Co-President of St Andrews Chapter', '2021-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/hanna-p%C3%A1lya-b7b93713b/', NULL, NULL, 'Global health and poverty')
+        ,('Bhumika Rathore', 'One for the World', 'Board Member', '2021-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/bhumika-rathore/', NULL, 'Board member', 'Global health and poverty')
+        ,('Brooke Berman', 'One for the World', 'OFTW Member', '2015-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brookeberman/', NULL, 'full-time', 'Global health and poverty')
+        ,('Pim Kangsanan', 'One for the World', 'OFTW Member', '2017-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/pim-kangsanan/', NULL, 'full-time', 'Global health and poverty')
+        ,('Samantha Heald', 'One for the World', 'OFTW Member', '2019-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/samantha-heald-318h2a77s/', NULL, 'full-time', 'Global health and poverty')
+        ,('Kristy Wong', 'One for the World', 'Student Ambassador', '2022-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/kristy-wong-8665341ba/', NULL, NULL, 'Global health and poverty')
+        ,('Bradley Graham', 'One for the World', 'Co-President (Manchester)', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/bradley-graham-33a500197/', NULL, NULL, 'Global health and poverty')
+        ,('Fernando S.', 'One for the World', 'Social Media Head', '2020-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/fernandosantandreu/', NULL, NULL, 'Global health and poverty')
+        ,('Tamara Hamade', 'One for the World', 'VP Communications of the McGill Chapter', '2021-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/tamarahamade/', NULL, NULL, 'Global health and poverty')
+        ,('Mikhail Oza', 'One for the World', 'OFTW Member', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/mikhail-oza/', NULL, 'full-time', 'Global health and poverty')
+        ,('Alexei Smirnov', 'One for the World', 'Advisor', '2020-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/lyosha/', NULL, 'Advisor', 'Global health and poverty')
+        ,('Sam Hocking', 'One for the World', 'OFTW Member', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/sam-hocking/', NULL, NULL, 'Global health and poverty')
+        ,('Tom Eastell', 'One for the World', 'Advisor', '2020-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/tom-eastell-87b39155/', NULL, 'Advisor', 'Global health and poverty')
+        ,('Evelyn Baek', 'One for the World', 'Ambassador', '2021-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/evelyn-baek-aa8168182/', NULL, NULL, 'Global health and poverty')
+        ,('Brian L.', 'One for the World', 'Founder and Co-President', '2021-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/briancmlee/', NULL, NULL, 'Global health and poverty')
+        ,('Liz Stiverson', 'One for the World', 'OFTW MEMBER', '2014-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lizstiverson/', NULL, NULL, 'Global health and poverty')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
