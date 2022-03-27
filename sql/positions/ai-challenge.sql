f8538ca3ad271257a48a13c40be82fd7c9e8434c
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-03-27 18:18:40 -0300

    update ai-challenge.sql

--- sql/positions/ai-challenge.sql
+++ sql/positions/ai-challenge.sql
@@ -1,14 +1,29 @@
-insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-	('Danit Gal', 'Keio University', 'Assistant Professor, Cyber Civilizations Research Center', '2018-02-01', 'month', '2019-04-30', 'month',  'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/danit-gal/', 'Judging panel for AI Challenge', 'position', NULL, NULL, 'AI safety' )
-	,('Danit Gal', 'Harvard University', 'Affiliate, Berkman Klein Center for Internet & Society', '2017-09-01', 'month', '2018-09-30', 'month',  'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/danit-gal/', 'Judging panel for AI Challenge', 'position',NULL, NULL, 'AI safety' )
-	,('Roman V. Yampolskiy', 'Associate Professor at University of Louisville', 'Associate Professor', '2014-07-01', 'month', NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/romanyam/', 'Judging panel for AI Challenge', 'unrelated', NULL, 'full-time', 'AI safety')
-	,('Virginia Dignum', 'TU Delft', 'Associate professor', '2009-11-01', 'month', NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/vdignum/', 'Judging panel for AI Challenge', 'unrelated', NULL, NULL, 'AI safety')
-	,('Rodolfo Rosini', 'Zeroth.ai', 'Partner', '2018-01-01', 'month', '2020-02-01', 'month', 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/rodol/', 'Judging panel for AI Challenge', 'unrelated', NULL, NULL, 'AI safety')
-	,('Kesavan Athimoolam', NULL, NULL, NULL, NULL, NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results', 'Won a $3,000 prize in the General AI Challenge', NULL, NULL, NULL, 'AI safety')
-	,('Alexey Turchin', NULL, NULL, NULL, NULL, NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://hpluspedia.org/wiki/Alexey_Turchin', 'Won a $3,000 prize in the General AI Challenge', 'GCR organization', NULL, NULL, 'AI safety')
-	,('Ehrik L. Aldana', 'University of California, Hastings College of the Law', 'Research Fellow, Center for Innovation', '2018-03-01', 'month', '2019-05-31', 'month', 'https://www.general-ai-challenge.org/solving-the-ai-race-results', 'Won a $3,000 prize in the General AI Challenge', NULL, NULL, NULL, 'AI safety')
-	,('David Klimek', 'Česká spořitelna', 'Head of Business Analysis Team', '2015-07-01', 'month', '2019-02-28', 'month', 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/klimek/?originalSubdomain=cz', 'Won a $2,000 prize in the General AI Challenge', NULL, NULL, NULL, 'AI safety')
-	,('G Gordon Worley III', 'AdStage', 'Head of SRE', '2015-11-01', 'month', '2020-03-31', 'month', 'https://www.general-ai-challenge.org/solving-the-ai-race-results https://www.linkedin.com/in/gworley3/', 'Won a $2,000 prize in the General AI Challenge', NULL, NULL, 'full-time', 'AI safety')
-	,('Morris Stuttard', NULL, NULL, NULL, NULL, NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results', 'Won a $2,000 prize in the General AI Challenge', NULL, NULL, NULL, 'AI safety')
-	,('Anastasia Slabukho', NULL, NULL, NULL, NULL, NULL, NULL, 'https://www.general-ai-challenge.org/solving-the-ai-race-results', 'Won a $2,000 prize in the General AI Challenge', NULL, NULL, NULL, 'AI safety')
-	
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+  
+     ('Marek Rosa', 'General AI Challenge', 'Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, NULL, 'AI safety')
+    ,('Will Millership', 'General AI Challenge', 'Challenge Director', '2017-08-01', 'month', '2021-08-01', 'month', 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team https://www.linkedin.com/in/will-millership-98393b58/details/experience/', NULL, NULL, 'AI safety')
+    ,('Olga Afanasjeva', 'General AI Challenge', 'Advisor', '2017-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team https://www.linkedin.com/in/afanasjevaolga/', NULL, 'Advisor', 'AI safety')
+    ,('Marek Havrda', 'General AI Challenge', 'Challenge Ambassador', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, NULL, 'AI safety')
+    ,('Roman Yampolskiy', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('José Hernández-Orallo', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Tomas Mikolov', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Julian Togelius', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Pavel Kordik', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Alison Lowndes', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Ling Ge', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Jan Romportl', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Tak Lo', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Jan Sekerka', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Ryota Kanai', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Ayako Fukui', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Jan Pospíšil', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Ivan Zelinka', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Miles Brundage', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Virginia Dignum', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Frank Dignum', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Irakli Beridze', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Danit Gal', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+    ,('Rodolfo Rosini', 'General AI Challenge', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210613020011/https://www.general-ai-challenge.org/team', NULL, 'Advisor', 'AI safety')
+           
+ 
+
