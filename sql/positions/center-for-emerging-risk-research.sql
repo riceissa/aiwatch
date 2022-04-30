78d426425ba97402b2414748df8d3081d2f32985
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-04-30 16:02:05 -0300

    add center-for-emerging-risk-research.sql to positions

---
+++ sql/positions/center-for-emerging-risk-research.sql
@@ -0,0 +1,44 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+  
+     ('Stefan Torges', 'Center for Emerging Risk Research', 'Founder', '2021-05-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211120084254/https://emergingrisk.ch/team/ https://www.linkedin.com/in/stefan-torges-9b8b537a/', NULL, 'part-time', NULL)
+    ,('Stefan Torges', 'Center for Emerging Risk Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/', NULL, 'Advisor', NULL)
+    ,('Ruairi Donnelly', 'Center for Emerging Risk Research', 'President', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/ https://www.linkedin.com/in/ruairidonnelly/?originalSubdomain=de', NULL, NULL, NULL)
+    ,('Daniel Kestenholz', 'Center for Emerging Risk Research', 'Chief Operating Officer', '2020-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/ https://www.linkedin.com/in/daniel-kestenholz/?originalSubdomain=ch', NULL, NULL, NULL)
+    ,('Tobias Bauman', 'Center for Emerging Risk Research', 'Analyst', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/', NULL, NULL, NULL)
+    ,('Adrian Hutter', 'Center for Emerging Risk Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/', NULL, 'Advisor', NULL)
+    ,('David Althaus', 'Center for Emerging Risk Research', 'Vice President', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210301002807/https://emergingrisk.ch/', NULL, NULL, NULL)
+    ,('Jesse Clifton', 'Center for Emerging Risk Research', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220430182412/https://emergingrisk.ch/team/', NULL, 'Advisor', NULL)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
