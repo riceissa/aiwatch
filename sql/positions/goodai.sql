7e2e27ac5c7989a33c7f2c3fd5b1ae023032a111
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-04-30 22:13:21 -0300

    update goodai.sql

--- sql/positions/goodai.sql
+++ sql/positions/goodai.sql
@@ -1,32 +1,58 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-	('Marek Rosa', 'GoodAI', 'Founder', '2015-07-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', 'Also founder of the General AI Challenge and the AI Roadmap Institute, two initiatives by GoodAI and Judging panel for AI Challenge 2018.', 'position', 'general', NULL, 'AI safety')
-	,('Marek Rosa', 'GoodAI', 'Chief Executive Officer', '2015-07-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Marek Rosa', 'GoodAI', 'Chief Technology Officer', '2015-07-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Olga Afanasjeva', 'GoodAI', 'Chief Operating Officer', '2015-10-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/afanasjevaolga/?originalSubdomain=cz', 'Also Challenge Director of the General AI Challenge and a Contributer to the AI Roadmap Institute, two initatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
-	,('Marek Havrda', 'GoodAI', 'AI Policy and Social Impact Director', '2016-12-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekhavrda/?originalSubdomain=cz', 'Also a Challenge Ambassador of the General AI Challenge and a Contributer to the AI Roadmap Institute, two initatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
-	,('Jan Feyereisl', 'GoodAI', 'Senior Research Scientist', '2016-02-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/feyereisl/?originalSubdomain=cz', 'Also a Challenge Ambassador of the General AI Challenge and the Executive Director of the AI Roadmap Institute, two initiatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
-	,('Martin Poliak', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Martin Poliak', 'GoodAI', 'AI Consultant', '2015-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/martin-poliak-423530185/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Martin Poliak', 'GoodAI', 'AI Researcher', '2015-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/martin-poliak-423530185/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Will Millership', 'GoodAI', 'PR Manager', '2017-08-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/will-millership-98393b58/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Nicholas Guttenberg', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg#selection-545.0-545.19', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Simon Andersson', 'GoodAI', 'Senior Research Scientist', '2015-05-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/simon-andersson/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Jaroslav Vitku', 'GoodAI', 'Senior Research Scientist', '2015-03-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/jvitku/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Petr Hlubuček', 'GoodAI', 'Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Joseph Davidson', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Isabeau Premont-Schwarz', 'GoodAI', 'Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Přemek Paška', 'GoodAI', 'Research Engineer', '2015-05-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/p%C5%99emek-pa%C5%A1ka-218a7356/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Shantesh Patil', 'GoodAI', 'Senior Game Designer', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Jan Štafa', 'GoodAI', 'Research Engineer', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Jan Štafa', 'GoodAI', 'Software Engineer', '2018-06-01', 'month', '2019-09-30', 'month', 'https://www.linkedin.com/in/jan-%C5%A1tafa-4b9719162/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Filip Hauptfleisch', 'GoodAI', 'AI Game Programmer', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Filip Hauptfleisch', 'GoodAI', 'Unity Developer', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/filip-hauptfleisch/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Steffen Eichler', 'GoodAI', 'Unity Programmer and Developer', '2020-10-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/steffen-eichler-065919199/', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Šimon Šicko', 'GoodAI', 'Advisor', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', 'advisor', 'AI safety')
-	,('Lucia Šicková', 'GoodAI', 'Advisor', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', 'advisor', 'AI safety')
-	,('Petr Šrámek', 'GoodAI', 'Advisor', '2019-11-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/petrsramek/', 'Also CEO of Whytics', 'position', 'general', 'advisor', 'AI safety')
-	,('Lucie Krestova', 'GoodAI', 'HR Manager', '2017-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lucie-krestova-4694738/?locale=zh_CN', NULL, 'position', 'general', NULL, 'AI safety')
-	,('Viktorie Knezkova', 'GoodAI', 'Tech Recruiter', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/knezkova/', NULL, 'position', 'general', 'full-time', 'AI safety')
-	,('Michal Dvořák', 'GoodAI', 'AI/ML Engineer', '2019-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/michal-dvo%C5%99%C3%A1k-33093b25/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')
+     ('Marek Rosa', 'GoodAI', 'Founder', '2015-07-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', 'Also founder of the General AI Challenge and the AI Roadmap Institute, two initiatives by GoodAI and Judging panel for AI Challenge 2018.', 'position', 'general', NULL, 'AI safety')
+    ,('Marek Rosa', 'GoodAI', 'Chief Executive Officer', '2015-07-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Marek Rosa', 'GoodAI', 'Chief Technology Officer', '2015-07-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekrosa1/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Olga Afanasjeva', 'GoodAI', 'Chief Operating Officer', '2015-10-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/afanasjevaolga/?originalSubdomain=cz', 'Also Challenge Director of the General AI Challenge and a Contributer to the AI Roadmap Institute, two initatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
+    ,('Marek Havrda', 'GoodAI', 'AI Policy and Social Impact Director', '2016-12-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/marekhavrda/?originalSubdomain=cz', 'Also a Challenge Ambassador of the General AI Challenge and a Contributer to the AI Roadmap Institute, two initatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
+    ,('Jan Feyereisl', 'GoodAI', 'Senior Research Scientist', '2016-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/feyereisl/?originalSubdomain=cz', 'Also a Challenge Ambassador of the General AI Challenge and the Executive Director of the AI Roadmap Institute, two initiatives by GoodAI.', 'position', 'general', NULL, 'AI safety')
+    ,('Martin Poliak', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Martin Poliak', 'GoodAI', 'AI Consultant', '2015-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/martin-poliak-423530185/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Martin Poliak', 'GoodAI', 'AI Researcher', '2015-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://www.linkedin.com/in/martin-poliak-423530185/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Will Millership', 'GoodAI', 'PR Manager', '2017-08-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/will-millership-98393b58/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Nicholas Guttenberg', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg#selection-545.0-545.19', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Simon Andersson', 'GoodAI', 'Senior Research Scientist', '2015-05-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/simon-andersson/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Jaroslav Vitku', 'GoodAI', 'Senior Research Scientist', '2015-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/jvitku/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Petr Hlubuček', 'GoodAI', 'Research Scientist', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Joseph Davidson', 'GoodAI', 'Senior Research Scientist', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Isabeau Premont-Schwarz', 'GoodAI', 'Research Scientist', '2020-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/isabeau-pr%C3%A9mont-schwarz/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Přemek Paška', 'GoodAI', 'Research Engineer', '2015-05-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg https://www.linkedin.com/in/p%C5%99emek-pa%C5%A1ka-218a7356/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Shantesh Patil', 'GoodAI', 'Senior Game Designer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Jan Štafa', 'GoodAI', 'Research Engineer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Jan Štafa', 'GoodAI', 'Software Engineer', '2018-06-01', 'month', '2019-09-30', 'month', 'https://www.linkedin.com/in/jan-%C5%A1tafa-4b9719162/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Filip Hauptfleisch', 'GoodAI', 'AI Game Programmer', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Filip Hauptfleisch', 'GoodAI', 'Unity Developer', '2020-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/filip-hauptfleisch/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Steffen Eichler', 'GoodAI', 'Unity Programmer and Developer', '2020-10-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/steffen-eichler-065919199/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Šimon Šicko', 'GoodAI', 'Advisor', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', 'advisor', 'AI safety')
+    ,('Lucia Šicková', 'GoodAI', 'Advisor', NULL, NULL, NULL, NULL, 'https://archive.vn/Og7Sg', NULL, 'position', 'general', 'advisor', 'AI safety')
+    ,('Petr Šrámek', 'GoodAI', 'Advisor', '2019-11-01', 'month', NULL, NULL, 'https://archive.vn/Og7Sg https://www.linkedin.com/in/petrsramek/', 'Also CEO of Whytics', 'position', 'general', 'advisor', 'AI safety')
+    ,('Lucie Krestova', 'GoodAI', 'HR Manager', '2017-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://www.linkedin.com/in/lucie-krestova-4694738/?locale=zh_CN', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Viktorie Knezkova', 'GoodAI', 'Tech Recruiter', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/knezkova/', NULL, 'position', 'general', 'full-time', 'AI safety')
+    ,('Michal Dvořák', 'GoodAI', 'AI/ML Engineer', '2019-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/michal-dvo%C5%99%C3%A1k-33093b25/?originalSubdomain=cz', NULL, 'position', 'general', NULL, 'AI safety')	
+    ,('Stephanie Wendler', 'GoodAI', 'Research Administrator', '2021-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://www.linkedin.com/in/stephanie-wendler-00261249/', NULL, 'position', 'general', 'contractor', 'AI safety')
+    ,('Christine Lee', 'GoodAI', 'Technical Writer / PR Manager', '2019-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Dominik Čech', 'GoodAI', 'Game Designer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Reham Bukhari', 'GoodAI', 'Advance QA Engineer', '2021-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/ https://www.linkedin.com/in/reham-bukhari-691a5a64/?originalSubdomain=pk', NULL, 'position', 'general', 'full-time', 'AI safety')
+    ,('Petr Sramek', 'GoodAI', 'Strategic Advisor', '2019-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/petrsramek/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Karolína H.', 'GoodAI', 'Internal Recruiter', '2022-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/karol%C3%ADna-h-12039560/', NULL, 'position', 'general', 'Intern', 'AI safety')
+    ,('Sarka Krejcova', 'GoodAI', 'Internal Recruiter', '2021-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/sarkakrejcova/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Alex Angelini', 'GoodAI', 'Producer', '2021-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alex-gamedev/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Ryan Camilleri', 'GoodAI', 'Unity / AI Developer', '2021-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ryan-camilleri-a16713180/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Jose Solorzano', 'GoodAI', 'Deep Learning Engineer', '2021-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jose-solorzano-7913381/', NULL, 'position', 'general', 'contractor', 'AI safety')
+    ,('Petr Šimánek', 'GoodAI', 'Deep Learning Engineer', '2020-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/simanekpetr/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Jan Stafa', 'GoodAI', 'Research Engineer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/', NULL, 'position', 'general', NULL, 'AI safety')
+    ,('Wendelin Boehmer', 'GoodAI', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20220327090940/https://www.goodai.com/about/', NULL, 'position', 'general', 'Advisor', 'AI safety')
+ 		
+			
+			
+					
+			
+				
+			
+		
+			
+		
+		
+		
 	
 	
+		
