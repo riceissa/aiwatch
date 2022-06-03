4060c58ae11ce91a89be38aeb97f25ff375ae719
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-02 23:48:26 -0300

    add new file: arb-research.sql

---
+++ sql/positions/arb-research.sql
@@ -0,0 +1,6 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+     ('Gavin Leech', 'Arb', 'Founder', '2021-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/gleech/?originalSubdomain=uk https://forum.effectivealtruism.org/users/arb', NULL, 'full-time', NULL)
+     ,('Misha Yagudin', 'Arb', NULL, NULL, NULL, NULL, NULL, 'https://forum.effectivealtruism.org/users/arb', NULL, NULL, NULL)
+
+ 
