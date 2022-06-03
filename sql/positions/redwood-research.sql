588933a2fdaa06391634fc85120a66c9717738e9
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-03 01:06:22 -0300

    order positions alphabetically

--- sql/positions/redwood-research.sql
+++ sql/positions/redwood-research.sql
@@ -1,22 +1,25 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-     ('Nathaniel Thomas', 'Redwood Research', 'Founder & Chief Executive Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/nathaniel-t-18603079/', NULL, NULL, 'AI safety')
-    ,('Buck Shlegeris', 'Redwood Research', 'Chief Technology Officer', '2020-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/buck-shlegeris-a2b89386/', NULL, 'full-time', 'AI safety')
-    ,('Bill Zito', 'Redwood Research', 'Chief Operating Officer', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Daniel Ziegler', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Ben Weinstein-Raun', 'Redwood Research', 'Member Of Technical Staff', '2021-07-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/benwr/', NULL, 'full-time', 'AI safety')
-    ,('Adam Scherlis', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Seraphina Nix', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('Paul Christiano', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
-    ,('Holden Karnofsky', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
+     ('Adam Scherlis', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Ajeya Cotra', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Ben Weinstein-Raun', 'Redwood Research', 'Member Of Technical Staff', '2021-07-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/benwr/', NULL, 'full-time', 'AI safety')
+    ,('Bill Zito', 'Redwood Research', 'Chief Operating Officer', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
     ,('Blake Borgeson', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
-    ,('Malo Bourgon', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
-    ,('Owen Cotton-Barratt', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
-    ,('Royston Noronha', 'Redwood Research', 'Business Development Manager', '2020-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/roystonnoronha11/', NULL, 'full-time', 'AI safety')
-    ,('Tao Lin', 'Redwood Research', 'Software Engineer', '2021-09-01', 'month', NULL, NULL, ' https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/https://www.linkedin.com/in/tao-lin-4651a5172/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACj2ek0BFLE-RuL4mPCjl1eh9334KM0JBds', NULL, 'full-time', 'AI safety')
-    ,('Noa Nabeshima', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
-    ,('James Bregan', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Buck Shlegeris', 'Redwood Research', 'Chief Technology Officer', '2020-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/buck-shlegeris-a2b89386/', NULL, 'full-time', 'AI safety')
     ,('Claire Zabel', 'Redwood Research', 'Advisor', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Daniel Ziegler', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('James Bregan', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Holden Karnofsky', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
+    ,('Malo Bourgon', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Nathaniel Thomas', 'Redwood Research', 'Founder & Chief Executive Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/ https://www.linkedin.com/in/nathaniel-t-18603079/', NULL, NULL, 'AI safety')
+    ,('Noa Nabeshima', 'Redwood Research', 'Staff', '2021-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Owen Cotton-Barratt', 'Redwood Research', 'Advisor', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Advisor', 'AI safety')
+    ,('Paul Christiano', 'Redwood Research', 'Board Member', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, 'Board Member', 'AI safety')
+    ,('Royston Noronha', 'Redwood Research', 'Business Development Manager', '2020-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/roystonnoronha11/', NULL, 'full-time', 'AI safety')
+    ,('Seraphina Nix', 'Redwood Research', 'Staff', '2021-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210927163803/https://www.redwoodresearch.org/', NULL, NULL, 'AI safety')
+    ,('Tao Lin', 'Redwood Research', 'Software Engineer', '2021-09-01', 'month', NULL, NULL, ' https://web.archive.org/web/20211121121040/https://www.redwoodresearch.org/https://www.linkedin.com/in/tao-lin-4651a5172/?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAACj2ek0BFLE-RuL4mPCjl1eh9334KM0JBds', NULL, 'full-time', 'AI safety')
+
+
+
 
 
