0f03da9cebff2aaa978c27c9533529757fb19edf
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-11 19:32:54 -0300

    add Wayback urls to cooperativeai.sql

--- sql/positions/cooperativeai.sql
+++ sql/positions/cooperativeai.sql
@@ -1,11 +1,20 @@
 # Automated add from scripts/open_phil_proc.py
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-     ('Lewis Hammond', 'Cooperative AI Foundation', 'Acting Executive Director', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lrhammond/', NULL, 'part-time', 'AI safety')
-    ,('Jesse Clifton', 'Cooperative AI Foundation', 'Research Analyst', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
-    ,('Vincent Conitzer', 'Cooperative AI Foundation', 'Advisor', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', 'Professor at Carnegie Mellon University and Professor at University of Oxford', 'advisor', 'AI safety')
+
+ 
+    ('Allan Dafoe', 'Cooperative AI Foundation', 'Engineer', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://www.companieslondon.com/uk/13485176/cooperative-ai-foundation', NULL, NULL, 'AI safety')
+    ,('Allan Dafoe', 'Cooperative AI Foundation', 'Trustee', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210505150134/https://www.cooperativeai.com/foundation https://web.archive.org/web/20220610165957/https://www.cooperativeai.com/foundation', NULL, 'board member', 'AI safety')
+    ,('Amrit Sidhu-Brar', 'Cooperative AI Foundation', 'Company Director', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://www.companieslondon.com/uk/13485176/cooperative-ai-foundation', NULL, NULL, 'AI safety')
     ,('Amrit Sidhu-Brar', 'Cooperative AI Foundation', 'Operations Associate', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
-
-
-
-
+    ,('Dario Amodei', 'Cooperative AI Foundation', 'Chief Executive', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://web.archive.org/web/20210505150134/https://www.cooperativeai.com/foundation https://web.archive.org/web/20220610165957/https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
+    ,('Dario Amodei', 'Cooperative AI Foundation', 'Trustee', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210505150134/https://www.cooperativeai.com/foundation https://web.archive.org/web/20220610165957/https://www.cooperativeai.com/foundation', NULL, 'board member', 'AI safety')
+    ,('Eric Horvitz', 'Cooperative AI Foundation', 'IT Professional', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://www.companieslondon.com/uk/13485176/cooperative-ai-foundation', NULL, NULL, 'AI safety')
+    ,('Eric Horvitz', 'Cooperative AI Foundation', 'Trustee', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, 'board member', 'AI safety')
+    ,('Gillian Hadfield', 'Cooperative AI Foundation', 'Trustee', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210505150134/https://www.cooperativeai.com/foundation https://web.archive.org/web/20220610165957/https://www.cooperativeai.com/foundation', NULL, 'board member', 'AI safety')
+    ,('Gillian Kereldena Hadfield', 'Cooperative AI Foundation', 'Lecturer', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://www.companieslondon.com/uk/13485176/cooperative-ai-foundation', NULL, NULL, 'AI safety')
+    ,('Jesse Clifton', 'Cooperative AI Foundation', 'Research Analyst', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
+    ,('Lewis Hammond', 'Cooperative AI Foundation', 'Acting Executive Director', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lrhammond/', NULL, 'part-time', 'AI safety')
+    ,('Ruairi Donnelly', 'Cooperative AI Foundation', 'Company Director', '2021-06-30'/*same day as company formation*/, 'day', NULL, NULL, 'https://www.companieslondon.com/uk/13485176/cooperative-ai-foundation', NULL, NULL, 'AI safety')
+    ,('Ruairi Donnelly', 'Cooperative AI Foundation', 'Trustee', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210505150134/https://www.cooperativeai.com/foundation https://web.archive.org/web/20220610165957/https://www.cooperativeai.com/foundation', NULL, 'board member', 'AI safety') 
+    ,('Vincent Conitzer', 'Cooperative AI Foundation', 'Advisor', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', 'Professor at Carnegie Mellon University and Professor at University of Oxford', 'advisor', 'AI safety')
