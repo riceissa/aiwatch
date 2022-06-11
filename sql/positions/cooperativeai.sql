1ef0e61e314c63ad33f5d1393f738c2edf370898
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-11 18:37:12 -0300

    add new org: cooperativeai.sql

---
+++ sql/positions/cooperativeai.sql
@@ -0,0 +1,11 @@
+# Automated add from scripts/open_phil_proc.py
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+     ('Lewis Hammond', 'Cooperative AI Foundation', 'Acting Executive Director', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lrhammond/', NULL, 'part-time', 'AI safety')
+    ,('Jesse Clifton', 'Cooperative AI Foundation', 'Research Analyst', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
+    ,('Vincent Conitzer', 'Cooperative AI Foundation', 'Advisor', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', 'Professor at Carnegie Mellon University and Professor at University of Oxford', 'advisor', 'AI safety')
+    ,('Amrit Sidhu-Brar', 'Cooperative AI Foundation', 'Operations Associate', NULL, NULL, NULL, NULL, 'https://www.cooperativeai.com/foundation', NULL, NULL, 'AI safety')
+
+
+
+
