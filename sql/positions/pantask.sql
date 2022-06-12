6e346e6b13201b9ea40e52211a88ea79d02e5170
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-12 20:59:46 -0300

    add new org: pantask.sql

---
+++ sql/positions/pantask.sql
@@ -0,0 +1,8 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+ 
+     ('Mati Roy', 'Pantask', 'Founder, Chief Executive Officer', '2019-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/matiroy/?original_referer=https%3A%2F%2Fwww.google.com%2F&originalSubdomain=ca https://web.archive.org/web/20211207064056/https://www.pantask.com/about', NULL, NULL, NULL)
+    ,('Haydn Thomas', 'Pantask', 'Co-founder, Chief Operating Officer', '2019-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211207064056/https://www.pantask.com/about https://www.linkedin.com/in/matiroy/?original_referer=https%3A%2F%2Fwww.google.com%2F&originalSubdomain=ca', NULL, NULL, NULL)
+ 
+
+
+ 
