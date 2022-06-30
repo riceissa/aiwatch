11265182b74a09c9c1f1d87d4966ad7dd146884d
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-06-29 22:51:58 -0300

    add new org: epoch.sql

---
+++ sql/positions/epoch.sql
@@ -0,0 +1,21 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+
+     ('Jaime Sevilla', 'Epoch ', 'Director', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Tamay Besiroglu', 'Epoch ', 'Associate director', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Pablo Villalobos', 'Epoch ', 'Staff Researcher', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Anson Ho', 'Epoch ', 'Staff Researcher', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Lennart Heim', 'Epoch ', 'Research Fellow and Strategy Specialist', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Marius Hobbhahn', 'Epoch ', 'Research Fellow', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Eduardo Infante-Roldán', 'Epoch ', 'Software Engineer', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, NULL, 'AI safety')
+    ,('Tom Davidson', 'Epoch ', 'Research Advisor', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, 'advisor', 'AI safety')
+    ,('Neil Thompson', 'Epoch ', 'Research Advisor', '2022-06-28', 'month', NULL, NULL, 'https://epochai.org/team', NULL, 'advisor', 'AI safety')
+
+
+
+  
+
+
+  
+
+ 
