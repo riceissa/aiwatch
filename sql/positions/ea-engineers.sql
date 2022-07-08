dc179878d3a8b8f23bd98a296f5115d2165a5750
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-07-08 16:02:18 -0300

    add new org: ea-engineers.sql

---
+++ sql/positions/ea-engineers.sql
@@ -0,0 +1,9 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+    ('Jessica Wen', 'EA Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
+    ,('Sean Lawrence', 'EA Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
+
+
+
+
+ 
