91de80c77486558810b2cf21c75b8ee526f9d96b
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-08-30 00:56:01 -0300

    rename EA engineers to High Impact Engineers and add position in ea-engineers.sql

--- sql/positions/ea-engineers.sql
+++ sql/positions/ea-engineers.sql
@@ -1,9 +1,11 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-    ('Jessica Wen', 'EA Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
-    ,('Sean Lawrence', 'EA Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
+    ('Jessica Wen', 'High Impact Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
+    ,('Sean Lawrence', 'High Impact Engineers', 'Co-founder', '2022-07-04', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/MEfEsQ9yeB7fhMJTq/announcing-ea-engineers', NULL, NULL, 'effective altruism')
+    ,('Bryce Rogers', 'High Impact Engineers', 'Operations lead', NULL, NULL, NULL, NULL, 'https://www.highimpactengineers.org/team', NULL, NULL, 'effective altruism')
 
 
 
+ 
 
  
