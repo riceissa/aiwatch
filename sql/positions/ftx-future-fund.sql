7bf6bce0782d34b3ebf96f8c8c57ee5bed40ee31
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2022-12-02 19:09:09 -0300

    Upodate positions

--- sql/positions/ftx-future-fund.sql
+++ sql/positions/ftx-future-fund.sql
@@ -1,10 +1,15 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
   
-     ('Nick Beckstead','FTX Future Fund','CEO','2021-11-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nick-beckstead-7aa54374/',NULL,NULL,NULL,NULL,NULL)
-    ,('Leopold Aschenbrenner','FTX Future Fund','Future Fund','2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/leopold-aschenbrenner/',NULL,NULL,NULL,'full-time',NULL)
-    ,('William MacAskill','FTX Future Fund',NULL,'2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
-    ,('Ketan Ramakrishnan','FTX Future Fund',NULL,'2022-02-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
-    ,('Sam Bankman-Fried','FTX Future Fund', 'Chief Executive Officer','2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/sam-bankman-fried-8367a346/',NULL,NULL,NULL,NULL,NULL)
-    ,('Caroline Ellison','FTX Future Fund',NULL,'2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
-    ,('Gary Wang','FTX Future Fund',NULL,'2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,NULL)
-    ,('Nishad Singh','FTX Future Fund', 'Director Of Engineering','2019-04-01','month',NULL,NULL,'https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nishad-singh-47a7b638/',NULL,NULL,NULL,NULL,NULL)
+    /*TEAM:*/
+    ('Avital Balwit', 'FTX Future Fund', 'Team member', '2022-12-07', 'month', '2022-11-10', 'day', 'https://web.archive.org/web/20220713205734/https://ftxfuturefund.org/about/ ', 'Resigned upon FTX bankruptcy', NULL, subject, NULL, 'Effective altruism')
+    ,('Nick Beckstead','FTX Future Fund','CEO','2021-11-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nick-beckstead-7aa54374/ https://forum.effectivealtruism.org/posts/xafpj3on76uRDoBja/the-ftx-future-fund-team-has-resigned-1','Resigned upon FTX bankruptcy',NULL,NULL,NULL,'Effective altruism')
+    ,('Leopold Aschenbrenner','FTX Future Fund','Team member','2022-02-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/leopold-aschenbrenner/ https://forum.effectivealtruism.org/posts/xafpj3on76uRDoBja/the-ftx-future-fund-team-has-resigned-1','Resigned upon FTX bankruptcy',NULL,NULL,'full-time','Effective altruism')
+    ,('William MacAskill','FTX Future Fund','Team member','2022-02-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://forum.effectivealtruism.org/posts/xafpj3on76uRDoBja/the-ftx-future-fund-team-has-resigned-1','Resigned upon FTX bankruptcy',NULL,NULL,NULL,'Effective altruism')
+    ,('Ketan Ramakrishnan','FTX Future Fund','Team member','2022-02-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://forum.effectivealtruism.org/posts/xafpj3on76uRDoBja/the-ftx-future-fund-team-has-resigned-1','Resigned upon FTX bankruptcy',NULL,NULL,NULL,'Effective altruism')
+
+
+    /*BOARD MEMBERS:*/
+    ,('Sam Bankman-Fried','FTX Future Fund', 'Chief Executive Officer','2019-04-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/sam-bankman-fried-8367a346/',NULL,NULL,NULL,NULL,'Effective altruism')
+    ,('Caroline Ellison','FTX Future Fund',NULL,'2019-04-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,'Effective altruism')
+    ,('Gary Wang','FTX Future Fund',NULL,'2019-04-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/',NULL,NULL,NULL,NULL,'Effective altruism')
+    ,('Nishad Singh','FTX Future Fund', 'Director Of Engineering','2019-04-01','month','2022-11-10', 'day','https://web.archive.org/web/20220228190650/https://ftxfuturefund.org/about/ https://www.linkedin.com/in/nishad-singh-47a7b638/',NULL,NULL,NULL,NULL,'Effective altruism')
