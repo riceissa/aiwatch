6b3471eb155ad357409fc967baf0f50fc9af0575
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-01-01 20:07:10 -0300

    add new org: forecasting-research-institute.sql

---
+++ sql/positions/forecasting-research-institute.sql
@@ -0,0 +1,16 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+/*Note: as of  January 1, 2023, there's no Wayback snapshot for the team page*/
+
+('Philip Tetlock', 'Forecasting Research Institute', 'President, Chief Scientist', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+,('Michael Page', 'Forecasting Research Institute', 'CEO', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+,('Josh Rosenberg', 'Forecasting Research Institute', 'Senior Research Manager', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+,('Ezra Karger', 'Forecasting Research Institute', 'Research Director', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+,('Tegan McCaslin', 'Forecasting Research Institute', 'Senior Research Analyst', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+,('Tegan McCaslin', 'Forecasting Research Institute', 'Data Analyst', '2022-12-13', 'month', NULL, NULL, 'https://forum.effectivealtruism.org/posts/kEd5qWwg8pZjWAeFS/announcing-the-forecasting-research-institute-we-re-hiring https://forecastingresearch.org/team', NULL, NULL, NULL)
+;
+
+
+
+
+ 
