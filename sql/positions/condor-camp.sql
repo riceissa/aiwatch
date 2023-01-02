f8066f909ded30b89d153a2e16d1cb46385c579b
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-01-01 21:11:39 -0300

    update several files

--- sql/positions/condor-camp.sql
+++ sql/positions/condor-camp.sql
@@ -2,11 +2,11 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
 /*Note: as of January 1, 2023, there are no Wayback snapshots for team page*/
 
-    ('Adriana Araúzo', 'Condor Camp', 'Co-organizer and Co-founder', '2022-10-04', 'month', '2022-09-01', 'month', 'https://www.linkedin.com/in/adriana-arauzo/?originalSubdomain=br', NULL, 'full-time', NULL)/*created on 2023-01-01*/
-   ,('Adriana Araúzo', 'Condor Camp', 'Talent outreach coordinator', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/adriana-arauzo/?originalSubdomain=br', NULL, 'full-time', NULL)/*created on 2023-01-01*/
+    ('Adriana Araúzo', 'Condor Camp', 'Co-organizer and Co-founder', '2022-10-04', 'month', '2022-09-01', 'month', 'https://www.linkedin.com/in/adriana-arauzo/?originalSubdomain=br', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
+   ,('Adriana Araúzo', 'Condor Camp', 'Talent outreach coordinator', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/adriana-arauzo/?originalSubdomain=br', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
    ,('Carmen Medina', 'Condor Camp', 'Alumni Support and Content Developer', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/carmen-csilla-medina/?originalSubdomain=dk', NULL, 'full-time', NULL)/*created on 2023-01-01*/
-   ,('Carolina Oliveira', 'Condor Camp', 'Brand and Marketing Coordinator', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/carolinaollive/', NULL, 'full-time', NULL)/*created on 2023-01-01*/
-   ,('Leo Arruda', 'Condor Camp', 'Community Strategy/Event Organizer', '2022-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/leohmarruda/', NULL, 'full-time', NULL)/*created on 2023-01-01*/
-   ,('Renan Araujo', 'Condor Camp', 'Co-organizer', '2022-04-01', 'month', '2022-09-01', 'month', 'https://www.linkedin.com/in/renannascimentoaraujo/', NULL, 'full-time', NULL)/*created on 2023-01-01*/
-   ,('Renan Araujo', 'Condor Camp', 'Manager', '2022-10-01', 'month', NULL,NULL, 'https://www.linkedin.com/in/renannascimentoaraujo/', NULL, 'full-time', NULL)/*created on 2023-01-01*/
+   ,('Carolina Oliveira', 'Condor Camp', 'Brand and Marketing Coordinator', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/carolinaollive/', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
+   ,('Leo Arruda', 'Condor Camp', 'Community Strategy/Event Organizer', '2022-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/leohmarruda/', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
+   ,('Renan Araujo', 'Condor Camp', 'Co-organizer', '2022-04-01', 'month', '2022-09-01', 'month', 'https://www.linkedin.com/in/renannascimentoaraujo/', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
+   ,('Renan Araujo', 'Condor Camp', 'Manager', '2022-10-01', 'month', NULL,NULL, 'https://www.linkedin.com/in/renannascimentoaraujo/', NULL, 'full-time', 'longtermism')/*created on 2023-01-01*/
  
