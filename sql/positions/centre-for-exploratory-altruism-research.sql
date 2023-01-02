a1aa9d8da38d9c0dd678e86014c2b8d5b2b17f5e
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-01-02 16:58:01 -0300

    update file

--- sql/positions/centre-for-exploratory-altruism-research.sql
+++ sql/positions/centre-for-exploratory-altruism-research.sql
@@ -1,5 +1,5 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values   
- ('Joel Tan', 'Centre for Exploratory Altruism Research', 'Founder', '2022-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/joel-tan-1677368a/?originalSubdomain=sg https://exploratory-altruism.org/team-partners/', NULL, 'full-time', 'cause prioritization'/*On LinkedIn reads: "CEARCH conducts cause prioritization research and outreach, to identify the most important problems facing the world today, and to help direct money and talent towards addressing them."*/) /*Updated as of 2022-11-01*/   
+ ('Joel Tan', 'Centre for Exploratory Altruism Research', 'Founder', '2022-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/joel-tan-1677368a/?originalSubdomain=sg https://exploratory-altruism.org/team-partners/ https://forum.effectivealtruism.org/posts/me6xDoDzruPPuemQr/centre-for-exploratory-altruism-research-cearch', NULL, 'full-time', 'cause prioritization'/*On LinkedIn reads: "CEARCH conducts cause prioritization research and outreach, to identify the most important problems facing the world today, and to help direct money and talent towards addressing them."*/) /*Updated as of 2023-01-01*/   
  ;
 
 
