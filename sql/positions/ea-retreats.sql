f0a8e9e0fceb41e4a33c30121a2e201ffcc13d98
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-01-17 18:08:04 -0300

    update ea-retreats

--- sql/positions/ea-retreats.sql
+++ sql/positions/ea-retreats.sql
@@ -1,7 +1,7 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
   
-     ('Leilani Bellamy', 'EA Retreats', 'Founder', '2022-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20220430180841/https://www.canopyretreats.org/about-us https://www.linkedin.com/in/leilani-bellamy/', NULL, 'full-time', 'Effective altruism')
-    ,('Jessica Mayer-Jones', 'EA Retreats', 'Design director', '2022-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20220430180841/https://www.canopyretreats.org/about-us', NULL, NULL, 'Effective altruism')
+     ('Leilani Bellamy', 'Canopy Retreats', 'Founder', '2022-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20221112103608/https://www.canopyretreats.org/ https://www.linkedin.com/in/leilani-bellamy/', NULL, 'full-time', 'Effective altruism')/*Updated as of 2023-01-17*/
+    ,('Jessica Mayer-Jones', 'Canopy Retreats', 'Design director', '2022-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20221112103608/https://www.canopyretreats.org/', NULL, NULL, 'Effective altruism')/*Updated as of 2023-01-17*/
 
 
 
