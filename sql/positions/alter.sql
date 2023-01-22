f0ee9f37145d303e21809d48aaaebb532ac730ee
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-01-22 03:32:38 -0300

    sort lines ascending

--- sql/positions/alter.sql
+++ sql/positions/alter.sql
@@ -1,7 +1,9 @@
 INSERT INTO `positions` (`person`, `organization`, `title`, `start_date`, `start_date_precision`, `end_date`, `end_date_precision`, `urls`, `ai_safety_relation`, `subject`, `employment_type`, `cause_area`, `notes`) VALUES
-    ('David Manheim', 'Association for Long Term Existence and Resilience', 'Director', '2022-01-01', 'month', NULL, NULL, 'https://alter.org.il https://www.linkedin.com/in/davidmanheim/?originalSubdomain=il', 'GCR organization', 'technical research', 'full-time', 'AI safety', 'policy')
-    ,('Vanessa Kosoy', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', 'technical research', 'board member', 'AI safety', NULL)
-    ,('Joshua Fox', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', NULL, 'board member', 'AI safety', NULL)
+   
+ 
+   
+     ('David Manheim', 'Association for Long Term Existence and Resilience', 'Director', '2022-01-01', 'month', NULL, NULL, 'https://alter.org.il https://www.linkedin.com/in/davidmanheim/?originalSubdomain=il', 'GCR organization', 'technical research', 'full-time', 'AI safety', 'policy')
     ,('Edo Arad', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', NULL, 'board member', 'AI safety', NULL)
     ,('Gidon Kadosh', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', NULL, 'board member', 'AI safety', NULL)
-;
+    ,('Joshua Fox', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', NULL, 'board member', 'AI safety', NULL)
+    ,('Vanessa Kosoy', 'Association for Long Term Existence and Resilience', 'Board', '2022-04-01', 'month', NULL, NULL, 'https://alter.org.il', 'GCR organization', 'technical research', 'board member', 'AI safety', NULL)
