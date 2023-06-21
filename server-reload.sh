1d1200cebabea8afa9859f2cfac0487a1e264153
Issa Rice <riceissa@gmail.com>
2023-06-21 14:38:51 -0700

    add command

---
+++ server-reload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is the command I run on my server to refresh the data/codebase.
+# my.cnf is a file that looks like this:
+#     [client]
+#     user=issa
+#     user=secret
+
+git pull && make -B access-portal/head_date.txt && make read MYSQL_ARGS="--defaults-extra-file=my.cnf"
