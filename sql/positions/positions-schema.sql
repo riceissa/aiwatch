1a42ab5898dac840a13e08b2d3941e6a2e4d82c9
Issa Rice <riceissa@gmail.com>
2023-06-21 14:51:30 -0700

    add start_date <= end_date check

--- sql/positions/positions-schema.sql
+++ sql/positions/positions-schema.sql
@@ -39,5 +39,6 @@ create table positions(
 
     employment_type enum('full-time','part-time','contractor','intern','undergraduate student','graduate student','postdoc','board member','associate','advisor','volunteer','affiliate'),
     cause_area enum('AI safety','Global health and poverty','Effective altruism','Animal welfare','Cause prioritization', 'Forecasting', 'Longevity', 'Longtermism', 'Medicine', 'Politics', 'Suffering-focused ethics'),
-    notes varchar(2000) default null
+    notes varchar(2000) default null,
+    check (start_date <= end_date)
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
