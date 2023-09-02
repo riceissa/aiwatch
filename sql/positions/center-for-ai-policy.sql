de9c78e62895ac217bcf0c4a26a8b0db6f9356d9
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-09-02 16:30:51 -0300

    add new file: center-for-ai-policy.sql

---
+++ sql/positions/center-for-ai-policy.sql
@@ -0,0 +1,11 @@
+    insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values  
+     ('Jakub Kraus', 'Center for AI Policy', 'Operations Director', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jakraus/ https://web.archive.org/web/20230902183109/https://www.aipolicy.us/about', NULL, 'Full-time', 'AI safety') /*Reviewed on 2023-09-02*/ current position
+    ,('Jason Green-Lowe', 'Center for AI Policy', 'Legislative Director', '2023-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/greenlowe/ https://web.archive.org/web/20230902183109/https://www.aipolicy.us/about', NULL, NULL, 'AI safety') /*Reviewed on 2023-09-02*/ current position
+    ,('Thomas Larsen', 'Center for AI Policy', 'Executive Director', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/thomas-larsen/ https://web.archive.org/web/20230902183109/https://www.aipolicy.us/about', NULL, 'Full-time', 'AI safety') /*Reviewed on 2023-09-02*/ current position
+    
+
+
+    
+
+ 
+ 
