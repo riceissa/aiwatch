a7855e97701aa243254afe142555611c13b41d64
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-11-03 00:35:26 -0300

    add new org: elicit.sql

---
+++ sql/positions/elicit.sql
@@ -0,0 +1,18 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+     ('Adrian Smith', 'Elicit', 'Software Engineer', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/smithdevio/', NULL, NULL, cause_area)
+    ,('Andreas Stuhlmüller', 'Elicit' 'Chief Executive Officer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/stuhlmueller/', NULL, 'Full-time', cause_area)
+    ,('Ben Rachbach', 'Elicit', 'Quality Iteration Program Manager', '2018-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brachbach/', NULL, NULL, cause_area)
+    ,('Charlie George', 'Elicit', 'Machine Learning Engineer', '2023-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/charlie-george-2868a0235/', NULL, 'contractor', cause_area)
+    ,('Étienne Fortier-Dubois', 'Elicit', 'AI data and evaluation contractor', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/%C3%A9tienne-fortier-dubois-93251111b/', NULL, 'Full-time', cause_area)
+    ,('James Brady', 'Elicit', 'Head of Engineering', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/goodgravy/', NULL, 'Full-time', cause_area)
+    ,('Jungwon Byun', 'Elicit', 'Cofounder & COO', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jungwonbyun/', NULL, 'Full-time', cause_area)
+    ,('Sarah Park', 'Elicit', 'Head of Operations', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/park-sarah/', NULL, 'Full-time', cause_area)
+
+ 
+
+ 
+ 
+
+
+ 
