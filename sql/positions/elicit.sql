c7361f6061ffb41b7e6f7a6414022606f1837fdf
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2023-11-03 15:22:34 -0300

    update elicit.sql and ought.sql

--- sql/positions/elicit.sql
+++ sql/positions/elicit.sql
@@ -1,15 +1,19 @@
  insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-     ('Adrian Smith', 'Elicit', 'Software Engineer', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/smithdevio/', NULL, NULL, NULL)
-    ,('Andreas Stuhlmüller', 'Elicit', 'Chief Executive Officer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/stuhlmueller/', NULL, 'Full-time', NULL)
-    ,('Ben Rachbach', 'Elicit', 'Quality Iteration Program Manager', '2018-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brachbach/', NULL, NULL, NULL)
-    ,('Charlie George', 'Elicit', 'Machine Learning Engineer', '2023-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/charlie-george-2868a0235/', NULL, 'contractor', NULL)
-    ,('Étienne Fortier-Dubois', 'Elicit', 'AI data and evaluation contractor', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/%C3%A9tienne-fortier-dubois-93251111b/', NULL, 'Full-time', NULL)
-    ,('James Brady', 'Elicit', 'Head of Engineering', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/goodgravy/', NULL, 'Full-time', NULL)
-    ,('Jungwon Byun', 'Elicit', 'Cofounder & COO', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jungwonbyun/', NULL, 'Full-time', NULL)
-    /*,('Justin Reppert', organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area)*/
-    ,('Sarah Park', 'Elicit', 'Head of Operations', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/park-sarah/', NULL, 'Full-time', NULL)
+     ('Adrian Smith', 'Elicit', 'Software Engineer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/smithdevio/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, NULL, NULL)/*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Andreas Stuhlmüller', 'Elicit', 'Chief Executive Officer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/stuhlmueller/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'Full-time', NULL) /*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Ben Rachbach', 'Elicit', 'Evaluation Program Manager', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brachbach/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, NULL, NULL) /*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Charlie George', 'Elicit', 'Machine Learning Engineer', '2023-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/charlie-george-2868a0235/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'contractor', NULL) /*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Étienne Fortier-Dubois', 'Elicit', 'AI data and evaluation contractor', '2022-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/%C3%A9tienne-fortier-dubois-93251111b/', NULL, 'Full-time', NULL)/*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('James Brady', 'Elicit', 'Head of Engineering', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/goodgravy/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'Full-time', NULL)/*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Jungwon Byun', 'Elicit', 'Cofounder & COO', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jungwonbyun/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'Full-time', NULL)/*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Justin Reppert', 'Elicit', 'Machine Learning Research Engineer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/justinreppert/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'Full-time', NULL) /*Reviewed as of 2023-11-03*/ /*current position*/
+    ,('Luke Stebbing', 'Elicit', 'Software Engineer', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/luke-stebbing-38897b3/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, 'Full-time', NULL) /*Reviewed as of 2023-11-03*/ /*current position*/
+    ('Sarah Park', 'Elicit', 'Head of Operations', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/park-sarah/ https://web.archive.org/web/20231103160743/https://elicit.com/team', NULL, NULL, NULL)/*Reviewed as of 2023-11-03*/ /*current position*/
 
  
+ 
+ 
+
 
  
 
