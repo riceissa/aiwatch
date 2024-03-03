fc555092a5eef1973173b76f0b1afbb2d36c1fef
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-03-02 22:43:47 -0300

    updating future-of-life-institute.sql

--- sql/positions/future-of-life-institute.sql
+++ sql/positions/future-of-life-institute.sql
@@ -1,7 +1,4 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
-
-
-
      ('Akhil Deo','Future of Life Institute','Social Media Manager',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Alan Alda','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Alan Guth','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
@@ -10,10 +7,12 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Andrea Berman','Future of Life Institute','Grants Manager',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Anna Hehir','Future of Life Institute','Program Manager for Autonomous Weapons Systems',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Anna Yelizarova','Future of Life Institute','Project Manager',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
-    ,('Anthony Aguirre','Future of Life Institute','Co-Founder',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/ https://foresight.org/publications/AGI-Timeframes&PolicyWhitePaper.pdf',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Anthony Aguirre','Future of Life Institute','Executive Director and co-founder','2014-05-01','month',NULL,NULL,'https://futureoflife.org/team/ https://foresight.org/publications/AGI-Timeframes&PolicyWhitePaper.pdf https://www.linkedin.com/in/anthony-aguirre-75751b9/',NULL,NULL,NULL,NULL,'AI safety') /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Ariel Conn','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Ben Eisenpress', 'Future of Life Institute', 'Director of Operations', '2023-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/beneisenpress/', NULL, NULL, NULL, 'Full-time', 'AI safety')/*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Blake Pierson','Future of Life Institute','Director of Communications, Outreach and Operations',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Carlos Ignacio Gutierrez','Future of Life Institute','Policy Researcher',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Carlos Ignacio Gutierrez', 'Future of Life Institute', 'Artificial Intelligence Policy Researcher / UN Representative', '2021-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/carlosig/', NULL, NULL, NULL, 'Full-time', 'AI safety') /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Chase Moores','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Christof Koch','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Claudia Prettner','Future of Life Institute','EU Representative',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
@@ -24,12 +23,14 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Emilia Javorsky','Future of Life Institute','Director of Grants and Multistakeholder Engagement',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Eric Gastfriend','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Erik Brynjolfsson','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Fazl Barez', 'Future of Life Institute', 'AI Research (AI Existential Safety) Affiliate', '2023-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/fbarez/', NULL, NULL, NULL, NULL, 'AI safety')  /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Francesca Rossi','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Frank Wilczek','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('George Church','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Grzegorz Orwiński','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Gus Docker','Future of Life Institute','Podcast Host',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
-    ,('Jaan Tallinn','Future of Life Institute','Co-Founder',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Hamza Tariq Chaudhry', 'Future of Life Institute', 'US Policy Specialist', '2023-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/hamza-tariq-chaudhry/', NULL, NULL, subject, 'Full-time', 'AI safety')  /*Reviewed as of 2024-03-02*/ /*current position*/
+    ,('Jaan Tallinn','Future of Life Institute','Co-Founder','2014-05-01','month',NULL,NULL,,'https://web.archive.org/web/20221013205644/https://futureoflife.org/about-us/our-people/',NULL,NULL,NULL,NULL,'AI safety')  /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Jacob Beebe','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Jacob Trefethen','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Janos Kramar','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
@@ -37,11 +38,12 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Jesse Galef','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Kazue Evans','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Lucas Perry','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
-    ,('Mark Brakel','Future of Life Institute','Director of Policy',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Mark Brakel','Future of Life Institute','Director of European Policy','2021-05-01','month','2022-08-01','month','https://futureoflife.org/team/',NULL,NULL,NULL,'Full-time','AI safety') /*closed*/    
+    ,('Mark Brakel','Future of Life Institute','Director of Policy','2022-09-01','month',NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,'Full-time','AI safety')  /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Martin Rees','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Max Kesin','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Max Tegmark','Future of Life Institute','Co-Founder',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
-    ,('Meia Chita-Tegmark','Future of Life Institute','Co-Founder',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Meia Chita-Tegmark','Future of Life Institute','Co-Founder','2014-05-01','month',NULL,NULL,'https://futureoflife.org/team/ https://www.linkedin.com/in/meia-chita-tegmark-2b5b529b/',NULL,NULL,NULL,NULL,'AI safety') /*Reviewed as of 2024-03-02*/ /*current position*/
     ,('Melody Guan','Future of Life Institute','Volunteer',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Morgan Freeman','Future of Life Institute','Advisor',NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Na Li','Future of Life Institute',NULL,NULL,NULL,NULL,NULL,'https://futureoflife.org/team/',NULL,NULL,NULL,NULL,'AI safety')
