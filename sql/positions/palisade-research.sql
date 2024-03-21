59339259097fe3bccc32281bad64ffe9c62114bd
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-03-20 22:10:07 -0300

    updating palisade-research.sql

--- sql/positions/palisade-research.sql
+++ sql/positions/palisade-research.sql
@@ -1,6 +1,5 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values 
-    ,('Jeffrey Ladish', 'Palisade Research', 'Executive Director', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jeffreyladish/ https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*open position*/
-
-
+    ,('Jeffrey Ladish', 'Palisade Research', 'Executive Director', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jeffreyladish/ https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
+    ,('Charlie Rogers-Smith', 'Palisade Research', 'Chief of staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
 
     
