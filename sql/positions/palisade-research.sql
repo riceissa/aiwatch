09fbdc3bb457c9098d31c45098e9354b1c6a4e3d
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-03-20 21:29:01 -0300

    adding new org: palisade-research.sql

---
+++ sql/positions/palisade-research.sql
@@ -0,0 +1,6 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values 
+    ,('Jeffrey Ladish', 'Palisade Research', 'Executive Director', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jeffreyladish/ https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*open position*/
+
+
+
+    
