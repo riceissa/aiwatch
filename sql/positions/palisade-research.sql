294eebcd94c27070ac851f90d6d976e121beec58
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-03-20 23:31:09 -0300

    updating palisade-research.sql

--- sql/positions/palisade-research.sql
+++ sql/positions/palisade-research.sql
@@ -1,5 +1,14 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values 
-    ,('Jeffrey Ladish', 'Palisade Research', 'Executive Director', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jeffreyladish/ https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
+     ('Jeffrey Ladish', 'Palisade Research', 'Executive Director', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jeffreyladish/ https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
     ,('Charlie Rogers-Smith', 'Palisade Research', 'Chief of staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
+    ,('Karina Belokapov', 'Palisade Research', 'SERI MATS scholar (2023)', '2023-11-01', 'year', NULL, NULL, 'https://web.archive.org/web/20231101141635/https://palisaderesearch.org/team https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/  
+    ,('Kyle Scott', 'Palisade Research', 'Treasurer', '2023-11-01', 'year', NULL, NULL, 'https://web.archive.org/web/20231101141635/https://palisaderesearch.org/team https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, 'part-time', 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/
+    ,('Pranav Gade', 'Palisade Research', 'SERI MATS scholar (2023)', '2023-11-01', 'year', NULL, NULL, 'https://web.archive.org/web/20231101141635/https://palisaderesearch.org/team https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/  
+    ,('Simon Lermen', 'Palisade Research', 'SERI MATS scholar (2023)', '2023-11-01', 'year', NULL, NULL, 'https://web.archive.org/web/20231101141635/https://palisaderesearch.org/team https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/  
+    ,('Timothee Chauvin', 'Palisade Research', 'SERI MATS scholar (2023)', '2023-11-01', 'year', NULL, NULL, 'https://web.archive.org/web/20231101141635/https://palisaderesearch.org/team https://web.archive.org/web/20240317203123/https://palisaderesearch.org/team', NULL, NULL, 'AI safety') /*reviewed on 2024-03-17*/ /*current position*/  
 
     
+    
+     
+    
+ 
