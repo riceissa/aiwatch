d001237a67f5250155194611deef0cd8c0e9a2c2
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-04-02 18:25:03 -0300

    updating google-deep-mind.sql

--- sql/positions/google-deep-mind.sql
+++ sql/positions/google-deep-mind.sql
@@ -1,8 +1,8 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-     ('Andrew Lefrancq','Google DeepMind',NULL,NULL,NULL,NULL,NULL,'https://arxiv.org/pdf/1711.09883.pdf https://www.linkedin.com/in/alefrancq',NULL,NULL,NULL,NULL,'AI safety')
-
-
-     
+     ('Andrew Lefrancq','Google DeepMind','Program Manager','2016-01-01','month','2020-04-01','month','https://www.linkedin.com/in/alefrancq',NULL,NULL,NULL,NULL,'AI safety') /*closed*/
+    ,('Andrew Lefrancq','Google DeepMind','Senior Program Manager','2017-11-01','month','2017-11-01','month','https://www.linkedin.com/in/alefrancq',NULL,NULL,NULL,NULL,'AI safety') /*closed*/
+    ,('Andrew Lefrancq','Google DeepMind','Lead Program Manager, AI Research','2020-05-01','month','2023-04-01','month','https://www.linkedin.com/in/alefrancq',NULL,NULL,NULL,NULL,'AI safety') /*closed*/
+    ,('Andrew Lefrancq', 'Google DeepMind', 'Head of International Operations', '2023-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alefrancq', NULL, 'Full-time', 'AI safety') /*Reviewed on 2024-03-27*/ /*current position*/
     ,('Christiana Figueres','Google DeepMind','DeepMind Ethics & Society Fellow',NULL,NULL,NULL,NULL,'https://deepmind.com/applied/deepmind-ethics-society/fellows/',NULL,NULL,NULL,NULL,'AI safety')
     ,('Demis Hassabis','Google DeepMind','Co-Founder and CEO','2010-09-23','day',NULL,NULL,'https://deepmind.com/about/ https://en.wikipedia.org/wiki/DeepMind',NULL,NULL,NULL,NULL,'AI safety')
     ,('Diane Coyle','Google DeepMind','DeepMind Ethics & Society Fellow',NULL,NULL,NULL,NULL,'https://deepmind.com/applied/deepmind-ethics-society/fellows/',NULL,NULL,NULL,NULL,'AI safety')
@@ -19,5 +19,21 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Sean Legassick','Google DeepMind','Head of DeepMind Ethics & Society','2017-10-04','day',NULL,NULL,'https://www.wired.co.uk/article/deepmind-ethics-and-society-artificial-intelligence https://www.theverge.com/2017/10/4/16417978/deepmind-ai-ethics-society-research-group',NULL,NULL,NULL,NULL,'AI safety')
     ,('Shane Legg','Google DeepMind','Co-Founder and Chief Scientist','2010-09-23','day',NULL,NULL,'https://deepmind.com/about/ https://en.wikipedia.org/wiki/DeepMind https://arxiv.org/pdf/1706.03741.pdf https://arxiv.org/pdf/1711.09883.pdf https://arxiv.org/pdf/1705.08417.pdf',NULL,NULL,NULL,NULL,'AI safety')
     ,('Stanislav Fort', 'Google DeepMind', 'Senior Research Scientist', '2023-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/stanislav-fort-38199a58/', NULL, 'Full-time', 'AI safety') /*Reviewed on 2024-03-27*/ /*current position*/
+    ,('Thibault Sellam', 'Google DeepMind', ' Senior Research Scientist', '2023-06-01', 'month', NULL, NULL, 'https://www.signalhire.com/profiles/thibault-sellam%27s-email/12935', NULL, NULL, 'AI safety') /*Reviewed on 2024-03-27*/ /*current position*/
     ,('Verity Harding','Google DeepMind','Head of DeepMind Ethics & Society','2017-10-04','day',NULL,NULL,'https://www.wired.co.uk/article/deepmind-ethics-and-society-artificial-intelligence https://www.theverge.com/2017/10/4/16417978/deepmind-ai-ethics-society-research-group',NULL,NULL,NULL,NULL,'AI safety')
     ,('Victoria Krakovna','Google DeepMind','Research Scientist',NULL,NULL,NULL,NULL,'http://www.aiandsociety.org/en/vikakrakovna/ https://arxiv.org/pdf/1711.09883.pdf https://arxiv.org/pdf/1705.08417.pdf',NULL,NULL,NULL,NULL,'AI safety')
+    ,('Zachary Gleicher', 'Google DeepMind', 'Product Manager', '2017-07-01', 'month', '2023-01-01', 'month', 'https://www.signalhire.com/profiles/zachary-gleicher%27s-email/3237', NULL, NULL, 'AI safety') /*closed*/
+    ,('Zachary Gleicher', 'Google DeepMind', 'Co-lead, Applied Product Manager', '2022-12-01', 'month', NULL,NULL, 'https://www.signalhire.com/profiles/zachary-gleicher%27s-email/3237', NULL, NULL, 'AI safety')  /*Reviewed on 2024-03-27*/ /*current position*/
+
+    
+
+    
+
+
+
+ 
+
+
+ 
+
+
