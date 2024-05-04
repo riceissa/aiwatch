621d6ecd1efa9e7d486681050709d08734429895
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-04 20:12:19 -0300

    updating astera.sql

--- sql/positions/astera.sql
+++ sql/positions/astera.sql
@@ -2,9 +2,18 @@ insert into positions(person, organization, title, start_date, start_date_precis
      ('Adam Marblestone','Astera Institute','Research Director, Longevity', '2021-01-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/adam-marblestone-87202813/',NULL,'part-time',NULL)
     ,('Anastasia Gamick','Astera Institute','Project Manager, FROs, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/anastasiagamick/',NULL,NULL,'Longevity')
     ,('Benjamin Reinhardt','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
+    ,('Gary Miguel', 'Astera Institute', 'Software Engineer', '2022-07-01', 'month','2023-10-01', 'month', 'https://www.linkedin.com/in/gary-miguel/', NULL, 'Full-time ', 'Longevity') /*closed*/
+    ,('Gary Miguel', 'Astera Institute', 'Vice President of Engineering', '2023-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/gary-miguel/', NULL, 'Full-time ', 'Longevity')  /*Reviewed on 2024-05-04*/ /*current position*/
     ,('Ginger Golub','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
-    ,('Jed McCaleb','Astera Institute','Founder', '2020-08-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jed-mccaleb-4052a4/',NULL,'full-time',NULL)
+    ,('Jed McCaleb','Astera Institute','Founder', '2020-08-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jed-mccaleb-4052a4/',NULL,'Part-time',NULL) /*Reviewed on 2024-05-04*/ /*current position*/
     ,('Jessica Baldwin','Astera Institute','Operations Manager', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/jessicaleighbaldwin/',NULL,'full-time',NULL)
     ,('Michael Nielsen','Astera Institute','Research Fellow, Metascience', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/',NULL,NULL,NULL)
     ,('Nicholas Schaum','Astera Institute','Scientific Director: Rejuvenome, Longevity', '2021-06-01', 'month',NULL,NULL,'https://web.archive.org/web/20210721224630/https://astera.org/about/ https://www.linkedin.com/in/nicholas-schaum-5a8a12b6/',NULL,NULL,'Longevity')
     ,('Pritha Ghosh','Astera Institute','Research Principal', '2021-01-01', 'year',NULL,NULL,'https://web.archive.org/web/20211229000323/https://astera.org/about/',NULL,NULL,NULL)
+    ,('Seemay Chou', 'Astera Institute', 'Chief of Strategy', '2021-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/seemay-chou-4b30a812/', NULL, NULL, 'Longevity')  /*Reviewed on 2024-05-04*/ /*current position*/
+    ,('Steven Byrnes', 'Astera Institute', 'Research Fellow', '2022-08-01', 'month', NULL, NULL, 'https://www.signalhire.com/profiles/steven-byrnes%27s-email/10089208 https://www.linkedin.com/in/steven-byrnes-005a304/', NULL, 'Full-time', 'Longevity') /*Reviewed on 2024-05-04*/ /*current position*/
+    ,('Yicong (Alan) Zheng', 'Astera Institute', 'Summer Intern', '2023-06-01', 'month', '2023-09-01', 'month', 'https://www.linkedin.com/in/yicongzheng/', NULL, 'Full-time', 'Longevity') /*closed*/
+    ,('Yicong (Alan) Zheng', 'Astera Institute', 'Student Intern', '2023-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/yicongzheng/', NULL, 'Full-time', 'Longevity') /*Reviewed on 2024-05-04*/ /*current position*/
+
+
+ 
