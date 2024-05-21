c569798603566ab06b64a4b0473b9581d3fefa27
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-20 23:14:41 -0300

    creating new file: biotein.sql

---
+++ sql/positions/biotein.sql
@@ -0,0 +1,5 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+     ('Andri Kadaifciu', 'Biotein', 'Chief Innovation Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240520181821/https://bioteinresearch.ca/team.html https://www.linkedin.com/in/andri-kadaifciu/', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Anupra Chandran', 'Biotein', 'Co-Founder', '2020-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/anupra-chandran/ https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Nina Khera', 'Biotein', 'Chief Executive Officer, Co-Founder', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nina-khera-115b5a175/ https://web.archive.org/web/20240520181821/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    
