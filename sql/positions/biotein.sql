2990a9afe3a34af578c42de2e734035fe3b1449a
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-21 15:33:15 -0300

    updating biotein.sql

--- sql/positions/biotein.sql
+++ sql/positions/biotein.sql
@@ -1,5 +1,24 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-     ('Andri Kadaifciu', 'Biotein', 'Chief Innovation Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240520181821/https://bioteinresearch.ca/team.html https://www.linkedin.com/in/andri-kadaifciu/', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+     ('Akshaj Darbar', 'Biotein', 'Chief Science Officer', '2020-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/akshaj-darbar/', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Andri Kadaifciu', 'Biotein', 'Chief Innovation Officer', '2019-09-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240520181821/https://bioteinresearch.ca/team.html https://www.linkedin.com/in/andri-kadaifciu/', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
     ,('Anupra Chandran', 'Biotein', 'Co-Founder', '2020-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/anupra-chandran/ https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Ben Nashman',  'Biotein', 'Advisor', '2022-01-25', NULL, NULL, NULL, 'https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/    
+    ,('Collin Ewald',  'Biotein', 'Advisor', '2022-01-25', NULL, NULL, NULL, 'https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/    
+    ,('Joseph Geraci',  'Biotein', 'Advisor', '2022-01-25', NULL, NULL, NULL, 'https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Karl Miller', 'Biotein', 'Advisor', '2022-01-25', NULL, NULL, NULL, 'https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
+    ,('Kelsey Moody',  'Biotein', 'Advisor', '2022-01-25', NULL, NULL, NULL, 'https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html https://web.archive.org/web/20220125021956/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/    
     ,('Nina Khera', 'Biotein', 'Chief Executive Officer, Co-Founder', '2019-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nina-khera-115b5a175/ https://web.archive.org/web/20240520181821/https://bioteinresearch.ca/team.html', NULL, NULL, 'Longevity') /*Reviewed on 2024-05-20*/ /*current position*/
     
+
+
+ 
+
+
+ 
+
+
+ 
+
+
+
+ 
