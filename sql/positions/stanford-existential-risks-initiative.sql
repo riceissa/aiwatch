a0622d21c19e1d91afa756d9a113ca932eab8389
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-25 19:45:30 -0300

    adding new org: stanford-existential-risks-initiative.sql

---
+++ sql/positions/stanford-existential-risks-initiative.sql
@@ -0,0 +1,4 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ,('James Chua', 'Stanford Existential Risks Initiative', 'Machine Learning Researcher', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/james-chua-8a445913a/', 'Not on website team page', 'Full-time', 'Existential risk prevention') /*Reviewed on 2024-05-25*/ /*current position*/
+
+ 
