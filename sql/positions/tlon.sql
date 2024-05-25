d7b72cd3aa3826383766c08798603a025d3788aa
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-25 15:52:17 -0300

    updating tlon.sql

--- sql/positions/tlon.sql
+++ sql/positions/tlon.sql
@@ -1,1 +1,7 @@
-insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+    ('Federico Stafforini', 'Tlön', 'Software Engineer', '2023-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20240525130943/https://tlon.team/about', NULL, NULL, 'cause prioritization') /*Reviewed on 2024-05-25*/ /*current position*/
+    ,('Leonardo Picón', 'Tlön', 'Head of Operations', '2023-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20240525130943/https://tlon.team/about', NULL, NULL, 'cause prioritization') /*Reviewed on 2024-05-25*/ /*current position*/
+    ,('Pablo Stafforini', 'Tlön', 'Director', '2023-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20240525130943/https://tlon.team/about', NULL, NULL, 'cause prioritization') /*Reviewed on 2024-05-25*/ /*current position*/
+
+
+ 
