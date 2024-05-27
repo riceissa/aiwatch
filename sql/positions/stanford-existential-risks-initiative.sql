f8c4ee2301d203bcae81e82fffbcee3810d974b6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-05-26 21:26:42 -0300

    update stanford-existential-risks-initiative.sql

--- sql/positions/stanford-existential-risks-initiative.sql
+++ sql/positions/stanford-existential-risks-initiative.sql
@@ -1,4 +1,10 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-    ,('James Chua', 'Stanford Existential Risks Initiative', 'Machine Learning Researcher', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/james-chua-8a445913a/', 'Not on website team page', 'Full-time', 'Existential risk prevention') /*Reviewed on 2024-05-25*/ /*current position*/
+     ('James Chua', 'Stanford Existential Risks Initiative', 'Machine Learning Researcher', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/james-chua-8a445913a/', 'Not on website team page', 'Full-time', 'Existential risk prevention') /*Reviewed on 2024-05-25*/ /*current position*/
+    ,('Michael Byun', 'Stanford Existential Risks Initiative', 'Research Assistant', '2023-04-01', 'month', '2023-12-01', 'month', 'https://web.archive.org/web/20231003182122/https://seri.stanford.edu/people https://www.linkedin.com/in/michael-byun/', 'Figures as Student Organizer on the org website', 'Full-time', 'Existential risk prevention') /*Reviewed on 2024-05-25*/ /*current position*/
+ 
+
+
+
+
 
  
