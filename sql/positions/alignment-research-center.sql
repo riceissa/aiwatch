ee73b0f3f9a46070cfa187954d5d2b37a7b51b36
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2024-09-30 19:05:38 -0300

    updating alignment-research-center.sql

--- sql/positions/alignment-research-center.sql
+++ sql/positions/alignment-research-center.sql
@@ -1,4 +1,50 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+  
+ ('Beth Barnes', 'Alignment Research Center', 'Founder & Head of Research', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Emma Abele', 'Alignment Research Center', 'Executive Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Chris Painter', 'Alignment Research Center', 'Head of Policy', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Kit Harris', 'Alignment Research Center', 'Chief of Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Ben West', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Brian Goodrich', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('David Rein', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Hjalmar Wijk', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Josh Clymer', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Kathy Garcia', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Lucas Sato', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Maksym Taran', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Max Hasin', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Megan Kinniment', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Neev Parikh', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Ryan Bloom', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Sami Jawhar', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Tao Lin', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Thomas Broadley', 'Alignment Research Center', 'Member of Technical Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Kris Chari', 'Alignment Research Center', 'Member of Operations Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Kyle Scott', 'Alignment Research Center', 'Member of Operations Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Rae She', 'Alignment Research Center', 'Member of Operations Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Rebecca Baron', 'Alignment Research Center', 'Member of Operations Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Michael Chen', 'Alignment Research Center', 'Member of Policy Staff', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, NULL, 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Adam Gleave', 'Alignment Research Center', 'Advisor and Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, 'Bord Member', 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Holden Karnofsky', 'Alignment Research Center', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, 'Advisor', 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+,('Yoshua Bengio', 'Alignment Research Center', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20240930213009/https://metr.org/team/', NULL, 'Advisor', 'AI safety')  /*Reviewed on 2024-09-30*/ /*current position*/
+
+  
+  
+  /*................................start modifying rows below.....................................*/
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
      ('Amanda She', 'Alignment Research Center', 'Executive Assistant (ARC Evals)', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/amandarshe/', NULL, 'Full-time', 'AI safety') /*Reviewed on 2023-09-12*/ /*current position*/
     ,('Aryan Bhatt', 'Alignment Research Center', 'SERI MATS Research Scholar', '2022-11-01', 'month', '2023-02-01', 'month', 'https://www.linkedin.com/in/aryanbhatt17/', NULL, 'Full-time', 'AI safety')  /*Reviewed on 2023-09-14*/ 
     ,('Aryan Bhatt', 'Alignment Research Center', 'Visiting Researcher', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/aryanbhatt17/', NULL, 'Full-time', 'AI safety')  /*Reviewed on 2023-09-14*/ /*current position*/ 
