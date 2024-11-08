d20dc057c8b3b4fdac48871d2578e4f9adc51eac
ReaganKibet <reagankibetsang@gmail.com>
2024-11-08 06:42:57 +0300

    Adding org docs

---
+++ sql/organization_docs/Future-of-humanity-institute-org-docs.sql
@@ -0,0 +1,15 @@
+insert into organization_documents(url, title, publication_date, modified_date, author, publisher, affected_organizations, affected_people, document_scope, cause_area, notes) values
+   (
+        'https://forum.effectivealtruism.org/posts/j5xhPbj7ywdv6aEJc/ama-future-of-life-institute-s-eu-team', /* url */
+        'AMA: Future of Life Institute''s EU Team', /* title */
+        '2022-01-31', /* publication_date */
+        NULL, /* modified_date */
+        'Risto Uuk', /* author */
+        'Effective Altruism Forum', /* publisher */
+        'Future of Life Institute', /* affected_organizations */
+        NULL, /* affected_people */
+        'AMA', /* document_scope */
+        'AI safety', /* cause_area */
+        'Risto Uuk and Mark Brakel of the Future of Life Institute''s EU team discuss their work on AI safety and governance, including the institute''s AI risk mitigation efforts and their hiring of a new EU Policy Analyst.' /* notes */
+    )
+      
