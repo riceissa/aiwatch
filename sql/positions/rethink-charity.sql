7e20218455c3429692cbad5717b15c4a37bffd28
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-01-20 15:21:26 -0300

    Updating rethink-charity.sql

--- sql/positions/rethink-charity.sql
+++ sql/positions/rethink-charity.sql
@@ -1,15 +1,34 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-     ('Peter Hurford', 'Rethink Charity', 'President', '2012-06-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/ https://www.linkedin.com/in/peterhurford8/', NULL, NULL, 'effective altruism')
+
+
+     ('SoonKhen OwYong', 'Rethink Charity', 'Executive Director', '2022-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/owyongsk/ https://web.archive.org/web/20241202165703/https://rcforward.org/team/', NULL, 'Full-time', 'effective altruism') /*Last reviewed Wayback snapshot is from 2024-12-02*/ /*current position*/
+     ,('Nathan Smith', 'Rethink Charity', 'General Operations Specialist', '2024-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nathan-smith-b56307b4/ https://web.archive.org/web/20241202165703/https://rcforward.org/team/', NULL, 'Part-time', 'effective altruism') /*Last reviewed Wayback snapshot is from 2024-12-02*/ /*current position*/
+
+ 
+
+ 
+    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and Board Director (USA & Canada Boards)', '2020-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/teebarnett/ https://web.archive.org/web/20241202165703/https://rcforward.org/team/', NULL, NULL, 'effective altruism') /*Last reviewed Wayback snapshot is from 2024-12-02*/ /*current position*/
+
+
+
+
+
+    ,('Peter Hurford', 'Rethink Charity', 'President', '2012-06-01', 'month', NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/ https://www.linkedin.com/in/peterhurford8/', NULL, NULL, 'effective altruism')
     ,('Peter Hurford', 'Rethink Charity', 'Co-founder and Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Jessica Marcus', 'Rethink Charity', 'Treasurer', '2021-03-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Ed Prokop', 'Rethink Charity', 'Secretary', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Baxter Bullock', 'Rethink Charity', 'Executive Director', '2017-06-01', 'month', NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/baxter-bullock-71a170103/?originalSubdomain=ca', NULL, NULL, 'effective altruism')
-    ,('Tee Barnett', 'Rethink Charity', 'Senior Director of Growth & Strategy', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism') 
-    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and COO at Students for High-Impact Charity', '2016-03-01', 'month', '2017-02-01', 'month', 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism')
-    ,('Tee Barnett', 'Rethink Charity', 'Executive Director of Development', '2017-02-01', 'month', '2018-10-01', 'month', 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism')
-    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and Senior Director of Growth & Strategy', '2018-10-01', 'month', '2020-12-01', 'month', 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism')
-    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and Board Director', '2020-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism')
+
+
+
+
+ 
+ 
+
+
+
+
     ,('Alexander Gordon-Brown', 'Rethink Charity', 'Director', '2021-03-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Travis Cooper', 'Rethink Charity', 'Director', '2021-03-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Ozzie Gooen', 'Rethink Charity', 'Director', '2021-03-01', NULL, NULL, NULL, 'https://web.archive.org/web/20210326004059/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
@@ -18,20 +37,20 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Jacob Grossbard', 'Rethink Charity', 'Tech officer', '2018-04-01', 'month', NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/jacob-grossbard-521380155/', NULL, NULL, 'effective altruism')
     ,('Marisa Jurczyk', 'Rethink Charity', 'Administrative Volunteer', '2018-03-01', 'month', '2018-05-01', 'month', 'https://www.linkedin.com/in/marisajurczyk/', NULL, 'Volunteer', 'effective altruism')
     ,('Marisa Jurczyk', 'Rethink Charity', 'Student Leader Coordinator for Students for High-Impact Charity', '2017-08-01', 'month', '2018-10-01', 'month', 'https://www.linkedin.com/in/marisajurczyk/', NULL, 'Contractor', 'effective altruism')
-    ,('Marisa Jurczyk', 'Rethink Charity', 'Operations Officer', '2018-09-01', 'month', '2020-01-01', 'month', 'https://www.linkedin.com/in/marisajurczyk/', NULL, 'Part-time', 'effective altruism')
+    ,('Marisa Jurczyk', 'Rethink Charity', 'Operations Officer', '2018-09-01', 'month', '2020-01-01', 'month', 'https://www.linkedin.com/in/marisajurczyk/', NULL, 'Part-time', 'effective altruism')/*closed*/
     ,('Marisa Jurczyk', 'Rethink Charity', 'Operation Officer', '2020-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/marisajurczyk/', NULL, 'Full-time', 'effective altruism')
     ,('Siobhan Brenton', 'Rethink Charity', 'Operations Manager', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
     ,('Siobhan Brenton', 'Rethink Charity', 'Manager of Operations', '2018-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/siobhanbrenton/?originalSubdomain=ca', NULL, NULL, 'effective altruism')
     ,('Richenda Herzig', 'Rethink Charity', 'Project Lead and Advisor', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
-    ,('Richenda Herzig', 'Rethink Charity', 'Project Manager', '2017-03-01', 'month', '2018-07-01', 'month', 'https://www.linkedin.com/in/richenda-herzig-837398123/?originalSubdomain=uk', NULL, NULL, 'effective altruism')
-    ,('Richenda Herzig', 'Rethink Charity', 'Product Manage', '2018-06-01', 'month', '2019-11-01', 'month', 'https://www.linkedin.com/in/richenda-herzig-837398123/?originalSubdomain=uk', NULL, NULL, 'effective altruism')
+    ,('Richenda Herzig', 'Rethink Charity', 'Project Manager', '2017-03-01', 'month', '2018-07-01', 'month', 'https://www.linkedin.com/in/richenda-herzig-837398123/?originalSubdomain=uk', NULL, NULL, 'effective altruism')/*closed*/
+    ,('Richenda Herzig', 'Rethink Charity', 'Product Manage', '2018-06-01', 'month', '2019-11-01', 'month', 'https://www.linkedin.com/in/richenda-herzig-837398123/?originalSubdomain=uk', NULL, NULL, 'effective altruism')/*closed*/
     ,('Catherine Low', 'Rethink Charity', 'Manager of SHIC', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
-    ,('Catherine Low', 'Rethink Charity', 'Resource Coordinator', '2019-04-01', 'month', '2020-02-01', 'month', 'https://www.linkedin.com/in/catherine-low-076a0a70/?originalSubdomain=nz', NULL, NULL, 'effective altruism')
-    ,('David Vatousios', 'Rethink Charity', 'Communications Officer', '2017-09-01', 'month', '2018-02-01', 'month', 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/david-vatousios-aa796b160/?originalSubdomain=ca', NULL, NULL, 'effective altruism')
-    ,('Marcus A. Davis', 'Rethink Charity', 'Senior Researcher', '2018-01-01', 'month', '2020-02-01', 'month', 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/marcusadavis1/', NULL, NULL, 'effective altruism')
+    ,('Catherine Low', 'Rethink Charity', 'Resource Coordinator', '2019-04-01', 'month', '2020-02-01', 'month', 'https://www.linkedin.com/in/catherine-low-076a0a70/?originalSubdomain=nz', NULL, NULL, 'effective altruism')/*closed*/
+    ,('David Vatousios', 'Rethink Charity', 'Communications Officer', '2017-09-01', 'month', '2018-02-01', 'month', 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/david-vatousios-aa796b160/?originalSubdomain=ca', NULL, NULL, 'effective altruism')/*closed*/
+    ,('Marcus A. Davis', 'Rethink Charity', 'Senior Researcher', '2018-01-01', 'month', '2020-02-01', 'month', 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/marcusadavis1/', NULL, NULL, 'effective altruism')/*closed*/
     ,('David Moss', 'Rethink Charity', 'Director of Research', '2018-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/david-moss-789a9616/?originalSubdomain=uk', NULL, NULL, 'effective altruism')
     ,('Michal Trzesimiech', 'Rethink Charity', 'Manager of LEAN', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, NULL, 'effective altruism')
-    ,('Michal Trzesimiech', 'Rethink Charity', 'Head Of Technology', '2018-08-01', 'month', '2020-01-01', 'month', 'https://www.linkedin.com/in/trzesimiech/?locale=en_US', NULL, 'part-time', 'effective altruism')
+    ,('Michal Trzesimiech', 'Rethink Charity', 'Head Of Technology', '2018-08-01', 'month', '2020-01-01', 'month', 'https://www.linkedin.com/in/trzesimiech/?locale=en_US', NULL, 'part-time', 'effective altruism')/*closed*/
     ,('Marcin Kowrygo', 'Rethink Charity', 'Strategy Officer', '2018-08-01', 'month', NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/ https://www.linkedin.com/in/kowrygo/?originalSubdomain=pl', NULL, NULL, 'effective altruism')
     ,('Ben Clifford', 'Rethink Charity', 'Key adviser', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Advisor', 'effective altruism')
     ,('Ozzie Gooen', 'Rethink Charity', 'Key adviser', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Advisor', 'effective altruism')
@@ -41,16 +60,18 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Sören Mindermann', 'Rethink Charity', 'Key volunteer', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Volunteer', 'effective altruism')
     ,('Erika Alonso', 'Rethink Charity', 'Key volunteer', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Volunteer', 'effective altruism')
     ,('Harysh Ilanghovan', 'Rethink Charity', 'Key volunteer', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Volunteer', 'effective altruism')
-    ,('Harysh Ilanghovan', 'Rethink Charity', 'Head Of Technology', '2015-01-01', 'year', '2017-01-01', 'year', 'https://www.linkedin.com/in/harysh/?originalSubdomain=in', NULL, NULL, 'effective altruism')
+    ,('Harysh Ilanghovan', 'Rethink Charity', 'Head Of Technology', '2015-01-01', 'year', '2017-01-01', 'year', 'https://www.linkedin.com/in/harysh/?originalSubdomain=in', NULL, NULL, 'effective altruism') /*closed*/
     ,('Caleb Ontiveros', 'Rethink Charity', 'Key volunteer', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190428234209/https://rtcharity.org/team/', NULL, 'Volunteer', 'effective altruism')
     ,('Litawn Gan', 'Rethink Charity', 'Data Consultant', '2020-08-01', NULL, NULL, NULL, 'https://www.linkedin.com/in/litawn/', NULL, 'Part-time', 'effective altruism')
-    ,('Megan Jamer', 'Rethink Charity', 'Communications Manager', '2021-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/meganjamer/', NULL, 'full-time', 'effective altruism')
-    ,('Aishwarya (Aisha) G.', 'Rethink Charity', 'Independent Contractor', '2021-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/missaishagurung/', NULL, 'contractor', 'effective altruism')
-    ,('Ilva Ravinska', 'Rethink Charity', 'Project Manager', '2021-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ilva-ravinska/', NULL, 'full-time', 'effective altruism')
-  
-
-
-
+    ,('Megan Jamer', 'Rethink Charity', 'Communications Manager', '2021-05-01', 'month', '2022-09-01', 'month', 'https://www.linkedin.com/in/meganjamer/', NULL, 'full-time', 'effective altruism') /*closed*/
+    ,('Megan Jamer', 'Rethink Charity', 'Acting Executive Director', '2022-07-01', 'month', '2022-12-01', 'month', 'https://www.linkedin.com/in/meganjamer/', NULL, 'full-time', 'effective altruism') /*closed*/
+    ,('Megan Jamer', 'Rethink Charity', 'Project Manager', '2022-12-01', 'month', '2023-09-01', 'month', 'https://www.linkedin.com/in/meganjamer/', NULL, 'full-time', 'effective altruism') /*closed*/
+    ,('Megan Jamer', 'Rethink Charity', 'Co Executive Director', '2023-09-01', 'month', '2024-06-01', 'month', 'https://www.linkedin.com/in/meganjamer/', NULL, 'full-time', 'effective altruism') /*closed*/
+    ,('Ilva Ravinska', 'Rethink Charity', 'Project Manager', '2021-01-01', 'month', '2022-06-01', 'month', 'https://www.linkedin.com/in/ilva-ravinska/', NULL, 'full-time', 'effective altruism') /*closed*/
+    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and COO at Students for High-Impact Charity', '2016-03-01', 'month', '2017-02-01', 'month', 'https://www.linkedin.com/in/teebarnett', NULL, NULL, 'effective altruism')/*closed*/
+    ,('Tee Barnett', 'Rethink Charity', 'Executive Director of Development', '2017-02-01', 'month', '2018-10-01', 'month', 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism')/*closed*/
+    ,('Tee Barnett', 'Rethink Charity', 'Co-founder and Senior Director of Growth & Strategy', '2018-10-01', 'month', '2020-12-01', 'month', 'https://www.linkedin.com/in/teebarnett/?originalSubdomain=cz', NULL, NULL, 'effective altruism') /*closed*/
+    ,('SoonKhen OwYong', 'Rethink Charity', 'Operations Engineer', '2022-05-01', 'month', '2022-11-01', 'month', 'https://www.linkedin.com/in/owyongsk/', NULL, 'Full-time', 'effective altruism') /*closed*/ 
 
 
 
