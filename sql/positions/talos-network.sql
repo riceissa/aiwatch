8f2bdc3c61ff9f6e68ae1a32afe89a24a3cf6278
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-01-27 00:28:31 -0300

    Uploading Gabriel Bobis file: talos-network.sql

---
+++ sql/positions/talos-network.sql
@@ -0,0 +1,25 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+
+  /* Reviewed on 2025-01-19 / / Last available Wayback snapshot date is 2025-01-19 */
+
+('Chiara Gerosa', 'Talos Network', 'Interim Executive Director', '2024-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chiaragerosa/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, 'intern', 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+,('David Conrad', 'Talos Network', 'Interim Managing Director', '2024-12-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/davidconrad93/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, 'intern', 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+,('Francesca Sheeka', 'Talos Network', 'EU Tech Policy Lead', '2024-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/fsheeka/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+,('Stephen Robcraft', 'Talos Network', 'Operations Manager', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+,('Carolin Basilowski', 'Talos Network', 'Network Strategy Lead', '2025-01-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/carolin-basilowski-9200921a4/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+,('Cillian Crosson', 'Talos Network', 'Board member', '2024-12-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/cillian-crosson/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, 'board member', 'AI safety') /* Reviewed on 2025-01-26 / / Last available Wayback snapshot date is 2025-01-23 */
+
+/* ONLY LINKEDIN */
+
+,('Nicoleta Kyosovska', 'Talos Network', 'EU AI Governance Fellow', '2024-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nicoleta-kyosovska-18b989153/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Lowe Lundin', 'Talos Network', 'Strategist', '2024-05-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/lowe-lundin/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Israel M.', 'Talos Network', 'EU AI Policy Fellow', '2024-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/israelfmw/', NULL, NULL, NULL, NULL, 'AI safety')
+
+/* CLOSED */
+
+;('Chiara Gerosa', 'Talos Network', 'Fellowship Lead', '2024-02-01', 'month', '2024-08-01', 'month', 'https://www.linkedin.com/in/chiaragerosa/', NULL, NULL, NULL, NULL, 'AI safety'),
+,('Chiara Gerosa', 'Talos Network', 'AI Fellowship Director', '2024-08-02', 'month', '2024-08-01', 'month', 'https://www.linkedin.com/in/chiaragerosa/', NULL, NULL, NULL, NULL, 'AI safety'),
+,('David Conrad', 'Talos Network', 'Talent Lead', '2023-11-01', 'month', '2024-12-01', 'month', 'https://www.linkedin.com/in/davidconrad93/', NULL, NULL, NULL, 'full-time', 'AI safety'),
+,('Carolin Basilowski', 'Talos Network', 'Operations & Strategy Associate', '2024-03-01', 'month', '2025-01-01', 'month', 'https://www.linkedin.com/in/carolin-basilowski-9200921a4/', NULL, NULL, NULL, NULL, 'AI safety'),
+,('Cillian Crosson', 'Talos Network', 'Interim Executive Director', '2024-01-01', 'month', '2024-12-01', 'month', 'https://www.linkedin.com/in/cillian-crosson/ https://web.archive.org/web/20250127012619/https://www.talosnetwork.org/about', NULL, NULL, NULL, 'intern', 'AI safety')
+,('Lowe Lundin', 'Talos Network', 'Special Projects Lead', '2024-01-01', 'month', '2024-05-01', 'month', 'https://www.linkedin.com/in/lowe-lundin/', NULL, NULL, NULL, NULL, 'AI safety')
