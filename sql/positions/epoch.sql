d518503bfd1fac00e1a6a80bd18d7520c0570a09
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-02-10 22:54:13 -0300

    Updating epoch.sql

--- sql/positions/epoch.sql
+++ sql/positions/epoch.sql
@@ -13,15 +13,17 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Alexander Erben', 'Epoch', 'Staff Researcher', '2024-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alexandererben/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, NULL, 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
     ,('Tom Adamczewski', 'Epoch', 'Machine Learning Research Engineer', '2024-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/thomasadamczewski/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, 'Full-time', 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
     ,('Elliot Glazer', 'Epoch', 'Lead mathematician', '2024-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/elliot-glazer-755190133/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, 'Full-time', 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
-
-
-
-
-
-
- 
+    ,('Robi Rahman', 'Epoch', 'Data Scientist', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/robirahman/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, 'Full-time', 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
+    ,('Josh You', 'Epoch', 'Data Analyst', '2023-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240722233330/https://epochai.org/team https://www.linkedin.com/in/josh-you-7b07a2102/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, 'Full-time', 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
+    ,('Luke Frymire', 'Epoch', 'Data Analyst', '2024-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/luke-frymire/ https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, NULL, 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
+    ,('Robert Sandler', 'organization', 'Product Designer', '2024-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240429231936/https://epochai.org/team https://web.archive.org/web/20250211000012/https://epoch.ai/about', NULL, NULL, 'AI safety')/*Last reviewed Wayback snapshot is from 2025-02-11*/ /*current position*/
 
  
+
+ 
+ 
+
+
 
 
 
@@ -33,14 +35,12 @@ insert into positions(person, organization, title, start_date, start_date_precis
     ,('Ege Erdil', 'Epoch', 'Technical Advisor', '2022-09-26', 'year', '2023-02-04', 'month', 'https://web.archive.org/web/20220926170336/https://epochai.org/team https://web.archive.org/web/20230204064434/https://epochai.org/team', NULL, NULL, 'AI safety')/*closed*/
     ,('Ege Erdil', 'Epoch', 'Technical Advisor', '2023-05-22', 'month', '2024-04-29', 'month', 'https://web.archive.org/web/20230522180129/https://epochai.org/team https://web.archive.org/web/20240429231936/https://epochai.org/team', NULL, NULL, 'AI safety')/*closed*/
     ,('Jenny Xiao', 'Epoch', 'Research Fellow', '2022-12-03', 'year', '2023-02-04', 'month', 'https://web.archive.org/web/20221203202954/https://epochai.org/team https://web.archive.org/web/20230204064434/https://epochai.org/team', NULL, NULL, 'AI safety') /*closed*/
-    ,('Josh You', 'Epoch', 'Data Analyst', '2023-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240722233330/https://epochai.org/team https://www.linkedin.com/in/josh-you-7b07a2102/', NULL, 'Full-time', 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
     ,('Keith Wynroe', 'Epoch', 'Research manager', '2022-12-03', 'year', '2023-02-04', 'month', 'https://web.archive.org/web/20221203202954/https://epochai.org/team https://web.archive.org/web/20230204064434/https://epochai.org/team', NULL, NULL, 'AI safety')/*closed*/
     ,('Lennart Heim', 'Epoch ', 'Research Fellow and Strategy Specialist', '2022-06-28', 'month', '2022-12-03', 'month', 'https://web.archive.org/web/20220628195022/https://epochai.org/team https://web.archive.org/web/20221203202954/https://epochai.org/team', NULL, NULL, 'AI safety')/*closed*/
     ,('Lennart Heim', 'Epoch ', 'Strategy Specialist', '2023-02-04', 'month', NULL, NULL, 'https://web.archive.org/web/20230204064434/https://epochai.org/team https://web.archive.org/web/20240722233330/https://epochai.org/team', NULL, NULL, 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
     ,('Maria da Lama', 'Epoch', 'Operations Coordinator', '2022-12-01', 'month', '2023-11-01', 'month', 'https://www.linkedin.com/in/merilalama/', NULL, NULL, 'AI safety')/*closed*/
     ,('Marius Hobbhahn', 'Epoch', 'Research Fellow', '2022-06-28', 'month', '2023-12-11', 'month', 'https://web.archive.org/web/20220628195022/https://epochai.org/team https://web.archive.org/web/20231211235451/https://epochai.org/team', NULL, NULL, 'AI safety')/*closed*/
     ,('Neil Thompson', 'Epoch ', 'Research Advisor', '2022-06-28', 'month', NULL, NULL, 'https://web.archive.org/web/20220628195022/https://epochai.org/team https://web.archive.org/web/20230930040549/https://epochai.org/team', NULL, 'Advisor', 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
-    ,('Robert Sandler', 'organization', 'Product Designer', '2024-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240429231936/https://epochai.org/team https://web.archive.org/web/20240722233330/https://epochai.org/team', NULL, NULL, 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
     ,('Tom Davidson', 'Epoch ', 'Research Advisor', '2022-06-28', 'month', NULL, NULL, 'https://web.archive.org/web/20220628195022/https://epochai.org/team https://web.archive.org/web/20230930040549/https://epochai.org/team', NULL, 'Advisor', 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
     ,('Virginia Blanton', 'Epoch', 'Leadership Advisor', '2023-11-28', 'month', NULL, NULL, 'https://web.archive.org/web/20231128000203/https://epochai.org/team https://web.archive.org/web/20240722233330/https://epochai.org/team', NULL, 'Advisor', 'AI safety')/*Reviewed on 2024-07-22*/ /*current position*/
 
