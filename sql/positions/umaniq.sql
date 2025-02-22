daa497931e6c61c43032cef83648eba249fa54e0
Sebastian <sebastian123sanchez@gmail.com>
2025-02-22 12:01:49 -0300

    adding Gabriel Bobis file: umaniq.sql

---
+++ sql/positions/umaniq.sql
@@ -0,0 +1,9 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+
+('Jens Meijen', 'Umaniq', 'Co-Founder', '2024-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20240716094804/https://umaniq.eu/ https://www.linkedin.com/in/jensmeijen/ https://web.archive.org/web/20250222002630/https://umaniq.eu/about-us', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-02-21 / / Last available Wayback snapshot date is 2025-02-21 */
+,('Koen Mathijs', 'Umaniq', 'Co-Founder', '2024-05-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240716094804/https://umaniq.eu/ https://www.linkedin.com/in/koen-mathijs-76552422/ https://web.archive.org/web/20250222002630/https://umaniq.eu/about-us', NULL, NULL, NULL, 'full-time', 'AI safety') /* Reviewed on 2025-02-21 / / Last available Wayback snapshot date is 2025-02-21 */
+,('Geoffrey Ceunen', 'Umaniq', 'Co-Founder', '2024-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240716094804/https://umaniq.eu/ https://www.linkedin.com/in/geoffreyceunen/ https://web.archive.org/web/20250222002630/https://umaniq.eu/about-us', NULL, NULL, NULL, 'full-time', 'AI safety') /* Reviewed on 2025-02-21 / / Last available Wayback snapshot date is 2025-02-21 */
+
+/* ONLY LINKEDIN */
+,('Nathalie Claes', 'Umaniq', 'Teacher', '2024-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nathalieclaes/', NULL, NULL, NULL, NULL, 'AI safety') 
+,('Erik Leung Shun', 'Umaniq', 'Teacher', '2024-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/erikleungpmp/', NULL, NULL, NULL, NULL, 'AI safety') 
