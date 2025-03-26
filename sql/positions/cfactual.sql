b2ad2a330c486fe90f7b33faf549457ce56afec0
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-03-26 16:56:28 -0300

    adding Gabriel Bobis file: cfactual.sql

---
+++ sql/positions/cfactual.sql
@@ -0,0 +1,16 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+ ('Jona Glade', 'cFactual', 'Executive Director', '2022-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230325103859/https://www.cfactual.com/ https://www.linkedin.com/in/jonaglade/ https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, 'full-time', 'Effective altruism')  /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */  
+,('Abraham Rowe', 'cFactual', 'Principal Operations', '2024-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/abrahamrowe/ https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, NULL, 'Effective altruism') /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */   
+,('Philip Mader', 'cFactual', 'Principal Family Offices', '2025-03-01', NULL, NULL, NULL, 'https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, NULL, 'Effective altruism')   /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */   
+,('Melissa Guzikowski', 'cFactual', 'People Operations Consultant', '2024-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/melissa-2b57a196/ https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, NULL, 'Effective altruism') /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */ 
+,('Tim Shavers', 'cFactual', 'Senior Advisor', '2024-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/tim-shavers-266a1411/ https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, 'advisor', 'Effective altruism') /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */ 
+,('Maximilian Negele', 'cFactual', 'AI Policy Affiliate', '2024-11-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/maximilian-negele/ https://web.archive.org/web/20250326130316/https://www.cfactual.com/about', NULL, 'part-time', 'Effective altruism') /* Reviewed on 2025-03-26 / / Last available Wayback snapshot date is 2025-03-26 */ 
+
+/* CLOSED */ 
+
+,('Maximilian Negele', 'cFactual', 'Head of AI Policy', '2022-12-01', 'month', '2024-11-01', 'month', 'https://web.archive.org/web/20230325103859/https://www.cfactual.com/ https://www.linkedin.com/in/maximilian-negele/', NULL, NULL, 'Effective altruism') 
+,('Callum Calver', 'cFactual', 'Expert in impact measuremen', '2023-03-01', NULL, '2023-09-01', NULL, 'https://web.archive.org/web/20230325103859/https://www.cfactual.com/ https://web.archive.org/web/20230929043537/https://www.cfactual.com/', NULL, 'contractor', 'Effective altruism') 
+,('Nils Völker', 'cFactual', 'Visiting Chief Operating Officer', '2023-03-01', NULL, '2023-09-01', NULL, 'https://web.archive.org/web/20230325103859/https://www.cfactual.com/ https://web.archive.org/web/20230929043537/https://www.cfactual.com/', NULL, NULL, 'Effective altruism') 
+
+
