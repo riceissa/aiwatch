b564be3040c13b80b2076eaef0ec313e462ab244
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-04-10 21:28:19 -0300

    Updating sentience-institute.sql

--- sql/positions/sentience-institute.sql
+++ sql/positions/sentience-institute.sql
@@ -1,16 +1,11 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
       ('Janet Pauketat', 'Sentience Institute', 'Research Fellow', '2021-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/janet-pauketat/ https://web.archive.org/web/20250410031129/https://www.sentienceinstitute.org/team', NULL, 'full-time', 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
-/*Spencer Case   Research Fellow*/
-
+    ,('Spencer Case', 'Sentience Institute', 'Research Fellow', '2023-07-05', 'month', NULL, NULL, 'https://web.archive.org/web/20230705200711/https://www.sentienceinstitute.org/team https://web.archive.org/web/20250410031129/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
     ,('Ali Ladak', 'Sentience Institute', 'Researcher', '2020-09-01', NULL, NULL, NULL, 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team https://web.archive.org/web/20250410031129/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
-
-
-/*Katerina Manoli Researcher*/
-
-
-    ,('Jacy Reese Anthis', 'Sentience Institute', 'Co-Founder & Research Fellow', '2016-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20170606224624/http://www.sentienceinstitute.org/team https://www.linkedin.com/in/jacyreeseanthis', NULL, NULL, 'effective altruism')
-    ,('Kelly Anthis', 'Sentience Institute', 'Co-Founder & Advisory Board Member', '2017-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211207190553/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/kellywitwicki/', NULL, 'Board Member', 'effective altruism')
+    ,('Katerina Manoli', 'Sentience Institute', 'Researcher', '2022-12-02', 'year', NULL, NULL, 'https://web.archive.org/web/20221202105715/https://www.sentienceinstitute.org/team https://web.archive.org/web/20250410031129/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
+    ,('Jacy Reese Anthis', 'Sentience Institute', 'Co-Founder', '2016-12-01', 'month', NULL, NULL, 'https://web.archive.org/web/20170606224624/http://www.sentienceinstitute.org/team  https://web.archive.org/web/20211207190553/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
+    ,('Kelly Anthis', 'Sentience Institute', 'Co-Founder', '2017-03-01', 'month', NULL, NULL, 'https://web.archive.org/web/20211207190553/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/kellywitwicki/ https://web.archive.org/web/20211207190553/https://www.sentienceinstitute.org/team', NULL, 'Board Member', 'effective altruism') /*Last reviewed Wayback snapshot is from 2025-04-10*/ /*current position*/
 
 
 
@@ -20,16 +15,12 @@ insert into positions(person, organization, title, start_date, start_date_precis
 
 
 
-    ,('Kelly Witwicki', 'Sentience Institute', 'Board member', '2017-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
-    ,('Natalie Cargill', 'Sentience Institute', 'Advisory Board Member', '2018-11-01', NULL, NULL, NULL, 'https://web.archive.org/web/20181117004617/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
-    ,('Oscar Horta', 'Sentience Institute', 'Advisory Board Member', '2018-11-01', NULL, NULL, NULL, 'https://web.archive.org/web/20181117004617/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
-    ,('Kelly McNamara', 'Sentience Institute', 'Advisory Board Member', '2019-04-01', NULL, NULL, NULL, 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/kelly-anne-mcnamara/', NULL, 'Advisor', 'effective altruism')
-    ,('J. Mohorčich', 'Sentience Institute', 'Research Fellow', '2017-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') 
-    ,('J. Mohorčich', 'Sentience Institute', 'Advisory Board Member', '2020-09-01', NULL, NULL, NULL, 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
-    ,('Janet Pauketat', 'Sentience Institute', 'Advisory Board Member', '2020-09-01', NULL, NULL, NULL, 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
 
-    ,('Diana Fleischman', 'Sentience Institute', 'Board Member', '2017-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
-    ,('Jonas Vollmer', 'Sentience Institute', 'Board member', '2017-12-01', NULL, NULL, NULL, 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
+
+ 
+
+
+
     ,('Jeff Sebo', 'Sentience Institute', 'Advisory Board Member', '2018-11-01', NULL, NULL, NULL, 'https://web.archive.org/web/20181117004617/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
     ,('Max Daniel', 'Sentience Institute', 'Board member', '2019-08-01', NULL, NULL, NULL, 'https://web.archive.org/web/20190805205904/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism')
 
@@ -42,12 +33,11 @@ insert into positions(person, organization, title, start_date, start_date_precis
      ,('Jamie Harris', 'Sentience Institute', 'Researcher', '2018-08-01', 'month', '2022-02-01', 'month', 'https://web.archive.org/web/20180823042042/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/jamie-harris-14b011138/', NULL, 'part-time', 'effective altruism') /*closed*/
     ,('Kelly Witwicki', 'Sentience Institute', 'Co-Founder & Executive Director', '2017-03-01', 'month', '2019-12-01', 'month', 'https://web.archive.org/web/20170606224624/http://www.sentienceinstitute.org/team https://www.linkedin.com/in/kellywitwicki/', NULL, NULL, 'effective altruism') /*closed*/
     ,('Michael Dello-Iacovo', 'Sentience Institute', 'Strategy Lead and Researcher', '2022-04-01', 'month', '2024-03-01', 'month', 'https://web.archive.org/web/20220430195630/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/michael-dello-iacovo/', NULL, NULL, 'effective altruism') /*closed*/
-
-
-
-
-
-
-
-
-
+    ,('Kelly Witwicki', 'Sentience Institute', 'Board member', '2017-12-01', 'multi-year', '2019-08-05', 'year', 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team https://web.archive.org/web/20190805205904/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism') /*closed*/
+    ,('Natalie Cargill', 'Sentience Institute', 'Advisory Board Member', '2018-11-01', NULL, '2018-11-22', 'year', 'https://web.archive.org/web/20181117004617/https://www.sentienceinstitute.org/team https://web.archive.org/web/20181122215420/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism') /*closed*/
+    ,('Oscar Horta', 'Sentience Institute', 'Advisory Board Member', '2018-11-01', NULL, '2024-10-01', 'year', 'https://web.archive.org/web/20181117004617/https://www.sentienceinstitute.org/team https://web.archive.org/web/20241001010231/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism') /*closed*/
+    ,('Kelly McNamara', 'Sentience Institute', 'Advisory Board Member', '2019-04-01', NULL, '2024-10-01', 'year', 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team https://www.linkedin.com/in/kelly-anne-mcnamara/ https://web.archive.org/web/20241001010231/https://www.sentienceinstitute.org/team', NULL, 'Advisor', 'effective altruism') /*closed*/
+    ,('J. Mohorčich', 'Sentience Institute', 'Advisory Board Member', '2020-09-01', NULL, '2024-10-01', 'year', 'https://web.archive.org/web/20200930035610/https://www.sentienceinstitute.org/team https://web.archive.org/web/20241001010231/https://www.sentienceinstitute.org/team', NULL, 'Advisor', 'effective altruism') /*closed*/
+    ,('J. Mohorčich', 'Sentience Institute', 'Research Fellow', '2017-12-01', NULL, '2019-08-05', 'year', 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team https://web.archive.org/web/20190805205904/https://www.sentienceinstitute.org/team', NULL, NULL, 'effective altruism') /*closed*/ 
+    ,('Diana Fleischman', 'Sentience Institute', 'Board Member', '2017-12-01', NULL, '2019-08-05', 'year', 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team https://web.archive.org/web/20190805205904/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism') /*closed*/ 
+    ,('Jonas Vollmer', 'Sentience Institute', 'Board member', '2017-12-01', NULL, '2018-11-22', 'year', 'https://web.archive.org/web/20171229231806/https://www.sentienceinstitute.org/team https://web.archive.org/web/20181122215420/https://www.sentienceinstitute.org/team', NULL, 'Board member', 'effective altruism') /*closed*/ 
