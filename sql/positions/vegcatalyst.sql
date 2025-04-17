db02317b855dceedf1c414634f94b8ced96eacde
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-04-17 00:10:44 -0300

    uploading Gabriel Bobis file:vegcatalist.sql

---
+++ sql/positions/vegcatalyst.sql
@@ -0,0 +1,9 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+ ('Ahn Michael', 'VegCatalyst', 'Director of Natural Language Processing', '2023-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/amanimichael/ https://web.archive.org/web/20250416223417/https://veg3.ai/page/ourmission', NULL, NULL, 'Animal welfare') /* Reviewed on 2025-04-16 / / Last available Wayback snapshot date is 2025-04-16 */  
+,('Sam Tucker-Davis', 'VegCatalyst', 'Chief Executive Officer', '2021-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/sam-tucker-vegan/ https://web.archive.org/web/20250416223417/https://veg3.ai/page/ourmission', NULL, 'full-time', 'Animal welfare') /* Reviewed on 2025-04-16 / / Last available Wayback snapshot date is 2025-04-16 */  
+,('Maddie Tucker-Davis', 'VegCatalyst', 'Co-Founder', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/maddie-tucker-davis-a2134a79/ https://web.archive.org/web/20250416223417/https://veg3.ai/page/ourmission', NULL, NULL, 'Animal welfare') /* Reviewed on 2025-04-16 / / Last available Wayback snapshot date is 2025-04-16 */  
+,('Eceo Brickle', 'VegCatalyst', 'Web Development', '2023-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/eceo-brickle-%E2%93%A5-278a52210/ https://web.archive.org/web/20250416223417/https://veg3.ai/page/ourmission', NULL, NULL, 'Animal welfare') /* Reviewed on 2025-04-16 / / Last available Wayback snapshot date is 2025-04-16 */  
+
+
+
