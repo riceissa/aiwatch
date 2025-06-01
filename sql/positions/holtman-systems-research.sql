f487cbe43d9e7d5d06aa11e71b0871eb38965f98
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-05-31 23:45:41 -0300

    uploading Gabriel Bobis file: holtman-systems-research.sql

---
+++ sql/positions/holtman-systems-research.sql
@@ -0,0 +1,4 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+
+     ('Koen Holtman', 'Holtman Systems Research', 'Founder', '2022-11-01', 'month', NULL, NULL, 'https://web.archive.org/web/20221130204431/https://holtmansystemsresearch.nl/ https://www.linkedin.com/in/koen-holtman-2312844/ https://web.archive.org/web/20250504021253/https://holtmansystemsresearch.nl/', NULL, NULL, NULL, 'full-time', 'AI safety')  /* Reviewed on 2025-05-04 / / Last available Wayback snapshot date is 2025-05-04 */
+   
