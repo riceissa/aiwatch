f02ae2809bfd3b25d958141b9523cc6c97ac0893
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-06-17 23:16:57 -0300

    uploading Gabriel Bobis file: ai-partners.sql

---
+++ sql/positions/ai-partners.sql
@@ -0,0 +1,28 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+
+ ('Sean Musch', 'AI & Partners', 'Chief Executive Officer', '2021-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230323030254/https://www.ai-and-partners.com/team https://www.linkedin.com/in/sean-musch-122ba1a7/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, 'full-time', 'AI safety')  /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Michael Borrelli', 'AI & Partners', 'Director', '2021-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230323030254/https://www.ai-and-partners.com/team https://www.linkedin.com/in/michael-charles-borrelli-6a557253/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, NULL, 'AI safety')   /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Jim Duffy', 'AI & Partners', 'Advisor', '2024-07-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240702184746/https://www.ai-and-partners.com/team https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, 'advisor', 'AI safety') /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Charles Kerrigan', 'AI & Partners', 'Advisor', '2024-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20240702184746/https://www.ai-and-partners.com/team https://www.linkedin.com/in/charles-kerrigan/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, 'advisor', 'AI safety') /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Doug Hohulin', 'AI & Partners', 'Business Associate', '2023-06-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230616031842/https://www.ai-and-partners.com/team https://www.linkedin.com/in/doughohulin/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, 'associate', 'AI safety') /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Mike Ellis', 'AI & Partners', 'Non-Executive Director', '2023-02-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230616031842/https://www.ai-and-partners.com/team https://www.linkedin.com/in/michael-ellis-31104832/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, 'part-time', 'AI safety') /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+,('Indranil Nath', 'AI & Partners', 'Non-Executive Director', '2021-10-01', 'month', NULL, NULL, 'https://web.archive.org/web/20230616031842/https://www.ai-and-partners.com/team https://www.linkedin.com/in/indranil/ https://web.archive.org/web/20250617223002/https://www.ai-and-partners.com/about-us', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-06-17 / / Last available Wayback snapshot date is 2025-06-17 */
+
+/* Only Linkedin */
+
+,('Rakshita G. Ohri', 'AI & Partners', 'Legal Intern', '2025-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/rakshita-g-ohri-a017a7245/', NULL, NULL, NULL, 'intern', 'AI safety')
+,('Barry Van der Ven', 'AI & Partners', 'Software-ingenieur', '2023-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/barry-van-der-ven-000169296/', NULL, NULL, NULL, 'full-time', 'AI safety')
+,('Alexander Vermeulen', 'AI & Partners', 'Advisor', '2023-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alexander-vermeulen/', NULL, NULL, NULL, 'advisor', 'AI safety')
+,('Tim Carswell', 'AI & Partners', 'Collaboration Partner', '2025-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/tim-carswell/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Daniel Flynn', 'AI & Partners', 'Sales & Brand Director', '2024-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/daniel-flynn-163265b9/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Bruna Nunes', 'AI & Partners', 'Strategic Collaboration Partner', '2025-03-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/brunanunesramos/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Eva Dias Costa', 'AI & Partners', 'External Collaborator', '2025-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/evadiascosta/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Ingrid Vasiliu-Feltes', 'AI & Partners', 'Co-Author', '2024-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/ingrid-vasiliu-feltes-mdmba/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Nazareth Q.', 'AI & Partners', 'Social Media Ambassador', '2025-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/nazareth-q-02a53b22/', NULL, NULL, NULL, NULL, 'AI safety')
+,('Wong F.', 'AI & Partners', 'AI Ethics and Regulation Tech', '2023-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/fabianwkp/', NULL, NULL, NULL, NULL, 'AI safety')
+
+/* Closed */
+
+,('Tirath Virdee', 'AI & Partners', 'Advisor', '2023-03-01', NULL, '2023-06-01', 'year', 'https://web.archive.org/web/20230323030254/https://www.ai-and-partners.com/team', NULL, NULL, NULL, 'advisor', 'AI safety') 
+,('Tijs Maas', 'AI & Partners', 'Chief Technology Officer', '2024-07-01', 'month', '2024-09-01', 'month', 'https://web.archive.org/web/20240702184746/https://www.ai-and-partners.com/team https://web.archive.org/web/20240920032538/https://www.ai-and-partners.com/team', NULL, NULL, NULL, 'advisor', 'AI safety') 
+
