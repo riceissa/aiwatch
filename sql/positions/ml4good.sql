733769e71e5992ffa02fbce94c28aef5bbd26e4c
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-06-18 22:28:05 -0300

    uploading Gabriel Bobis file: ml4good.sql

---
+++ sql/positions/ml4good.sql
@@ -0,0 +1,20 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+
+
+
+,('Charbel-Raphael Segerie', 'ML4Good', 'Advisor', '2025-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20250517185942/https://www.ml4good.org/about https://web.archive.org/web/20250618221313/https://www.ml4good.org/about', NULL, NULL, NULL, 'advisor', 'AI safety') /* Reviewed on 2025-06-18 / / Last available Wayback snapshot date is 2025-06-18 */    
+,('Nia Gardner', 'ML4Good', 'Executive Director', '2025-01-01', NULL, NULL, NULL, 'https://web.archive.org/web/20250517185942/https://www.ml4good.org/about https://web.archive.org/web/20250618221313/https://www.ml4good.org/about', NULL, NULL, NULL, NULL, 'AI safety') /* Reviewed on 2025-06-18 / / Last available Wayback snapshot date is 2025-06-18 */    
+,('Carolina Oliveira', 'ML4Good', 'Director of Projects', '2025-01-01', 'month', NULL, NULL, 'https://web.archive.org/web/20250517185942/https://www.ml4good.org/about https://www.linkedin.com/in/carolinaollive/ https://web.archive.org/web/20250618221313/https://www.ml4good.org/about', NULL, NULL, NULL, NULL, 'AI safety')   /* Reviewed on 2025-06-18 / / Last available Wayback snapshot date is 2025-06-18 */  
+
+/* Only Linkedin*/  
+
+,('Michał Kubiak', 'ML4Good', 'AI Governance Teacher', '2025-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/michalkubiaklodz/', NULL, NULL, NULL, NULL, 'AI safety')   
+,('Linda Broglio', 'ML4Good', 'Operations Manager', '2025-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/linda-broglio-2449b02b1/', NULL, NULL, NULL, NULL, 'AI safety')   
+,('Jonathan Mannhart', 'ML4Good', 'Co-Organiser', '2024-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jonathan-mannhart/', NULL, NULL, NULL, 'part-time', 'AI safety')   
+,('Jonathan Claybrough', 'ML4Good', 'Teacher', '2023-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jonathan-claybrough-733a68125/', NULL, NULL, NULL, 'part-time', 'AI safety')  
+
+/* Closed */  
+
+,('Linda Broglio', 'ML4Good', 'Operations Associate ', '2025-05-01', 'month', '2025-05-02', 'month', 'https://www.linkedin.com/in/linda-broglio-2449b02b1/', NULL, NULL, NULL, NULL, 'AI safety')  
+,('Carolina Oliveira', 'ML4Good', 'Partnership support, Design tinkering', '2024-10-01', 'month', '2025-01-01', 'month', 'https://www.linkedin.com/in/carolinaollive/', NULL, NULL, NULL, NULL, 'AI safety')   
+
