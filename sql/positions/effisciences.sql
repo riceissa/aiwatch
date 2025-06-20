1723c2c699ebdeb5dd67db934c2b58115a26c3f8
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-06-19 21:08:04 -0300

    uploading Gabriel Bobis file: effisciences.sql

---
+++ sql/positions/effisciences.sql
@@ -0,0 +1,22 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+
+ ('Florent Berthet', 'EffiSciences', 'Executive Director', '2023-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/berthetflorent/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention') /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Lola Elissalde', 'EffiSciences', 'Administrative and Communication', '2022-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lola-elissalde-a4bb29241/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention') /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Charbel-Raphaël Ségerie', 'EffiSciences', 'Head of AI Unit', '2025-06-01', NULL, NULL, NULL, 'https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention') /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Diane Letourneur', 'EffiSciences', 'Head of Biorisk Unit', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/diane-letourneur-396a7a25a/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention')  /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Jérémy Andréoletti', 'EffiSciences', 'Head of mission-driven research Unit', '2024-03-01', 'month', NULL, NULL, 'http://linkedin.com/in/jeremy-andreoletti-330445216/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention')  /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Manuel Bimich', 'EffiSciences', 'AI Strategy Director', '2024-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/manuel-bimich/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, NULL, 'Existential risk prevention')  /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Jonathan Claybrough', 'EffiSciences', 'Board Member', '2023-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jonathan-claybrough-733a68125/ https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, 'board member', 'Existential risk prevention')  /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+,('Antoine Sérandour', 'EffiSciences', 'Board Member', '2025-06-01', NULL, NULL, NULL, 'https://web.archive.org/web/20250619202718/https://effisciences.org/en/about-us', NULL, 'board member', 'Existential risk prevention') /* Reviewed on 2025-06-19 / / Last available Wayback snapshot date is 2025-06-19 */
+
+/* LINKEDIN */
+
+,('Michel Brun', 'EffiSciences', 'Member of the Biorisks division', '2024-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/michel-brun-471467229/', NULL, NULL, 'Existential risk prevention')
+,('Leo Karoubi', 'EffiSciences', 'Mission-driven research unit, Operationnal and IT support', '2023-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/leo-karoubi/', NULL, NULL, 'Existential risk prevention')
+,('Léo Dana', 'EffiSciences', 'Volunteer', '2022-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/l%C3%A9o-dana-b169b7207/', NULL, 'volunteer', 'Existential risk prevention')
+
+/* CLOSED */
+
+,('Manuel Bimich', 'EffiSciences', 'Board Member', '2023-10-01', 'month', '2024-03-01', 'month', 'https://www.linkedin.com/in/manuel-bimich/', NULL, 'board member', 'Existential risk prevention')
+,('Manuel Bimich', 'EffiSciences', 'Advisor', '2023-05-01', 'month', '2023-11-01', 'month', 'https://www.linkedin.com/in/manuel-bimich/', NULL, 'advisor', 'Existential risk prevention')
