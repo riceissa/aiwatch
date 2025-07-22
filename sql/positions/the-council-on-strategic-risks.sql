f0272cec67ea374cead5724f8e57c77da2d392d9
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-07-22 00:57:15 -0300

    uploading Gabriel Bobis file: the-council-on-strategic-risks.sql

---
+++ sql/positions/the-council-on-strategic-risks.sql
@@ -0,0 +1,47 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+ /* Reviewed on 2025-05-11 / / Last available Wayback snapshot date is 2025-05-11 */
+
+('Christine Parthemore', 'The Council on Strategic Risks', 'Chief Executive Officer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Mallory Stewart', 'The Council on Strategic Risks', 'Executive Vice President', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Catherine Dill', 'The Council on Strategic Risks', 'Vice President of Programs', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Scott Stinson', 'The Council on Strategic Risks', 'Chief Operating Officer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Francesco Femia', 'The Council on Strategic Risks', 'Co-Founder & Director of Research', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Andrew Facini', 'The Council on Strategic Risks', 'Director of Communications', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Courtney Tillman', 'The Council on Strategic Risks', 'Communications Associate', '2022-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/courtney-tillman/ https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, 'full-time', 'Existential risk prevention'),
+,('Caitlin Werrell', 'The Council on Strategic Risks', 'Co-Founder and Senior Advisor, CSR and The Center for Climate and Security', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Erin Sikorsky', 'The Council on Strategic Risks', 'Director, Center for Climate and Security', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Tom Ellison', 'The Council on Strategic Risks', 'Deputy Director, The Center for Climate and Security', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Noah Fritzhand', 'The Council on Strategic Risks', 'Research Fellow, The Center for Climate and Security', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Siena Cicarelli', 'The Council on Strategic Risks', 'Fellow, The Center for Climate and Security', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('John Moulton (USN, Ret.)', 'The Council on Strategic Risks', 'Deputy Director and Senior Fellow, The Janne E. Nolan Center on Strategic Weapons', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Christopher East', 'The Council on Strategic Risks', 'Senior Fellow and Program Manager, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Crystal Grant', 'The Council on Strategic Risks', 'Senior Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Daniel P. Regan', 'The Council on Strategic Risks', 'Senior Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Jess Rogers', 'The Council on Strategic Risks', 'Senior Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Andrew Weber', 'The Council on Strategic Risks', 'Senior Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Jackson du Pont', 'The Council on Strategic Risks', 'Research Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Kyle Wilgus', 'The Council on Strategic Risks', 'Research Fellow, Nolan Center', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Caroline Baxter', 'The Council on Strategic Risks', 'Director, The Converging Risks Lab', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention'),
+,('Lily Boland', 'The Council on Strategic Risks', 'Strategic Foresight Fellow, The Converging Risks Lab', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250615044224/https://councilonstrategicrisks.org/experts/', NULL, NULL, 'Existential risk prevention')
+
+,('Sherri Goodman', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Marcus D. King', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('John C. Cruden', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Bob Barnes', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Deborah C. Gordon', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Rose Gottemoeller', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Dan Levine', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Robert J. Palay', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Daniel Poneman', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Glenn Prickett', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Rajeev Surati', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Janet Wu', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention'),
+,('Janne E. Nolan', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250612025320/https://councilonstrategicrisks.org/board-of-directors/', NULL, 'board member', 'Existential risk prevention')
+
+
+/*.......ONLY linkedin.....*/
+
+,('Brigitte Hugh', 'The Council on Strategic Risks', 'Board Member', NULL, NULL, NULL, NULL, 'https://www.linkedin.com/in/brigittehugh/', NULL, 'board member', 'Existential risk prevention')
+
+
