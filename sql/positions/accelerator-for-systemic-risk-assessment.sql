d88c6e540fa0c1e442cbd5446831c5b28948347b
Sebastian <sebastian123sanchez@gmail.com>
2025-08-04 08:40:34 -0300

    Adding Gabriel Bobis file: accelerator-for-systemic-risk-assessment.sql

---
+++ sql/positions/accelerator-for-systemic-risk-assessment.sql
@@ -0,0 +1,13 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+
+  /* Reviewed on 2025-07-31 / / Last available Wayback snapshot date is 2025-07-31 */
+
+
+('Ruth Richardson', 'Accelerator for Systemic Risk Assessment', 'Executive Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Ajay Gambhir', 'Accelerator for Systemic Risk Assessment', 'Director of Systemic Risk Assessment', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Hanna Asipovich', 'Accelerator for Systemic Risk Assessment', 'Director of Network Facilitation', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Sarah Hendel-Blackford', 'Accelerator for Systemic Risk Assessment', 'Director of Systemic Risk Policy and Response', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Zabrina Kjeldsen', 'Accelerator for Systemic Risk Assessment', 'Operations Manager', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Kasia Murphy', 'Accelerator for Systemic Risk Assessment', 'Director of Communications', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention'),
+('Phil Tovey', 'Accelerator for Systemic Risk Assessment', 'Director, Nature-Centric Approaches', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250801005837/https://www.asranetwork.org/who-we-are', NULL, NULL, 'Existential risk prevention');
