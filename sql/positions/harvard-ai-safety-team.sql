bf4f749e7077461a1e38b4907f388afec6a847ea
Sebastian <sebastian123sanchez@gmail.com>
2025-08-04 08:55:01 -0300

    Adding Gabriel Bobis file: harvard-ai-safety-team..sql

---
+++ sql/positions/harvard-ai-safety-team.sql
@@ -0,0 +1,16 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values   
+    
+  /* Reviewed on 2025-07-16 / / Last available Wayback snapshot date is 2025-07-16 */
+
+
+('Salvador Cole', 'Harvard AI Safety Team', 'Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Hillel Ehrenreich', 'Harvard AI Safety Team', 'Deputy Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Kevin Wei', 'Harvard AI Safety Team', 'Director of Governance Programs', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Ryan Kaufman', 'Harvard AI Safety Team', 'Director of Technical Programs', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Carson Ezell', 'Harvard AI Safety Team', 'Policy Fellowship Co-Lead, Treasurer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('JJ Andrade', 'Harvard AI Safety Team', 'Technical Fellowship Lead', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Rod Moshtagi', 'Harvard AI Safety Team', 'Workshops Lead', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Romeo Dean', 'Harvard AI Safety Team', 'AI Career and Strategy Lead', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Hannah Kim', 'Harvard AI Safety Team', 'Research Coordinator', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety'),
+('Gabriel Wu', 'Harvard AI Safety Team', 'Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250716140817/https://haist.ai/team', NULL, NULL, NULL, NULL, 'AI safety')
+
