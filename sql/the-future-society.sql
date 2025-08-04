7ffc7eaba08b7d221d9cba3e2baa28dc469e7c25
Sebastian <sebastian123sanchez@gmail.com>
2025-08-04 08:32:16 -0300

    Adding Gabriel Bobis file: the-future-society.sql

---
+++ sql/the-future-society.sql
@@ -0,0 +1,24 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values   
+    
+  /* Reviewed on 2025-07-01 / / Last available Wayback snapshot date is 2025-07-01*/
+
+
+('Nick Moës', 'The Future Society', 'Executive Director & Secretary of the Board', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Ellen O’Connell', 'The Future Society', 'Chief Operating Officer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Niki Iliadis', 'The Future Society', 'Director, Global AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Caroline Jeanmaire', 'The Future Society', 'Interim Director, U.S. AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Toni Lorente', 'The Future Society', 'Interim Director, European AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Sam Baskeyfield', 'The Future Society', 'Operations Associate', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Eloise Dunn', 'The Future Society', 'Operations Manager', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Kathrin Gardhouse', 'The Future Society', 'Visiting Research Fellow, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('George Gor', 'The Future Society', 'Associate, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Delfina Belli', 'The Future Society', 'Associate, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Caio Vieira Machado', 'The Future Society', 'Senior Associate, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Tereza Zoumpalova', 'The Future Society', 'Associate, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Anoush Rima Tatevossian', 'The Future Society', 'Senior Associate, Stakeholder Engagement', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Mai Lynn Miller Nguyen', 'The Future Society', 'Head of Communications', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Amin Oueslati', 'The Future Society', 'Senior Associate, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Jonathan Schmidt', 'The Future Society', 'Analyst, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Robin Staes-Polet', 'The Future Society', 'Analyst, AI Governance', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Yolanda Lannquist', 'The Future Society', 'Senior Advisor', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety'),
+('Nicolas Economou', 'The Future Society', 'Chair, AI and the Rule of Law – Volunteer', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250701060146/https://thefuturesociety.org/our-team/', NULL, NULL, NULL, NULL, 'AI safety')
