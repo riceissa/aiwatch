8b5745a603f820ae20723f4168baec113f9d6b1c
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-09-04 10:30:58 -0300

    fixing cellular-agriculture-australia.sql

--- sql/positions/cellular-agriculture-australia.sql
+++ sql/positions/cellular-agriculture-australia.sql
@@ -1,7 +1,2 @@
 insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
-
-
-  /* Reviewed on 2025-07-11 / / Last available Wayback snapshot date is 2025-07-11 */
-
-
-('Ahmed Khan', 'Cellular Agriculture Australia', 'Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250711013642/https://www.cell.ag/about', NULL, NULL, 'Animal welfare'),
+('Ahmed Khan', 'Cellular Agriculture Australia', 'Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20250711013642/https://www.cell.ag/about', NULL, NULL, 'Animal welfare')   /* Reviewed on 2025-07-11 / / Last available Wayback snapshot date is 2025-07-11 */
