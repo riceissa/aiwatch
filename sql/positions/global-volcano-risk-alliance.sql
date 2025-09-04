7b9bcdf0c4f7373da64c8351b75768dc54e70b4b
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2025-09-04 17:12:53 -0300

    adding Gabriel Bobis file: global-volcano-risk-alliance.sql

---
+++ sql/positions/global-volcano-risk-alliance.sql
@@ -0,0 +1,7 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+
+
+
+ ('Lara Mani', 'Global Volcano Risk Alliance', 'Co-Founder', '2024-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lara-mani-9b23a228/', NULL, NULL, 'Existential risk prevention')  /* Reviewed on 2025-08-01 / / Last available Wayback snapshot date is 2025-08-01 */
+,('Mike Cassidy', 'Global Volcano Risk Alliance', 'Co-Founder', '2024-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/mike-cassidy-86a7161b8/', NULL, NULL, 'Existential risk prevention')  /* Reviewed on 2025-08-01 / / Last available Wayback snapshot date is 2025-08-01 */
