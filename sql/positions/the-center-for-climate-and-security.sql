2aa55eec0c4ee280a3dd8a4c6e6fc942c2c0f67d
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-02-04 17:20:32 -0300

    fixing the-center-for-climate-and-security.sql

--- sql/positions/the-center-for-climate-and-security.sql
+++ sql/positions/the-center-for-climate-and-security.sql
@@ -1,41 +1,41 @@
-insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+insert into positions,(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
  /* Reviewed on 2025-08-01 / / Last available Wayback snapshot date is 2025-08-01 */
 
-('Erin Sikorsky', 'The Center for Climate and Security', 'Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Tom Ellison', 'The Center for Climate and Security', 'Deputy Director', '2022-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/123tomellison/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, 'full-time', 'Politics'),
-('John Conger', 'The Center for Climate and Security', 'Director Emeritus', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Siena Cicarelli', 'The Center for Climate and Security', 'Research Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Noah Fritzhand', 'The Center for Climate and Security', 'Research Fellow', '2024-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/noah-fritzhand/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, 'full-time', 'Politics'),
-('Annalise Blum', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Joshua Busby', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Duncan Depledge', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Shiloh Fetzek', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Rachel Fleishman', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Heidi Hardt', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Cullen Hendrix', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Eric Hunt', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Marc Kodack', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Marcus King', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Jennie King', 'The Center for Climate and Security', 'CCS Non-resident Fellow', '2025-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jneking/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Stephanie Leutert', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Marisol Maddox', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Genevieve Maricle', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('David Michel', 'The Center for Climate and Security', 'CCS Non-resident Fellow', '2019-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/david-michel-0108b532/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Kimberly Miner', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Scott Moore', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Alexander Nassikas', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Peter Schwartzstein', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Michael Thomas', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Ann Vaughan', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Sherri Goodman', 'The Center for Climate and Security', 'Secretary General, IMCCS', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Francesco Femia', 'The Center for Climate and Security', 'CCS Co-Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
-('Caitlin E. Werrell', 'The Center for Climate and Security', 'CCS Co-Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics')
+ ('Erin Sikorsky', 'The Center for Climate and Security', 'Director', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Tom Ellison', 'The Center for Climate and Security', 'Deputy Director', '2022-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/123tomellison/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, 'full-time', 'Politics'),
+,('John Conger', 'The Center for Climate and Security', 'Director Emeritus', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Siena Cicarelli', 'The Center for Climate and Security', 'Research Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Noah Fritzhand', 'The Center for Climate and Security', 'Research Fellow', '2024-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/noah-fritzhand/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, 'full-time', 'Politics'),
+,('Annalise Blum', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Joshua Busby', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Duncan Depledge', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Shiloh Fetzek', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Rachel Fleishman', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Heidi Hardt', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Cullen Hendrix', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Eric Hunt', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Marc Kodack', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Marcus King', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Jennie King', 'The Center for Climate and Security', 'CCS Non-resident Fellow', '2025-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jneking/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Stephanie Leutert', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Marisol Maddox', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Genevieve Maricle', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('David Michel', 'The Center for Climate and Security', 'CCS Non-resident Fellow', '2019-05-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/david-michel-0108b532/ https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Kimberly Miner', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Scott Moore', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Alexander Nassikas', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Peter Schwartzstein', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Michael Thomas', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Ann Vaughan', 'The Center for Climate and Security', 'CCS Non-resident Fellow', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Sherri Goodman', 'The Center for Climate and Security', 'Secretary General, IMCCS', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Francesco Femia', 'The Center for Climate and Security', 'CCS Co-Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics'),
+,('Caitlin E. Werrell', 'The Center for Climate and Security', 'CCS Co-Founder', NULL, NULL, NULL, NULL, 'https://web.archive.org/web/20260127000250/https://climateandsecurity.org/staff/', NULL, NULL, 'Politics')
 
  
  /*..LINKEDIN..*/
- ('Kimberley R. Miner', 'The Center for Climate and Security', 'Research Fellow', '2015-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/drkimberleyminer/', NULL, NULL, 'Politics')
- ('Chris Kurowski', 'The Center for Climate and Security', 'Working Group Member', '2017-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chris-kurowski-a2a29425/', NULL, NULL, 'Politics')
- ('Chris Kurowski', 'The Center for Climate and Security', 'Working Group Member', '2017-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chris-kurowski-a2a29425/', NULL, NULL, 'Politics')
+ ,('Kimberley R. Miner', 'The Center for Climate and Security', 'Research Fellow', '2015-10-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/drkimberleyminer/', NULL, NULL, 'Politics')
+ ,('Chris Kurowski', 'The Center for Climate and Security', 'Working Group Member', '2017-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chris-kurowski-a2a29425/', NULL, NULL, 'Politics')
+ ,('Chris Kurowski', 'The Center for Climate and Security', 'Working Group Member', '2017-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chris-kurowski-a2a29425/', NULL, NULL, 'Politics')
 
 /*Tom Ellison*/
