96c5f312842a9fce27f568f9ffbe2389d201eb53
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-03-18 16:16:39 -0300

    uploading new file: saving-humanity-from-homo-sapiens.sql

---
+++ sql/positions/saving-humanity-from-homo-sapiens.sql
@@ -0,0 +1,6 @@
+ insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+ 
+ 
+
+('Rick Schwall', 'Saving Humanity from Homo Sapiens', 'Founder & Volunteer', '2011-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/rick-schwall-63652226/ http://shfhs.org/', NULL, NULL, 'Existential risk prevention'),
