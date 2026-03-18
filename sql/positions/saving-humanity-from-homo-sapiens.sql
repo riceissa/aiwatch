e77ea37ef88999614250705bebe6b1d593f814e6
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-03-18 16:30:31 -0300

    updating saving-humanity-from-homo-sapiens.sql

--- sql/positions/saving-humanity-from-homo-sapiens.sql
+++ sql/positions/saving-humanity-from-homo-sapiens.sql
@@ -1,7 +1,7 @@
  insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
 
-('Rick Schwall', 'Saving Humanity from Homo Sapiens', 'Founder & Volunteer', '2011-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/rick-schwall-63652226/ http://shfhs.org/', NULL, NULL, 'Existential risk prevention')
-,('Joy Livingwell', 'Saving Humanity from Homo Sapiens', 'Chief Researcher', '2019-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/joylivingwell/ http://shfhs.org/', NULL, NULL, 'Existential risk prevention')
+('Rick Schwall', 'Saving Humanity from Homo Sapiens', 'Founder & Volunteer', '2011-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/rick-schwall-63652226/ https://web.archive.org/web/20260318192249/https://shfhs.org/aboutus.html', NULL, NULL, 'Existential risk prevention') /*Last consulted Wayback snapshot is from 2026-03-18*/
+,('Joy Livingwell', 'Saving Humanity from Homo Sapiens', 'Chief Researcher', '2019-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/joylivingwell/ https://web.archive.org/web/20260318192249/https://shfhs.org/aboutus.html', NULL, NULL, 'Existential risk prevention') /*Last consulted Wayback snapshot is from 2026-03-18*/
 
 
  
