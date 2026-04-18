ce957c3ab91d5abb21cb85e69d9209c662e90a99
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-04-18 17:32:15 -0300

    uploading Gabriel Bobis file: positive-impact-society-erasmus.sql

---
+++ sql/positions/positive-impact-society-erasmus.sql
@@ -0,0 +1,12 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, employment_type, cause_area) values
+
+
+ ('Philipp van Sprang', 'Positive Impact Society Erasmus', 'Chair', '2024-01-01', 'year', NULL, NULL, 'https://web.archive.org/web/20260225000434/https://piserotterdam.com/aboutus', NULL, NULL, 'Cause prioritization')  /* Reviewed on 2026-02-14 / / Last available Wayback snapshot date is 2026-02-14 */
+,('Lili-Marie Schmidt', 'Positive Impact Society Erasmus', 'Chair', '2025-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/lili-marie-schmidt-917596237/ https://web.archive.org/web/20260225000434/https://piserotterdam.com/aboutus', NULL, NULL, 'Cause prioritization')  /* Reviewed on 2026-02-14 / / Last available Wayback snapshot date is 2026-02-14 */
+,('Trym Braathen', 'Positive Impact Society Erasmus', 'Chair', '2024-10-02', 'month', NULL, NULL, 'https://www.linkedin.com/in/trymbraathen/ https://web.archive.org/web/20260225000434/https://piserotterdam.com/aboutus', NULL, NULL, 'Cause prioritization')  /* Reviewed on 2026-02-14 / / Last available Wayback snapshot date is 2026-02-14 */
+,('Andrei Cucu', 'Positive Impact Society Erasmus', 'Chair', '2024-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/andrei-cucu-a9266b297/?originalSubdomain=nl https://web.archive.org/web/20260225000434/https://piserotterdam.com/aboutus', NULL, NULL, 'Cause prioritization') /* Reviewed on 2026-02-14 / / Last available Wayback snapshot date is 2026-02-14 */
+
+
+/* CLOSED */
+
+,('Trym Braathen', 'Positive Impact Society Erasmus', 'Board Member', '2024-06-01', 'month', '2024-10-01', 'month', 'https://www.linkedin.com/in/trymbraathen/', NULL, 'board member', 'Cause prioritization') 
