71bf70364ba848114f7aefff733191be440f374d
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-06-19 14:15:01 -0300

    updating longevity-global.sql

--- sql/positions/longevity-global.sql
+++ sql/positions/longevity-global.sql
@@ -1,16 +1,26 @@
 insert into positions
 (person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, ai_safety_relation, subject, employment_type, cause_area, notes)
 values
-('Christin Glorioso','Longevity Global','Founder & Executive Director','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+('Christin Glorioso','Longevity Global','Founder & Executive Director','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Nina Klymenko','Longevity Global','Head of Design','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','general',NULL,'Longevity',NULL),
+('Nina Klymenko','Longevity Global','Head of Design','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','general',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Niko Markov','Longevity Global','Assist. Director, Longevity SF','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+('Niko Markov','Longevity Global','Assist. Director, Longevity SF','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Matt Stergiou','Longevity Global','Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+('Matt Stergiou','Longevity Global','Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Franco Mendoza','Longevity Global','Assist. Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+('Franco Mendoza','Longevity Global','Assist. Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Nikolina Lauc','Longevity Global','Director, Longevity UAE','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+('Nikolina Lauc','Longevity Global','Director, Longevity UAE','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
 
-('Aleks Singer','Longevity Global','Director, Longevity SJ','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-03-23*/;
+('Aleks Singer','Longevity Global','Director, Longevity SJ','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
+
+('Stephanie Lindstrom','Longevity Global','Assist. Director, Longevity SF','2023-06-05','month',NULL,NULL,'https://web.archive.org/web/20230605012241/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
+
+('Justin Taylor','Longevity Global','VP of Communications','2023-09-28','month',NULL,NULL,'https://web.archive.org/web/20230928113623/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','popularization',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
+
+('Liv Kraemer','Longevity Global','Director, Longevity CH','2023-09-28','month',NULL,NULL,'https://web.archive.org/web/20230928113623/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
+
+('Natasa Billeci','Longevity Global','Director, Longevity MIA','2023-09-28','month',NULL,NULL,'https://web.archive.org/web/20230928113623/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/,
+
+('David Doswell','Longevity Global','Platform Engineer & Contributing Writer','2023-09-28','month',NULL,NULL,'https://web.archive.org/web/20230928113623/https://longevitygl.org/about-2/ https://web.archive.org/web/20231203180259/https://longevitygl.org/about-2/','unrelated','popularization',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-12-03*/;
