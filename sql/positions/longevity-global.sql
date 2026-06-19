b26c208e8a5772ae895f17cb7f18929d0f48dda2
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-06-19 14:11:46 -0300

    uploading new file: longevity-global.sql

---
+++ sql/positions/longevity-global.sql
@@ -0,0 +1,16 @@
+insert into positions
+(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, ai_safety_relation, subject, employment_type, cause_area, notes)
+values
+('Christin Glorioso','Longevity Global','Founder & Executive Director','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+
+('Nina Klymenko','Longevity Global','Head of Design','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','general',NULL,'Longevity',NULL),
+
+('Niko Markov','Longevity Global','Assist. Director, Longevity SF','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+
+('Matt Stergiou','Longevity Global','Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+
+('Franco Mendoza','Longevity Global','Assist. Director, Longevity NYC','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+
+('Nikolina Lauc','Longevity Global','Director, Longevity UAE','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL),
+
+('Aleks Singer','Longevity Global','Director, Longevity SJ','2023-03-23','multi-year',NULL,NULL,'https://web.archive.org/web/20230323020734/https://longevitygl.org/about-2/','unrelated','strategy',NULL,'Longevity',NULL) /*Last checked Wayback snapshot is from 2023-03-23*/;
