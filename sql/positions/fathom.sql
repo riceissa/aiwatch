53f8cb8cff5e2349e84870f73edb44231c192222
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-07-06 19:56:02 -0300

    Uploading new file: fathom.sql

---
+++ sql/positions/fathom.sql
@@ -0,0 +1,14 @@
+insert into positions(person, organization, title, start_date, start_date_precision, end_date, end_date_precision, urls, notes, ai_safety_relation, subject, employment_type, cause_area) values
+('Alekhya Tallapaka', 'Fathom', 'Director of Communications', '2024-09-01', 'month', '2025-04-01', 'month', 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'popularization', 'full-time', 'AI safety') /*Closed*/,
+('Alekhya Tallapaka', 'Fathom', 'Director of Policy & Partnerships', '2025-04-01', 'month', '2026-07-01', 'month', 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'policy', 'full-time', 'AI safety') /*Closed*/,
+('Alekhya Tallapaka', 'Fathom', 'AI Policy', '2026-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'policy', 'full-time', 'AI safety'),
+
+('Erik Preston', 'Fathom', 'Operations', '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/prestonerik/', NULL, 'position', 'general', NULL, 'AI safety'),
+
+('Andrew Freedman', 'Fathom', 'Co-Founder and CEO', '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/andrew-freedman-ba794939/', NULL, 'position', 'strategy', NULL, 'AI safety'),
+
+('Bri Treece', 'Fathom', NULL, '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/britreece/', NULL, 'position', 'strategy', NULL, 'AI safety'),
+
+('Adam Edgerton', 'Fathom', 'Director of Philanthropic Partnerships', '2025-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/adam-edgerton-362b138/', NULL, 'position', 'strategy', 'full-time', 'AI safety'),
+
+('Lucía de la Torre', 'Fathom', 'AI Policy', '2025-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/luciagonzalezmantecon/', NULL, 'position', 'policy', NULL, 'AI safety');
