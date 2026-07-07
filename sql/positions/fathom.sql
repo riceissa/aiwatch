b0fd669851e2f46fcff8672e16722e743fda211d
Sebastian Sanchez <sebastian123sanchez@gmail.com>
2026-07-06 21:48:23 -0300

    updating fathom.sql

--- sql/positions/fathom.sql
+++ sql/positions/fathom.sql
@@ -2,13 +2,18 @@ insert into positions(person, organization, title, start_date, start_date_precis
 ('Alekhya Tallapaka', 'Fathom', 'Director of Communications', '2024-09-01', 'month', '2025-04-01', 'month', 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'popularization', 'full-time', 'AI safety') /*Closed*/,
 ('Alekhya Tallapaka', 'Fathom', 'Director of Policy & Partnerships', '2025-04-01', 'month', '2026-07-01', 'month', 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'policy', 'full-time', 'AI safety') /*Closed*/,
 ('Alekhya Tallapaka', 'Fathom', 'AI Policy', '2026-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alekhya-tallapaka/', NULL, 'position', 'policy', 'full-time', 'AI safety'),
-
 ('Erik Preston', 'Fathom', 'Operations', '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/prestonerik/', NULL, 'position', 'general', NULL, 'AI safety'),
-
 ('Andrew Freedman', 'Fathom', 'Co-Founder and CEO', '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/andrew-freedman-ba794939/', NULL, 'position', 'strategy', NULL, 'AI safety'),
-
 ('Bri Treece', 'Fathom', NULL, '2024-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/britreece/', NULL, 'position', 'strategy', NULL, 'AI safety'),
-
 ('Adam Edgerton', 'Fathom', 'Director of Philanthropic Partnerships', '2025-08-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/adam-edgerton-362b138/', NULL, 'position', 'strategy', 'full-time', 'AI safety'),
-
-('Lucía de la Torre', 'Fathom', 'AI Policy', '2025-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/luciagonzalezmantecon/', NULL, 'position', 'policy', NULL, 'AI safety');
+('Lucía de la Torre', 'Fathom', 'AI Policy', '2025-01-01', 'year', NULL, NULL, 'https://www.linkedin.com/in/luciagonzalezmantecon/', NULL, 'position', 'policy', NULL, 'AI safety'),
+('Komal Trivedi', 'Fathom', 'Executive Assistant to the Co-Founders', '2024-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/komaltrivediezexec/', NULL, 'position', 'general', 'contractor', 'AI safety'),
+('Alexis Sacasas', 'Fathom', 'Chief of Staff', '2025-09-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/alexis-sacasas-a88bb694/', NULL, 'position', 'strategy', 'full-time', 'AI safety'),
+('Hugh Wyman H.', 'Fathom', 'Advisor', '2024-06-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/hugh-wyman-h-98002b27b/', NULL, 'position', 'strategy', 'advisor', 'AI safety'),
+('Graeme Finley', 'Fathom', 'Vice President of AI Assurance Provider Services', '2026-01-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/graeme-finley-660833/', NULL, 'position', 'strategy', 'full-time', 'AI safety'),
+('PJ Amelia', 'Fathom', 'Director, Member Services', '2026-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/pj-amelia-6b376316b/', NULL, 'position', 'general', 'full-time', 'AI safety'),
+('Jake Swanton', 'Fathom', 'VP of State Policy & External Affairs', '2025-07-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/jake-swanton/', NULL, 'position', 'policy', NULL, 'AI safety'),
+('Dave Barmore', 'Fathom', 'Policy Consultant', '2025-11-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/dave-barmore/', NULL, 'position', 'policy', 'contractor', 'AI safety'),
+('John Jacobs, MBA', 'Fathom', 'Vice President, Operations', '2026-04-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/johnjacobs777/', NULL, 'position', 'general', 'full-time', 'AI safety'),
+('Christopher Coger', 'Fathom', 'Senior Director of Digital Strategy', '2025-12-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/chriscoger/', NULL, 'position', 'popularization', 'full-time', 'AI safety'),
+('Lauren Wilson', 'Fathom', 'Senior Director, Stakeholder Engagement & Policy Partnerships', '2026-02-01', 'month', NULL, NULL, 'https://www.linkedin.com/in/laurenelainewilson/', NULL, 'position', 'policy', 'full-time', 'AI safety');
